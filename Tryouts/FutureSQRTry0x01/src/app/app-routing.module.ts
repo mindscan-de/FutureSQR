5cbdede1ee84a126ab3f96e4cd26b1ad7ec2cc8e
mindscan-de <maxim.gansert@gmail.com>
2022-05-30 23:27:25 +0200

    add idea, on the different directory/router structure + loadChildren

--- Tryouts/FutureSQRTry0x01/src/app/app-routing.module.ts
+++ Tryouts/FutureSQRTry0x01/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	
+	// { path:'', pathMatch: 'full', component: MainComponent},
+	// this path should lead to a component, where we will show some lists.
+	// { path:'p/{projectname}'}
+	// this path should lead to a project containing all recent commits as a list
+	// { path:'p/{projectname}/revision/{revisonid}}'}
+	// this path should lead to a certain revision
+	// { path:'p/{projectname}/diff/{revisionid}'}
+	// this path should lead to a diff to the previous version
+	// { path:'login', loadChildren: './authentication/authentication.module#AuthenticationModule' }
+	// 
+	
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
