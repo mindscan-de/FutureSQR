225f62c10a761f19a01a0fcb14c77708a46557ef
mindscan-de <maxim.gansert@gmail.com>
2022-05-31 22:54:57 +0200

    add review url

--- Tryouts/FutureSQRTry0x01/src/app/app-routing.module.ts
+++ Tryouts/FutureSQRTry0x01/src/app/app-routing.module.ts
@@ -5,13 +5,21 @@ const routes: Routes = [
 	
 	// { path:'', pathMatch: 'full', component: MainComponent},
 	// this path should lead to a component, where we will show some lists.
+	
 	// { path:'p/{projectname}'}
 	// this path should lead to a project containing all recent commits as a list
+	
 	// { path:'p/{projectname}/revision/{revisonid}}'}
 	// this path should lead to a certain revision
+	
 	// { path:'p/{projectname}/diff/{revisionid}'}
 	// this path should lead to a diff to the previous version
-	// { path:'login', loadChildren: './authentication/authentication.module#AuthenticationModule' }
+	
+	// { path:'p/{projectname}/review/{reviewname}'}
+	// this path should lead to a review in the project.
+	
+	
+	// { path:'login/', loadChildren: './authentication/authentication.module#AuthenticationModule' }
 	// 
 	
 ];
