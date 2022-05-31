1dd55901d438f4d878ad2bb9d9670fc2582595b6
mindscan-de <maxim.gansert@gmail.com>
2022-05-31 22:58:09 +0200

    reorder from special url to general url.

--- Tryouts/FutureSQRTry0x01/src/app/app-routing.module.ts
+++ Tryouts/FutureSQRTry0x01/src/app/app-routing.module.ts
@@ -4,20 +4,19 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
 	
 	// { path:'', pathMatch: 'full', component: MainComponent},
-	// this path should lead to a component, where we will show some lists.
-	
-	// { path:'p/{projectname}'}
-	// this path should lead to a project containing all recent commits as a list
+	// this path should lead to a component, where we will show some project lists customized tot the user
+
+	// { path:'p/{projectname}/review/{reviewname}'}
+	// this path should lead to a review in the project.
 	
 	// { path:'p/{projectname}/revision/{revisonid}}'}
-	// this path should lead to a certain revision
+	// this path should lead to a certain revision and a list of diffs for this particular version in the project
 	
 	// { path:'p/{projectname}/diff/{revisionid}'}
 	// this path should lead to a diff to the previous version
 	
-	// { path:'p/{projectname}/review/{reviewname}'}
-	// this path should lead to a review in the project.
-	
+	// { path:'p/{projectname}'}
+	// this path should lead to a project containing all recent commits as a list
 	
 	// { path:'login/', loadChildren: './authentication/authentication.module#AuthenticationModule' }
 	// 
