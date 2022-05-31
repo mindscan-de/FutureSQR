f6a02c0bb98d24a1cf5a1ce34f6d824b0e6881bc
mindscan-de <maxim.gansert@gmail.com>
2022-05-31 23:18:57 +0200

    add review component

---
+++ Tryouts/FutureSQRTry0x01/src/app/review/review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-review',
+  templateUrl: './review.component.html',
+  styleUrls: ['./review.component.scss']
+})
+export class ReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
