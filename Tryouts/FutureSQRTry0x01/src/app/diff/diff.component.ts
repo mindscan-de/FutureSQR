9f8fb4b443545513d49e6096ab459fc6137e1fc8
mindscan-de <maxim.gansert@gmail.com>
2022-05-31 23:47:57 +0200

    add a diff component

---
+++ Tryouts/FutureSQRTry0x01/src/app/diff/diff.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diff',
+  templateUrl: './diff.component.html',
+  styleUrls: ['./diff.component.scss']
+})
+export class DiffComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
