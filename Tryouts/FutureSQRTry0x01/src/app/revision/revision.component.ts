98344d55075b39f2140d47bce50157c2be209f6e
mindscan-de <maxim.gansert@gmail.com>
2022-06-03 00:07:31 +0200

    extract activeProjectID and activeRevisionID from path.

--- Tryouts/FutureSQRTry0x01/src/app/revision/revision.component.ts
+++ Tryouts/FutureSQRTry0x01/src/app/revision/revision.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-revision',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./revision.component.scss']
 })
 export class RevisionComponent implements OnInit {
+	
+  public activeProjectID: string = '';
+  public activeRevisionID: string = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+	this.activeProjectID = this.route.snapshot.paramMap.get('projectid');
+	this.activeRevisionID = this.route.snapshot.paramMap.get('revisionid');	
   }
 
 }
