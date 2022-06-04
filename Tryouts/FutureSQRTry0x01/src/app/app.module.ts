8bf242b1f2709d8f9fa8eab5c5080eb701cd21eb
mindscan-de <maxim.gansert@gmail.com>
2022-06-04 11:24:00 +0200

    add http client module to project.

--- Tryouts/FutureSQRTry0x01/src/app/app.module.ts
+++ Tryouts/FutureSQRTry0x01/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { AllProjectsComponent } from './all-projects/all-projects.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
