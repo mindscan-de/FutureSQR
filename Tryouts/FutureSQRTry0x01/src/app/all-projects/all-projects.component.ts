1915ce0c17c28350f19eefbbb2adc4cddde4c426
mindscan-de <maxim.gansert@gmail.com>
2022-06-04 11:35:27 +0200

    start querying the backend all projects.

--- Tryouts/FutureSQRTry0x01/src/app/all-projects/all-projects.component.ts
+++ Tryouts/FutureSQRTry0x01/src/app/all-projects/all-projects.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+// Services
+import { ProjectDataQueryBackendService } from '../backend-service/project-data-query-backend.service';
+
+// UI-Model (TODO: currently we use the backend model directly, but the backend model should be translated to a ui model) 
+
+// BackendModel
+import { BackendModelProjectItem } from '../backend-service/project-model/backend-model-project-item';
+
+
 @Component({
   selector: 'app-all-projects',
   templateUrl: './all-projects.component.html',
@@ -7,9 +16,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllProjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private projectDataQueryBackend : ProjectDataQueryBackendService ) { }
 
   ngOnInit(): void {
+	this.projectDataQueryBackend.getAllProjects().subscribe( 
+		data => this.onAllProjectsProvided(data),
+		error => console.log(error)
+	);
+  }
+
+  onAllProjectsProvided( allProjects: BackendModelProjectItem[]) : void {
+	
   }
 
 }
