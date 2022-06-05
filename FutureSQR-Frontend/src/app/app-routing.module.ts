61614dfd9fce1098eb6e67dedd265f8310968bd3
mindscan-de <maxim.gansert@gmail.com>
2022-06-05 09:20:09 +0200

    add empty main page component view to Project.

--- FutureSQR-Frontend/src/app/app-routing.module.ts
+++ FutureSQR-Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Views
+import { MainPageComponent } from './views/main-page/main-page.component';
+
+const routes: Routes = [
+	{ path:'', pathMatch: 'full', component: MainPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FutureSQR-Frontend/src/app/app.module.ts
+++ FutureSQR-Frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './views/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
