bc5bcbe2b273ca309a6f2a88e4a388660bfbd2a8
mindscan-de <maxim.gansert@gmail.com>
2022-06-05 20:14:38 +0200

    provide starred project data.

--- FutureSQR-Frontend/src/app/views/main-page/starred-projects/starred-projects.component.ts
+++ FutureSQR-Frontend/src/app/views/main-page/starred-projects/starred-projects.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+// Services
+import { ProjectDataQueryBackendService } from '../../../backend/services/project-data-query-backend.service';
+
+// BackendModel
+import { BackendModelProjectItem } from '../../../backend/model/backend-model-project-item';
+
+
 @Component({
   selector: 'app-starred-projects',
   templateUrl: './starred-projects.component.html',
   styleUrls: ['./starred-projects.component.css']
 })
 export class StarredProjectsComponent implements OnInit {
+	
+  public uiModelStarredProjects: BackendModelProjectItem[] = [];
 
-  constructor() { }
+  
+  constructor( private projectDataQueryBackend : ProjectDataQueryBackendService ) { }
 
   ngOnInit(): void {
+	this.projectDataQueryBackend.getMyStarredProjects().subscribe( 
+		data => this.onStarredProjectsProvided(data),
+		error => console.log(error)
+	);
   }
+
+  onStarredProjectsProvided( starredProjects: BackendModelProjectItem[]) : void {
+	this.uiModelStarredProjects = starredProjects;
+  }
+
 
 }
