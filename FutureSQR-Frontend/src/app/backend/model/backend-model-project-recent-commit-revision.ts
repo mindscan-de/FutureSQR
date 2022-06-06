f2fc17d006e77b81875b8ae2242a2bb9af8f80c5
mindscan-de <maxim.gansert@gmail.com>
2022-06-06 18:23:39 +0200

    refine backend model data for a single commit.

--- FutureSQR-Frontend/src/app/backend/model/backend-model-project-recent-commit-revision.ts
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-project-recent-commit-revision.ts
@@ -1,2 +1,8 @@
 export class BackendModelProjectRecentCommitRevision {
+		public shortrev:string = "";
+		public revisionid:string = "";
+		public authorname:string = "";
+		public authorid:string = "";
+		public date:string = "";
+		public message:string = "";
 }
