595c1091a06c9a6152a2b0c986d13c69fcd1bdb3
mindscan-de <maxim.gansert@gmail.com>
2022-06-09 20:47:12 +0200

    refactor move hardcoded values to assets.hardcoded, clean up server code

--- FutureSQR-DevBackend/src/de/mindscan/futuresqr/devhttpserver/futuresqr_dev.py
+++ FutureSQR-DevBackend/src/de/mindscan/futuresqr/devhttpserver/futuresqr_dev.py
@@ -6,7 +6,7 @@ Created on 05.06.2022
 
 
 from de.mindscan.futuresqr.gittools.dev_local_git_access import calculateRecentRevisionsForLocalGitRepo
-from de.mindscan.futuresqr.assets.hardcoded import getAllProjectToLocalPathMap, getAllStarredProjectsForUser
+from de.mindscan.futuresqr.assets.hardcoded import getAllProjectToLocalPathMap, getAllStarredProjectsForUser, getAllProjectsForUser
 
 from fastapi import FastAPI, Form, HTTPException
 
@@ -24,20 +24,7 @@ def getUserStarredProjects(user_uuid:str = ""):
 
 @app.get("/FutureSQR/rest/user/allaccessibleprojetcs")
 def getUserAllAccessibleProjects(user_uuid: str = ""):
-    result = [
-        {"project_id":"furiousiron-frontend", "project_name":"FuriousIron-Frontend", "description":"My personal source code engine project. Frontend. (Angular. TS)"},
-        {"project_id":"furiousiron-hfb", "project_name":"FuriousIron-HFB", "description":"Hash-Free Bloom-Filter (Proof of concept implementation)"},
-        {"project_id":"furiousiron-indexer", "project_name":"FuriousIron-Indexer", "description":"My personal source code search engine project. Indexer. (Java. Windows. No Database. Filesystem only) "},
-        {"project_id":"furiousiron-searchbackend", "project_name":"FuriousIron-SearchBackend", "description":"My personal source code search engine project. Backend. (Java. Tomcat. Windows. No Database. Filesystem only) "},
-        {"project_id":"brightflux", "project_name":"BrightFlux", "description":"LogFileViewer and LogFileAnalysis with yet unseen Features and written in Java"},
-        {"project_id":"futuresqr", "project_name":"FutureSQR", "description":"Code Review Tool - Future Source Quality Review"},
-        {"project_id":"curiousmyth", "project_name":"CuriousMyth", "description":"Modelling facts, entities and information for knowledge representation"},
-        {"project_id":"orangemoon-frontend", "project_name":"OrangeMoon-Frontend", "description":"Japanese Dictionary Web-App - Frontend (based on nodejs and angular)"},
-        {"project_id":"orangemoon-backend", "project_name":"OrangeMoon-Backend", "description":"Japanese Dictionary Web-App - Backend (based on fastapi and jamdict) "},
-        {"project_id":"fluentgenesis-embedder", "project_name":"FluentGenesis-Embedder", "description":""},
-        {"project_id":"fluentgenesis-classifier", "project_name":"FluentGenesis-Classifier", "description":""},
-        {"project_id":"fluentgenesis-plugin", "project_name":"FluentGenesis-Plugin", "description":""}
-        ]
+    result = getAllProjectsForUser()
     return result
 
 @app.get("/FutureSQR/rest/project/{projectid}/recentcommits")
