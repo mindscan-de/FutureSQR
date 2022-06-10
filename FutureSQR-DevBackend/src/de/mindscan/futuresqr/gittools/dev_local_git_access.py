a36926c5768f0a2edf3d4e4fe79b732a03656049
mindscan-de <maxim.gansert@gmail.com>
2022-06-10 23:29:17 +0200

    add some skeleton code to calculate diffs; ADDED WARNING for this code

--- FutureSQR-DevBackend/src/de/mindscan/futuresqr/gittools/dev_local_git_access.py
+++ FutureSQR-DevBackend/src/de/mindscan/futuresqr/gittools/dev_local_git_access.py
@@ -8,18 +8,19 @@ import subprocess
 GIT_FIELDS = ['shortrev','revisionid','authorname','authorid','date','reldate','message']
 GIT_FORMAT_PARAMS = ['%h','%H','%an','%ae','%ad','%ar','%s']
 
+# ATTENTION: NEVER EVER PUT THIS CODE INTO PRODUCTION, THIS CODE IS DANGEROUS
 
 def calculateRecentRevisionsForLocalGitRepo(local_git_repo_path:str):
     formatdetails = '%x1f'.join(GIT_FORMAT_PARAMS)
     
-    GIT_LOG_COMMAND = [
+    GIT_COMMAND = [
         'C:\\Program Files\\Git\\cmd\\git.exe', 
         '-C',  local_git_repo_path, 
         'log',  
         '--pretty=format:%x1f'+formatdetails+'%x1e'
     ]
     
-    output = subprocess.run(GIT_LOG_COMMAND, stdout=subprocess.PIPE)
+    output = subprocess.run(GIT_COMMAND, stdout=subprocess.PIPE)
     log = output.stdout.decode()    
 
     log = log.strip('\n\x1e').split('\x1e')
@@ -30,3 +31,24 @@ def calculateRecentRevisionsForLocalGitRepo(local_git_repo_path:str):
         'revisions': log
     }
     return recentRevisions
+
+def calculateDiffForSingleRevision(local_git_repo_path:str, revisionid:str):
+    
+    GIT_COMMAND = [
+        'C:\\Program Files\\Git\\cmd\\git.exe', 
+        '-C',  local_git_repo_path, 
+        'log',  
+        '-u',
+        '-1',
+        revisionid
+        ]
+    
+    output = subprocess.run(GIT_COMMAND, stdout=subprocess.PIPE)
+    log = output.stdout.decode()
+    
+    fileChanges = []
+    
+    diffData = {
+            'changes': fileChanges
+        }
+    return diffData
