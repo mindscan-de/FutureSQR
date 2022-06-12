1ea81b25842228e84fcd464189effa2ce12a0f32
mindscan-de <maxim.gansert@gmail.com>
2022-06-12 17:58:00 +0200

    add content change set variables

--- FutureSQR-Frontend/src/app/backend/model/backend-model-single-commit-file-content-change-set.ts
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-single-commit-file-content-change-set.ts
@@ -1,4 +1,9 @@
 export class BackendModelSingleCommitFileContentChangeSet {
 	// the file content changeset actually contains the particular line changes 
 	// and the involved line numbers and so on.
+	
+	// that will be reworked and updated, when better parsed
+	public line_info:string = "";
+	public line_diff_data: string[] = [];
+	
 }
