e508f716df0571f542662c57f2799b107f90463a
mindscan-de <maxim.gansert@gmail.com>
2022-06-12 19:46:27 +0200

    add a todo for the diff.

--- FutureSQR-DevBackend/src/de/mindscan/futuresqr/gittools/git_output_parser.py
+++ FutureSQR-DevBackend/src/de/mindscan/futuresqr/gittools/git_output_parser.py
@@ -33,10 +33,13 @@ def parse_log_by_rs_us(log, fieldnames):
     records = [ dict(zip(fieldnames,row)) for row in records ]
     return records
 
-# TODO: 
+# TODO:
+# * the last entry cotains one superfluous emtpy diff element.... (would require to parse the number of lines and process the the particluar diff.
+ 
 # * actually a full changeset contains a list of file changesets, and information about the commit
 # * a filechangeset contains a description of the file change (add, remove, modify...), and a list of line changesets 
 # * a line changeset contains a description of the lines changed and a list of lines.
+
 
 def parse_log_full_changeset(log):
     fileChangeSets = []
