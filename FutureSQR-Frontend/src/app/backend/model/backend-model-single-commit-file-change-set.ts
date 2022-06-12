75932770b3e1f068edccfdc8bf79eb6542f8347c
mindscan-de <maxim.gansert@gmail.com>
2022-06-12 18:54:53 +0200

    introduce member for containing filenames for the file changeset

--- FutureSQR-Frontend/src/app/backend/model/backend-model-single-commit-file-change-set.ts
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-single-commit-file-change-set.ts
@@ -4,6 +4,7 @@ export class BackendModelSingleCommitFileChangeSet {
 	// TODO: contains information about a particular file
 	//       and what happened to this file:
 	// modified, added, renamed, etc., which hash it has. and so on....
+	public lazy_diff_line:string = "";
 	
 	// It contains a list of file_content_changeset
 	public fileContentChangeSet: BackendModelSingleCommitFileContentChangeSet[] = [];
