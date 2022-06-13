349664f056cb1a8ed49b1f339a5d6aed890f1432
mindscan-de <maxim.gansert@gmail.com>
2022-06-13 22:13:51 +0200

    read the url parameters from path.

--- FutureSQR-Frontend/src/app/views/single-review-page/single-review-page.component.ts
+++ FutureSQR-Frontend/src/app/views/single-review-page/single-review-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+// Services
+import { ProjectDataQueryBackendService } from '../../backend/services/project-data-query-backend.service';
+
+// Backend Model
 
 @Component({
   selector: 'app-single-review-page',
@@ -6,10 +12,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./single-review-page.component.css']
 })
 export class SingleReviewPageComponent implements OnInit {
+	
+	public activeProjectID: string = '';
+	public activeReviewID: string = '';
 
-  constructor() { }
+	constructor( private projectDataQueryBackend : ProjectDataQueryBackendService, private route: ActivatedRoute ) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.activeProjectID = this.route.snapshot.paramMap.get('projectid');
+		this.activeReviewID = this.route.snapshot.paramMap.get('reviewid');
+		
+		// TODO: query the review information
+		// TODO: query some revision infromation for this particular review
+		// TODO: query the filelist for this review
+		// TODO: query the diffs for this review
+	}
 
 }
