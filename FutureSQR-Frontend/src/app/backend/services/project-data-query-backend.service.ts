900447e779cb41354c3210ccc39946e0fcfd1093
mindscan-de <maxim.gansert@gmail.com>
2022-06-15 21:20:12 +0200

    implement query method in backend for list of changed files.

--- FutureSQR-Frontend/src/app/backend/services/project-data-query-backend.service.ts
+++ FutureSQR-Frontend/src/app/backend/services/project-data-query-backend.service.ts
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs/Observable';
 import { BackendModelProjectItem } from '../model/backend-model-project-item';
 import { BackendModelProjectRecentCommits } from '../model/backend-model-project-recent-commits';
 import { BackendModelSingleCommitFullChangeSet } from '../model/backend-model-single-commit-full-change-set'; 
+import { BackendModelSingleCommitFileActionsInfo } from '../model/backend-model-single-commit-file-actions-info';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +18,8 @@ export class ProjectDataQueryBackendService {
 	private static readonly URL_GET_MY_STARRED_PROJECTS:string    = "/FutureSQR/rest/user/starredprojects";
 	// TODO: this should be cooler
 	private static readonly URL_GET_RECENT_PROJECT_COMMITS:string = "/FutureSQR/rest/project/${projectid}/recentcommits";
-	private static readonly URL_GET_PROJECT_REVISION_DIFF:string  = "/FutureSQR/rest/project/${projectid}/revisiondiff/${revisionid}"
+	private static readonly URL_GET_PROJECT_REVISION_DIFF:string  = "/FutureSQR/rest/project/${projectid}/revisiondiff/${revisionid}";
+	private static readonly URL_GET_PROJECT_REVISION_FILES:string = "/FutureSQR/rest/project/${projectid}/filelist/${revisionid}";
 
     constructor(private httpClient : HttpClient ) { }
 
@@ -39,6 +41,11 @@ export class ProjectDataQueryBackendService {
 	getRecentProjectRevisionDiffFullChangeSet(projectid:string, revisionid:string): Observable<BackendModelSingleCommitFullChangeSet> {
 		var url = `/FutureSQR/rest/project/${projectid}/revisiondiff/${revisionid}`;
 		return this.httpClient.get<BackendModelSingleCommitFullChangeSet>(url, {});
+	}
+	
+	getRecentProjectRevisionFilePathsData(projectid:string, revisionid:string): Observable<BackendModelSingleCommitFileActionsInfo> {
+		var url = `/FutureSQR/rest/project/${projectid}/filelist/${revisionid}`;
+		return this.httpClient.get<BackendModelSingleCommitFileActionsInfo>(url, {});
 	}
 	
 }
