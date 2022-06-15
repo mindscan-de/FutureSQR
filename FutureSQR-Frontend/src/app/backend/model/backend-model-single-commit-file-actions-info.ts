9f0b9d6e0f9083c65b0c7104fa1035f16ecc7a1c
mindscan-de <maxim.gansert@gmail.com>
2022-06-15 21:17:00 +0200

    export both Indexes...

--- FutureSQR-Frontend/src/app/backend/model/backend-model-single-commit-file-actions-info.ts
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-single-commit-file-actions-info.ts
@@ -1,7 +1,7 @@
 
 export class BackendModelSingleCommitFileActionsInfo {
-	public readonly FILENAME_IDX = 1;
-	public readonly FILEACTION_IDX = 0;	
+	public static readonly FILENAME_IDX = 1;
+	public static readonly FILEACTION_IDX = 0;	
 	
 	public fileActionList : string[][] = []; 
 }
