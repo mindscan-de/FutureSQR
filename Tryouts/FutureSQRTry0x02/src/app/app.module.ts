85dfb358add5cd4509938e4a07946218d4e3a993
mindscan-de <maxim.gansert@gmail.com>
2022-06-16 21:32:28 +0200

    got buildbreaks because of codemirror components.

--- Tryouts/FutureSQRTry0x02/src/app/app.module.ts
+++ Tryouts/FutureSQRTry0x02/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CodemirrorModule } from '@ctrl/ngx-codemirror'
+// import { CodemirrorModule } from '@ctrl/ngx-codemirror'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,8 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-	FormsModule,
-	CodemirrorModule
+	FormsModule
+// 	CodemirrorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Tryouts/FutureSQRTry0x02/src/main.ts
+++ Tryouts/FutureSQRTry0x02/src/main.ts
@@ -4,8 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/markdown/markdown';
+// import 'codemirror/mode/javascript/javascript';
+// import 'codemirror/mode/markdown/markdown';
 
 if (environment.production) {
   enableProdMode();
