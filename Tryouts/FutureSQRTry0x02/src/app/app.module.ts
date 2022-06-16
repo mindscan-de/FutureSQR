605265973df3e66701ce1b2392f03dbf09f61c6d
mindscan-de <maxim.gansert@gmail.com>
2022-06-16 21:08:38 +0200

    register codemirror

--- Tryouts/FutureSQRTry0x02/src/app/app.module.ts
+++ Tryouts/FutureSQRTry0x02/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CodemirrorModule } from '@ctrl/ngx-codemirror'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	FormsModule,
+	CodemirrorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
