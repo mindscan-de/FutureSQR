8630475fa7eb4e2887da6dca1a9e7d4b44a1b9e8
mindscan-de <maxim.gansert@gmail.com>
2022-06-18 17:02:14 +0200

    add routing to single and dual view.

--- Tryouts/FutureSQRTry0x02/src/app/app-routing.module.ts
+++ Tryouts/FutureSQRTry0x02/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Views
+import { MyDualDiffViewComponent } from './views/my-dual-diff-view/my-dual-diff-view.component';
+import { MySingleDiffViewComponent } from './views/my-single-diff-view/my-single-diff-view.component';
+
+const routes: Routes = [
+	{ path:'dual', pathMatch: 'full', component: MyDualDiffViewComponent},
+	{ path:'single', pathMatch: 'full', component: MySingleDiffViewComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
