de70ee9d352f969174ca4e03cc50efa1ff3fe30b
mindscan-de <maxim.gansert@gmail.com>
2022-06-18 17:23:55 +0200

    start with dual view and fixpresentation.

--- Tryouts/FutureSQRTry0x02/src/app/views/my-dual-diff-view/my-dual-diff-view.component.ts
+++ Tryouts/FutureSQRTry0x02/src/app/views/my-dual-diff-view/my-dual-diff-view.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyDualDiffViewComponent implements OnInit {
 
+	public content:string = "# Chapter 1\n"+"## Chapter 2\n"+"* X\n"+"  * Y\n"+"    * Z\n";  
+	public readOnly:boolean = true;
+	
+
   constructor() { }
 
   ngOnInit(): void {
