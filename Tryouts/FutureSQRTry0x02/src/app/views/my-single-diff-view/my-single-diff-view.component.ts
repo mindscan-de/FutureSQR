4f010de04d28209ee29c9f34f35cb001d30b2046
mindscan-de <maxim.gansert@gmail.com>
2022-06-18 17:16:01 +0200

    refactor: move codemirror example to single diff component view

--- Tryouts/FutureSQRTry0x02/src/app/views/my-single-diff-view/my-single-diff-view.component.ts
+++ Tryouts/FutureSQRTry0x02/src/app/views/my-single-diff-view/my-single-diff-view.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-single-diff-view.component.css']
 })
 export class MySingleDiffViewComponent implements OnInit {
+	
+	public content:string = "# Chapter 1\n"+"## Chapter 2\n"+"* X\n"+"  * Y\n"+"    * Z\n";  
+	public readOnly:boolean = true;
+	
 
   constructor() { }
 
