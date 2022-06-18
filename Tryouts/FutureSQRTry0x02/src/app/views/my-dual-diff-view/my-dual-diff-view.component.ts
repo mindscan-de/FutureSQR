29ae089d1e0220a07e0187303e703d688c3e4ec7
mindscan-de <maxim.gansert@gmail.com>
2022-06-18 19:33:28 +0200

    introduce ui diffmodel to rendering.

--- Tryouts/FutureSQRTry0x02/src/app/views/my-dual-diff-view/my-dual-diff-view.component.ts
+++ Tryouts/FutureSQRTry0x02/src/app/views/my-dual-diff-view/my-dual-diff-view.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+// ui model
+import { UiDiffContentModel } from './uimodel/ui-diff-content-model';
+ 
+
 @Component({
   selector: 'app-my-dual-diff-view',
   templateUrl: './my-dual-diff-view.component.html',
@@ -10,6 +14,14 @@ export class MyDualDiffViewComponent implements OnInit {
 	public content:string = "# Chapter 1\n"+"## Chapter 2\n"+"* X\n"+"  * Y\n"+"    * Z\n";  
 	public readOnly:boolean = true;
 	
+	public leftContent : UiDiffContentModel = new UiDiffContentModel("",1); 
+	public rightContent : UiDiffContentModel = new UiDiffContentModel("",100);
+	
+	
+	// input should be a full file-diff
+	// which then is m2m model transformed to a series of two diffs
+	// left removals and constant
+	// right adds and constant
 
   constructor() { }
 
