4160f6de3a783308b3619fc505a2a3dca389688e
mindscan-de <maxim.gansert@gmail.com>
2022-06-19 18:05:12 +0200

    refactor: move rendering for FileChangeSet to separate component.

--- FutureSQR-Frontend/src/app/commonui/file-change-set-single-diff/file-change-set-single-diff.component.ts
+++ FutureSQR-Frontend/src/app/commonui/file-change-set-single-diff/file-change-set-single-diff.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+
+
+// Should be a ui model instead of a backend model...
+import { BackendModelSingleCommitFileChangeSet } from '../../backend/model/backend-model-single-commit-file-change-set';
+
 
 @Component({
   selector: 'app-file-change-set-single-diff',
@@ -6,10 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./file-change-set-single-diff.component.css']
 })
 export class FileChangeSetSingleDiffComponent implements OnInit {
+	
+	public currentUiFileChangeSet: BackendModelSingleCommitFileChangeSet = new BackendModelSingleCommitFileChangeSet();
 
-  constructor() { }
+	// backed model is an intermediate modul until ui-model for a file changeset got refined.	
+	@Input() fileChangeSet:BackendModelSingleCommitFileChangeSet = new BackendModelSingleCommitFileChangeSet();
+	
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	ngOnChanges(changes: SimpleChanges): void {
+		let fileChangeSetCandidate:BackendModelSingleCommitFileChangeSet = changes.fileChangeSet.currentValue;
+		
+		if(this.currentUiFileChangeSet != fileChangeSetCandidate) {
+			this.currentUiFileChangeSet = fileChangeSetCandidate;
+		}
+	}
 }
