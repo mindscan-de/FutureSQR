fbccb721aad91971e4a87c893603b69f2a6bdb68
mindscan-de <maxim.gansert@gmail.com>
2022-06-20 19:58:00 +0200

    add missig import.

--- FutureSQR-Frontend/src/app/commonui/single-revision-side-by-side-dialog/single-revision-side-by-side-dialog.component.ts
+++ FutureSQR-Frontend/src/app/commonui/single-revision-side-by-side-dialog/single-revision-side-by-side-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 // Should be a ui model instead of a backend model...
