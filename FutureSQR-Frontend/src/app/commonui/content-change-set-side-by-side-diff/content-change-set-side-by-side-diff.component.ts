bcc81fb5de894ef48ecba17d933e6efd62fe5508
mindscan-de <maxim.gansert@gmail.com>
2022-06-20 19:27:38 +0200

    provide side by side diff for a content change set

--- FutureSQR-Frontend/src/app/commonui/content-change-set-side-by-side-diff/content-change-set-side-by-side-diff.component.ts
+++ FutureSQR-Frontend/src/app/commonui/content-change-set-side-by-side-diff/content-change-set-side-by-side-diff.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+
+// ui model
+import { UiDiffContentModel } from './uimodel/ui-diff-content-model';
+
 
 @Component({
   selector: 'app-content-change-set-side-by-side-diff',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-change-set-side-by-side-diff.component.css']
 })
 export class ContentChangeSetSideBySideDiffComponent implements OnInit {
+	
+	public leftContent : UiDiffContentModel = new UiDiffContentModel("",1); 
+	public rightContent : UiDiffContentModel = new UiDiffContentModel("",1);
+	
+	// make the editor readonly
+	public readOnly:boolean = true;
+	public viewPortMargin:number = 1;
+	
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+	
+ 	ngOnChanges(changes: SimpleChanges): void {
+		let contentChangeSetCurrent = changes.contentChangeSet.currentValue;
+		if(contentChangeSetCurrent) {
+			this.leftContent = this.filterLeftDiff(contentChangeSetCurrent);
+			this.rightContent = this.filterRightDiff(contentChangeSetCurrent)
+		}
+	}
+
+	filterLeftDiff(linediff: string[]) : UiDiffContentModel {
+		let leftdiff = linediff.filter(line => !line.startsWith("+")).join("\n");
+		
+		let result:UiDiffContentModel = new UiDiffContentModel(leftdiff,12);
+		
+		return result; 
+	}
+	
+	filterRightDiff(linediff: string[]) : UiDiffContentModel {
+		let rightdiff = linediff.filter(line => !line.startsWith("-")).join("\n");
+		
+		let result:UiDiffContentModel = new UiDiffContentModel(rightdiff,12);
+		
+		return result; 
+	}
 
 }
