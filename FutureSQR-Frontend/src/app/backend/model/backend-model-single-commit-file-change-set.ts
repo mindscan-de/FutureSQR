e0e00687bd855d3896656056e1f43b5c7bca4ac9
mindscan-de <maxim.gansert@gmail.com>
2022-06-20 19:11:32 +0200

    addmissing index line to commitfile change set.

--- FutureSQR-Frontend/src/app/backend/model/backend-model-single-commit-file-change-set.ts
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-single-commit-file-change-set.ts
@@ -5,6 +5,7 @@ export class BackendModelSingleCommitFileChangeSet {
 	//       and what happened to this file:
 	// modified, added, renamed, etc., which hash it has. and so on....
 	public lazy_diff_line:string = "";
+	public lazy_index_line:string = "";
 	
 	// It contains a list of file_content_changeset
 	public fileContentChangeSet: BackendModelSingleCommitFileContentChangeSet[] = [];
