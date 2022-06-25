57cadaee95929d08b9bb06bb860284dad3b3bc61
mindscan-de <maxim.gansert@gmail.com>
2022-06-25 13:56:28 +0200

    introduce a result object when creating a review.

---
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-create-review-result.spec.ts
@@ -0,0 +1,7 @@
+import { BackendModelCreateReviewResult } from './backend-model-create-review-result';
+
+describe('BackendModelCreateReviewResult', () => {
+  it('should create an instance', () => {
+    expect(new BackendModelCreateReviewResult()).toBeTruthy();
+  });
+});
---
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-create-review-result.ts
@@ -0,0 +1,6 @@
+export class BackendModelCreateReviewResult {
+	public projectId: string = "";
+	public revisionId: string = "";
+	public reviewId:string = "";
+	public reviewData: any;
+}
