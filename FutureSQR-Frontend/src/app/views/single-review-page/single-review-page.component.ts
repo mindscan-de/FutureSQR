beeeb51002c7a2c35c7ee7faffb7cc458edec23d
mindscan-de <maxim.gansert@gmail.com>
2022-06-25 17:01:31 +0200

    provide basic review information.

--- FutureSQR-Frontend/src/app/views/single-review-page/single-review-page.component.ts
+++ FutureSQR-Frontend/src/app/views/single-review-page/single-review-page.component.ts
@@ -5,6 +5,7 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { ProjectDataQueryBackendService } from '../../backend/services/project-data-query-backend.service';
 
 // Backend Model
+import { BackendModelReviewData } from '../../backend/model/backend-model-review-data';
 
 @Component({
   selector: 'app-single-review-page',
@@ -15,6 +16,7 @@ export class SingleReviewPageComponent implements OnInit {
 	
 	public activeProjectID: string = '';
 	public activeReviewID: string = '';
+	public activeReviewData: BackendModelReviewData = new BackendModelReviewData();
 
 	constructor( private projectDataQueryBackend : ProjectDataQueryBackendService, private route: ActivatedRoute ) { }
 
@@ -23,9 +25,18 @@ export class SingleReviewPageComponent implements OnInit {
 		this.activeReviewID = this.route.snapshot.paramMap.get('reviewid');
 		
 		// TODO: query the review information
+		this.projectDataQueryBackend.getReviewData(this.activeProjectID, this.activeReviewID).subscribe(
+			data => this.onReviewDataReceived(data),
+			error => {}			
+		);
+		
 		// TODO: query some revision infromation for this particular review
 		// TODO: query the filelist for this review
 		// TODO: query the diffs for this review
+	}
+	
+	onReviewDataReceived(reviewData: BackendModelReviewData):  void {
+		this.activeReviewData = reviewData;
 	}
 
 }
