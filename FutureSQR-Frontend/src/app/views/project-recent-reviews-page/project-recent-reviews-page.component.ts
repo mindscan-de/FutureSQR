43b9ad911205357760f8e61dd5885b21ccc5b06b
mindscan-de <maxim.gansert@gmail.com>
2022-06-29 18:57:31 +0200

    start with simple recent review page (body)

--- FutureSQR-Frontend/src/app/views/project-recent-reviews-page/project-recent-reviews-page.component.ts
+++ FutureSQR-Frontend/src/app/views/project-recent-reviews-page/project-recent-reviews-page.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+// Services
+import { ProjectDataQueryBackendService } from '../../backend/services/project-data-query-backend.service';
+
+
+// backend model -> should be a ui model.
+import { BackendModelReviewData } from '../../backend/model/backend-model-review-data';
 
 @Component({
   selector: 'app-project-recent-reviews-page',
@@ -6,10 +14,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-recent-reviews-page.component.css']
 })
 export class ProjectRecentReviewsPageComponent implements OnInit {
+	
+	public uiModelRecentProjectReviews:BackendModelReviewData[] = [];
+	public activeProjectID: string = '';
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+	constructor( private projectDataQueryBackend : ProjectDataQueryBackendService, private route: ActivatedRoute ) { }
+
+	ngOnInit(): void {
+		this.activeProjectID = this.route.snapshot.paramMap.get('projectid');
+	}
 
 }
