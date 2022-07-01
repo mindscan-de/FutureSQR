ed2afc7cb1f25128eccb46fcaee41c27455addbe
mindscan-de <maxim.gansert@gmail.com>
2022-07-01 20:56:47 +0200

    add a ui button which can star or unstar a project.

--- FutureSQR-Frontend/src/app/views/project-recent-commits-page/basic-project-information/basic-project-information.component.ts
+++ FutureSQR-Frontend/src/app/views/project-recent-commits-page/basic-project-information/basic-project-information.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 })
 export class BasicProjectInformationComponent implements OnInit {
 	
+	public isStarred: boolean = false;
+	
+	// TODO: we might provide better project information here.
 	@Input() activeProjectId:string;
 
 	constructor() { }
@@ -20,6 +23,14 @@ export class BasicProjectInformationComponent implements OnInit {
 		if(this.activeProjectId != activeProjectId) {
 			this.activeProjectId = activeProjectId;
 		}
+	}
+
+	onStarMe(activeProjectId:string): void {
+		this.isStarred = true;
+	}
+	
+	onUnstarMe(activeProjectId:string): void {
+		this.isStarred = false; 
 	}
 
 }
