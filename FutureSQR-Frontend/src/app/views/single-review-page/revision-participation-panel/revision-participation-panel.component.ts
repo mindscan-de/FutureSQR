5f52860ab7d42323537d2bd4a3e19db87ed90566
mindscan-de <maxim.gansert@gmail.com>
2022-07-03 21:05:50 +0200

    show the number of revisions in revision management panel

--- FutureSQR-Frontend/src/app/views/single-review-page/revision-participation-panel/revision-participation-panel.component.ts
+++ FutureSQR-Frontend/src/app/views/single-review-page/revision-participation-panel/revision-participation-panel.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, SimpleChanges, EventEmitter } from '@angular/core';
+
+// should be a uimodel instead of a backend model
+import { BackendModelReviewData } from '../../../backend/model/backend-model-review-data';
+
 
 @Component({
   selector: 'app-revision-participation-panel',
@@ -7,12 +11,24 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class RevisionParticipationPanelComponent implements OnInit {
 	
+	public currentUiReviewData: BackendModelReviewData = new BackendModelReviewData();
+	
+	@Input() activeReviewData: BackendModelReviewData = new BackendModelReviewData();
 	@Output() onRevisionStateChanged = new EventEmitter<string>();
 
 	constructor() { }
 
 	ngOnInit(): void {
 	}
+	
+	ngOnChanges(changes: SimpleChanges): void {
+		let reviewDataCandidate:BackendModelReviewData = changes.activeReviewData.currentValue;
+		
+		if(this.currentUiReviewData != reviewDataCandidate) {
+			this.currentUiReviewData = reviewDataCandidate;
+		}
+	}
+	
 
 	onAddRevisions(): void {
 		
