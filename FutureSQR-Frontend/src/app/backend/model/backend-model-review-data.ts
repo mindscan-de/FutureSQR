2adf38e71f4928a1ed8215311e7b6009a1af02b5
mindscan-de <maxim.gansert@gmail.com>
2022-07-04 20:56:58 +0200

    change return type of review authors according to backend.

--- FutureSQR-Frontend/src/app/backend/model/backend-model-review-data.ts
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-review-data.ts
@@ -5,7 +5,7 @@ export class BackendModelReviewData {
 	public reviewDescription: string = "";
 	public reviewRevisions: string[] = [];
 	// 
-	public reviewAuthors: string[] = [];
+	public reviewAuthors: Map<string,string> = new Map<string,string>();
 	// TODO: we have review results by different reviewers, actually we want both here.
 	public reviewReviewersResults: string[] = [];
 	public reviewFkProjectId: string = "";
