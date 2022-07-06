3d9cacd4d225a3c5ef438a277f4e9b05246fd5fe
mindscan-de <maxim.gansert@gmail.com>
2022-07-06 19:43:25 +0200

    start simple dialog content for add revison to review selection dialog

--- FutureSQR-Frontend/src/app/commonui/add-revision-to-review-selection-dialog/add-revision-to-review-selection-dialog.component.ts
+++ FutureSQR-Frontend/src/app/commonui/add-revision-to-review-selection-dialog/add-revision-to-review-selection-dialog.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-add-revision-to-review-selection-dialog',
   templateUrl: './add-revision-to-review-selection-dialog.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddRevisionToReviewSelectionDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit(): void {
   }
