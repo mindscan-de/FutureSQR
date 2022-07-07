ebc712a8fc5479294affdf53639907d09ddd018c
mindscan-de <maxim.gansert@gmail.com>
2022-07-07 21:27:29 +0200

    present the newest revisions since first revision of the review.

--- FutureSQR-Frontend/src/app/commonui/add-revision-to-review-selection-dialog/add-revision-to-review-selection-dialog.component.ts
+++ FutureSQR-Frontend/src/app/commonui/add-revision-to-review-selection-dialog/add-revision-to-review-selection-dialog.component.ts
@@ -3,10 +3,13 @@ import { Component, OnInit } from '@angular/core';
 
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
+// Services
+import { ProjectDataQueryBackendService } from '../../backend/services/project-data-query-backend.service';
 
 // should be a uimodel instead of a backend model
 import { BackendModelReviewData } from '../../backend/model/backend-model-review-data';
 import { BackendModelProjectRecentCommits } from '../../backend/model/backend-model-project-recent-commits';
+import { BackendModelProjectRecentCommitRevision } from '../../backend/model/backend-model-project-recent-commit-revision';
 
 
 @Component({
@@ -19,7 +22,7 @@ export class AddRevisionToReviewSelectionDialogComponent implements OnInit {
 	public uiModelRecentProjectCommitsGroupedByDate: Map<string, BackendModelProjectRecentCommits> = new Map<string, BackendModelProjectRecentCommits>();
 	public currentUiReviewData : BackendModelReviewData = new BackendModelReviewData();
 
-	constructor(public activeModal: NgbActiveModal) { }
+	constructor(private projectDataQueryBackend : ProjectDataQueryBackendService, public activeModal: NgbActiveModal) { }
 
 	ngOnInit(): void {
 	}
@@ -29,11 +32,43 @@ export class AddRevisionToReviewSelectionDialogComponent implements OnInit {
 		
 		// TODO collect the project commits groupted by date, without those
 		//      which are already connected to reviews.
+		// console.log(activeReviewData);
+		
+		this.projectDataQueryBackend.getRecentProjectCommutsSinceRevision( activeReviewData.reviewFkProjectId, activeReviewData.reviewRevisions[0]).subscribe (
+			data => this.onRevisionDataProvided(data) ,
+			error => {}			
+		);
+	}
+	
+	onRevisionDataProvided(ungrouped:BackendModelProjectRecentCommits) : void {
+		this.uiModelRecentProjectCommitsGroupedByDate = this.m2mGroupByDateTransformer(ungrouped);
 	}
 	
 	
 	public originalOrder = (a: KeyValue<number,string>, b: KeyValue<number,string>): number => {
 	  return 0;
-	}	
+	}
+	
+	m2mGroupByDateTransformer(ungrouped: BackendModelProjectRecentCommits): Map<string, BackendModelProjectRecentCommits> {
+		var grouped = new Map<string, BackendModelProjectRecentCommits>();
+		
+		// initialize with an illegal date first / instead of empty.
+		var previousdate = "1999-13-32";
+		
+		// iterate grouped and 
+		for(var i:number = 1;i<ungrouped.revisions.length;i++) {
+			var currentCommit:BackendModelProjectRecentCommitRevision = ungrouped.revisions[i];
+			
+			if(currentCommit.shortdate != previousdate) {
+				previousdate = currentCommit.shortdate;
+				grouped.set(previousdate, new BackendModelProjectRecentCommits());
+			}
+			
+			grouped.get(previousdate).revisions.push(currentCommit);
+		}
+		
+		return grouped;
+	}
+	
 	
 }
