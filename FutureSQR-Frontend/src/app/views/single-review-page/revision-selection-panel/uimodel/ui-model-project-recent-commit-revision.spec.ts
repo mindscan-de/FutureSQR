08af8e11a7135b63362d8583a97f98f22f62eed5
mindscan-de <maxim.gansert@gmail.com>
2022-07-11 22:12:15 +0200

    introduce new ui model for the recent commit revisions

---
+++ FutureSQR-Frontend/src/app/views/single-review-page/revision-selection-panel/uimodel/ui-model-project-recent-commit-revision.spec.ts
@@ -0,0 +1,7 @@
+import { UiModelProjectRecentCommitRevision } from './ui-model-project-recent-commit-revision';
+
+describe('UiModelProjectRecentCommitRevision', () => {
+  it('should create an instance', () => {
+    expect(new UiModelProjectRecentCommitRevision()).toBeTruthy();
+  });
+});
