3f99f690727e72f9f657f1406e524b8f1398ffad
mindscan-de <maxim.gansert@gmail.com>
2022-07-11 22:29:15 +0200

    introduce ui model and its m2m conversion.

--- FutureSQR-Frontend/src/app/views/single-review-page/revision-selection-panel/revision-selection-panel.component.ts
+++ FutureSQR-Frontend/src/app/views/single-review-page/revision-selection-panel/revision-selection-panel.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit, Input,  SimpleChanges } from '@angular/core';
 import { BackendModelReviewData } from '../../../backend/model/backend-model-review-data';
 import { BackendModelProjectRecentCommitRevision } from '../../../backend/model/backend-model-project-recent-commit-revision';
 
+
+import { UiModelProjectRecentCommitRevision } from './uimodel/ui-model-project-recent-commit-revision';
+
 @Component({
   selector: 'app-revision-selection-panel',
   templateUrl: './revision-selection-panel.component.html',
@@ -10,7 +13,8 @@ import { BackendModelProjectRecentCommitRevision } from '../../../backend/model/
 })
 export class RevisionSelectionPanelComponent implements OnInit {
 
-	public currentUiReviewRevisions: BackendModelProjectRecentCommitRevision [] = [];
+	public currentUiReviewRevisions: UiModelProjectRecentCommitRevision [] = [];
+	
 
 	@Input() activeReviewData: BackendModelReviewData = new BackendModelReviewData();
 	@Input() reviewRevisions: BackendModelProjectRecentCommitRevision[] = [];	
@@ -23,13 +27,47 @@ export class RevisionSelectionPanelComponent implements OnInit {
 	ngOnChanges(changes: SimpleChanges): void {
 		
 		if(changes.reviewRevisions !== undefined) {
-			this.currentUiReviewRevisions = changes.reviewRevisions.currentValue.reverse(); 
+			this.currentUiReviewRevisions = this.m2mTransform(changes.reviewRevisions.currentValue.reverse()); 
 		}
 	}
 	
 	// todo: convert to ui model
 
-	m2mTransform(inputList: BackendModelProjectRecentCommitRevision[]) : void {
+	m2mTransform(inputList: BackendModelProjectRecentCommitRevision[]) : UiModelProjectRecentCommitRevision[] {
+		let resultList: UiModelProjectRecentCommitRevision[] = [];
 		
+		let previousUiItem:UiModelProjectRecentCommitRevision = null;
+		for(var i:number=0; i<inputList.length;i++) {
+			let currentItem = inputList[i];
+			let newItem = new UiModelProjectRecentCommitRevision();
+			
+			newItem.authorid = currentItem.authorid;
+			newItem.authorname = currentItem.authorname;
+			newItem.date = currentItem.date;
+			newItem.hasReview = currentItem.hasReview;
+			newItem.message = currentItem.message;
+			newItem.parents = currentItem.parents;
+			newItem.parentsshort = currentItem.parentsshort;
+			newItem.reldate = currentItem.reldate;
+			newItem.reviewID = currentItem.reviewID;
+			newItem.revisionid = currentItem.revisionid;
+			newItem.shortdate = currentItem.shortdate;
+			newItem.shortrev = currentItem.shortrev;
+			
+			if(previousUiItem != null) {
+				newItem.isRoot = false;
+				
+				newItem.isDirectChild = (previousUiItem.shortrev == currentItem.parentsshort);
+			}
+			else
+			{
+				newItem.isRoot = true;
+			}
+			
+			previousUiItem=newItem;
+			resultList.push(newItem);
+		}
+		
+		 return resultList;
 	}
 }
