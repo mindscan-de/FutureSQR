23873590c0cf712cc6a21750071811586b92b9c4
mindscan-de <maxim.gansert@gmail.com>
2022-07-11 22:28:47 +0200

    improve uiModel for a commit.

--- FutureSQR-Frontend/src/app/views/single-review-page/revision-selection-panel/uimodel/ui-model-project-recent-commit-revision.ts
+++ FutureSQR-Frontend/src/app/views/single-review-page/revision-selection-panel/uimodel/ui-model-project-recent-commit-revision.ts
@@ -1,2 +1,18 @@
 export class UiModelProjectRecentCommitRevision {
+		public shortrev:string = "";
+		public revisionid:string = "";
+		public authorname:string = "";
+		public authorid:string = "";
+		public date:string = "";
+		public shortdate:string = "";
+		public reldate:string = "";
+		public message:string = "";
+		public hasReview: boolean = false;
+		public parents: string = "";
+		public parentsshort: string ="";
+		public reviewID: string = "";
+		
+		public isRoot: boolean = false;
+		public isDirectChild: boolean = false;
+
 }
