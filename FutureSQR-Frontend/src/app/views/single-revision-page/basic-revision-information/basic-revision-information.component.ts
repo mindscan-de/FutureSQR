de33f39bd2faa3b3e8bb26bc4aeb4aa1e0cc69cb
mindscan-de <maxim.gansert@gmail.com>
2022-07-16 23:02:09 +0200

    refactor: extract revision information presentation.

--- FutureSQR-Frontend/src/app/views/single-revision-page/basic-revision-information/basic-revision-information.component.ts
+++ FutureSQR-Frontend/src/app/views/single-revision-page/basic-revision-information/basic-revision-information.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input,  SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-basic-revision-information',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basic-revision-information.component.css']
 })
 export class BasicRevisionInformationComponent implements OnInit {
+	
+	@Input() activeProjectID: string = "";
+	@Input() activeRevisionID: string = "";
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		
+	}
 
 }
