baaee0f1b64672fc5339646d958a7e27cc839482
mindscan-de <maxim.gansert@gmail.com>
2022-07-18 22:59:43 +0200

    provide function to create a new review from a revision number.

--- FutureSQR-Frontend/src/app/views/single-revision-page/revision-action-panel/revision-action-panel.component.ts
+++ FutureSQR-Frontend/src/app/views/single-revision-page/revision-action-panel/revision-action-panel.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Input,  SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+
+// Services
+import { ProjectDataQueryBackendService } from '../../../backend/services/project-data-query-backend.service';
+
 
 import { BackendModelProjectRecentCommitRevision } from '../../../backend/model/backend-model-project-recent-commit-revision';
 
@@ -13,13 +18,23 @@ export class RevisionActionPanelComponent implements OnInit {
 	@Input() activeProjectID: string = "";
 	@Input() activeRevisionData: BackendModelProjectRecentCommitRevision = new BackendModelProjectRecentCommitRevision();	
 
-	constructor() { }
+	constructor(private projectDataQueryBackend : ProjectDataQueryBackendService,private router: Router ) { }
 
 	ngOnInit(): void {
 	}
 
 	ngOnChanges(changes: SimpleChanges): void {
 		
+	}
+
+	onCreateReview(projectId: string, revisionId: string) : void {
+		this.projectDataQueryBackend.createNewReview(projectId, revisionId).subscribe (
+			data => {
+				// TODO redirect o review page.
+				this.router.navigate(['/', projectId, 'review', data['reviewId']]);
+			},
+			error => {}
+		);
 	}
 
 }
