2fff1c681a043149dcceade6f87b001e23fe6a04
mindscan-de <maxim.gansert@gmail.com>
2022-07-23 22:05:11 +0200

    add new ui type for the informations of a file to review

---
+++ FutureSQR-Frontend/src/app/views/single-review-page/uimodel/ui-review-file-information.spec.ts
@@ -0,0 +1,7 @@
+import { UiReviewFileInformation } from './ui-review-file-information';
+
+describe('UiReviewFileInformation', () => {
+  it('should create an instance', () => {
+    expect(new UiReviewFileInformation()).toBeTruthy();
+  });
+});
