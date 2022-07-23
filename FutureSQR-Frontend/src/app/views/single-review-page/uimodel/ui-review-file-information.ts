1de0f6d6b1a2a041438d17abcac99d55cf900ba2
mindscan-de <maxim.gansert@gmail.com>
2022-07-23 22:19:41 +0200

    start ui model for review file information

--- FutureSQR-Frontend/src/app/views/single-review-page/uimodel/ui-review-file-information.ts
+++ FutureSQR-Frontend/src/app/views/single-review-page/uimodel/ui-review-file-information.ts
@@ -1,2 +1,23 @@
 export class UiReviewFileInformation {
+	public fileAction: string = "";
+	public fullFilePath: string = "";
+	public simpleFileName: string = "";
+	public parentFilePath: string = "";
+	
+	// maybe a separate object type
+	// TODO: statistics
+	// TODO: added lines
+	// TOOD: removed lines
+	
+	// maybe a separate object type
+	// TODO: open discussion threads open for this file
+	// TOOD: resolved discussion threads for this file
+	// TODO: number of discussion threads for this file
+	
+	constructor(filepath:string , fileAction: string ) {
+		this.fileAction = fileAction;
+		this.fullFilePath = filepath;
+		
+		//TODO: split the filepath into sinplefilename and parentfilepath
+	}
 }
