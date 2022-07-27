90d98d415200cdef189f00cf6c446eac293f8d34
mindscan-de <maxim.gansert@gmail.com>
2022-07-27 22:18:34 +0200

    add reminder what to do in review discussion panel component

--- FutureSQR-Frontend/src/app/views/single-review-page/review-discussion-panel/review-discussion-panel.component.ts
+++ FutureSQR-Frontend/src/app/views/single-review-page/review-discussion-panel/review-discussion-panel.component.ts
@@ -17,7 +17,12 @@ export class ReviewDiscussionPanelComponent implements OnInit {
 	}
 
 	ngOnChanges(changes: SimpleChanges): void {
+		// TODO: use backend service to retrieve discussion data, 
+		// when both values are valid.
 		
+		// the result of this query needs to be transformed into a ui model,
+		// especially transform the map data, because the map is not an object
+		// but must be converted from an object into a Map.
 	}
 
 }
