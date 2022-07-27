9d4ff1cc62be91408995a2ed69a9b74a09ad87d9
mindscan-de <maxim.gansert@gmail.com>
2022-07-27 22:07:03 +0200

    add message model for backend model for thread messages

---
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-threads-message.spec.ts
@@ -0,0 +1,7 @@
+import { BackendModelThreadsMessage } from './backend-model-threads-message';
+
+describe('BackendModelThreadsMessage', () => {
+  it('should create an instance', () => {
+    expect(new BackendModelThreadsMessage()).toBeTruthy();
+  });
+});
