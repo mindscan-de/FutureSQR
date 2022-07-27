8b40796f737dfe0eb01bb79a8facd4b4b4dbcd6e
mindscan-de <maxim.gansert@gmail.com>
2022-07-27 22:01:01 +0200

    extend the backend threads model by full thread model.

---
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-threads-full-thread.spec.ts
@@ -0,0 +1,7 @@
+import { BackendModelThreadsFullThread } from './backend-model-threads-full-thread';
+
+describe('BackendModelThreadsFullThread', () => {
+  it('should create an instance', () => {
+    expect(new BackendModelThreadsFullThread()).toBeTruthy();
+  });
+});
---
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-threads-full-thread.ts
@@ -0,0 +1,7 @@
+export class BackendModelThreadsFullThread {
+	public threadId: string = "";
+	public authorId: string = "";
+	public messagesId: string[] = [];
+	// TODO: message threads_messages type / map of messageId->message
+	public messages:any;
+}
