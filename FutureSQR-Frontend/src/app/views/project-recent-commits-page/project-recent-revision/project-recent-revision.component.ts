97eee0b7d6f45309719e886dca8a3c36964cc1b9
mindscan-de <maxim.gansert@gmail.com>
2022-08-03 23:38:34 +0200

    start retrieval of file list for commit revision.

--- FutureSQR-Frontend/src/app/views/project-recent-commits-page/project-recent-revision/project-recent-revision.component.ts
+++ FutureSQR-Frontend/src/app/views/project-recent-commits-page/project-recent-revision/project-recent-revision.component.ts
@@ -4,6 +4,12 @@ import { Router } from '@angular/router';
 // Services
 import { ProjectDataQueryBackendService } from '../../../backend/services/project-data-query-backend.service';
 
+// UI Model
+import { UiReviewFileInformation } from '../../../commonui/uimodel/ui-review-file-information';
+
+// Backend Model
+import { BackendModelSingleCommitFileActionsInfo } from '../../../backend/model/backend-model-single-commit-file-actions-info';
+import { BackendModelProjectRecentCommitRevision } from '../../../backend/model/backend-model-project-recent-commit-revision';
 
 @Component({
   selector: 'app-project-recent-revision',
@@ -13,7 +19,9 @@ import { ProjectDataQueryBackendService } from '../../../backend/services/projec
 export class ProjectRecentRevisionComponent implements OnInit {
 
 	@Input() activeProjectID : string;
-	@Input() activeRevision : any;
+	@Input() activeRevision : BackendModelProjectRecentCommitRevision = undefined;
+	
+	public uiFileInformations: UiReviewFileInformation[] = undefined;
 	
 	public showFileList: boolean = false;
 	
@@ -38,11 +46,38 @@ export class ProjectRecentRevisionComponent implements OnInit {
 	// Open Close filelist
 	onToggleFileList(): void {
 		// if file list is known/received, then just toggle to show
-		this.showFileList = !this.showFileList;
+		
+		if(this.uiFileInformations != undefined) {
+			this.showFileList = !this.showFileList;
+			return;
+		}
+		
+		let that = this;
 		
 		// otherwise retrieve the list and then show the list.
+		
+		this.projectDataQueryBackend.getRecentProjectRevisionFilePathsData(this.activeProjectID,this.activeRevision.revisionid).subscribe(
+			data =>  this.onFileListActionsProvided(data),
+			error => console.log(error)
+		);
 	}
 	
 	
 	// TODO: retrieve file list for this version
+	
+	onFileListActionsProvided( fileChanges: BackendModelSingleCommitFileActionsInfo) : void {
+		let fileInformations : UiReviewFileInformation[] = [];
+		
+		let map = fileChanges.fileActionMap;
+		for(let i: number = 0;i<map.length;i++) {
+			let fileInfo: UiReviewFileInformation = new UiReviewFileInformation( map[i][1], map[i][0], true );
+			fileInformations.push(fileInfo);
+		}
+		this.uiFileInformations = fileInformations;
+		
+		this.showFileList = !this.showFileList;
+	}
+	
 }
+
+
