dddfd191c26563361e1c161820fe7345d9ba9dba
mindscan-de <maxim.gansert@gmail.com>
2022-08-03 23:08:54 +0200

    extract presentation of a review revision to a component.

--- FutureSQR-Frontend/src/app/views/project-recent-commits-page/project-recent-revision/project-recent-revision.component.ts
+++ FutureSQR-Frontend/src/app/views/project-recent-commits-page/project-recent-revision/project-recent-revision.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+
+// Services
+import { ProjectDataQueryBackendService } from '../../../backend/services/project-data-query-backend.service';
+
 
 @Component({
   selector: 'app-project-recent-revision',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectRecentRevisionComponent implements OnInit {
 
-  constructor() { }
+	@Input() activeProjectID : string;
+	@Input() activeRevision : any;
+	
+	
 
-  ngOnInit(): void {
-  }
+	constructor( private projectDataQueryBackend : ProjectDataQueryBackendService, private router: Router  ) { }
 
+	ngOnInit(): void {
+	}
+
+
+	ngOnChanges(changes: SimpleChanges): void {
+		
+	}
+	
+	onCreateReview(projectId: string, revisionId: string) : void {
+		this.projectDataQueryBackend.createNewReview(projectId, revisionId).subscribe (
+			data => {
+				// TODO redirect o review page.
+				this.router.navigate(['/', projectId, 'review', data['reviewId']]);
+			},
+			error => {}
+		);
+	}
+	
 }
