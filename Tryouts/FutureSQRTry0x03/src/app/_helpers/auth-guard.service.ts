149df448a0bec1ee45e53ec86cf63afde52f7f21
mindscan-de <maxim.gansert@gmail.com>
2022-08-13 20:58:02 +0200

    add auth guard service.

---
+++ Tryouts/FutureSQRTry0x03/src/app/_helpers/auth-guard.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {Router, CanActivate} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate{
+	
+	constructor(
+		private router: Router
+	) { }
+	
+	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+		// TODO: check if we have a user
+		
+		// if we don't have a user, we want the user to navigate to the account page and perform a login
+		// using a return URL
+		
+		
+		return false; 
+	}
+	
+}
