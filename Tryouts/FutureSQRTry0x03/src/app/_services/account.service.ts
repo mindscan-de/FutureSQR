2c67262f6b91f31d89a70e4b77275b8d1ed5ecf8
mindscan-de <maxim.gansert@gmail.com>
2022-08-13 22:26:10 +0200

    start with account service

---
+++ Tryouts/FutureSQRTry0x03/src/app/_services/account.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+	private userSubject: BehaviorSubject<User>;
+	public user: Observable<User>;
+
+	constructor() {
+		this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user')));
+		this.user = this.userSubject.asObservable();
+	}
+	
+	public get userValue(): User {
+		return this.userSubject.value;
+	}
+}
