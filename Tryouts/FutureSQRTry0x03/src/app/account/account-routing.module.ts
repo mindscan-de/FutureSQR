056ea0ee289c5043c03de271f3e237b7e5440bc5
mindscan-de <maxim.gansert@gmail.com>
2022-08-14 21:53:20 +0200

    provide LayoutComponent.

--- Tryouts/FutureSQRTry0x03/src/app/account/account-routing.module.ts
+++ Tryouts/FutureSQRTry0x03/src/app/account/account-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AccountLayoutComponent } from './account-layout/account-layout.component';
 import { LoginComponent } from '../views/login/login.component';
 
 const routes: Routes = [
 	{
-		path: '',
+		path: '', component: AccountLayoutComponent,
 		children: [
 			{ path: 'login', component: LoginComponent }
 			
