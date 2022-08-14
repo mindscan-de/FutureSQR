ceee2992793759bbc314ddd80d4876ab3cf25774
mindscan-de <maxim.gansert@gmail.com>
2022-08-14 21:46:12 +0200

    add commented section to the routes, to protect "urls" and login first

--- Tryouts/FutureSQRTry0x03/src/app/_helpers/auth-guard.service.ts
+++ Tryouts/FutureSQRTry0x03/src/app/_helpers/auth-guard.service.ts
@@ -13,9 +13,12 @@ export class AuthGuardService implements CanActivate{
 	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 		// TODO: check if we have a user
 		
+		
+		
 		// if we don't have a user, we want the user to navigate to the account page and perform a login
 		// using a return URL
 		
+		// redirect to the login page.
 		this.router.navigate(['/account/login'], { queryParams: { returnUrl : state.url}});
 		return false; 
 	}
--- Tryouts/FutureSQRTry0x03/src/app/app-routing.module.ts
+++ Tryouts/FutureSQRTry0x03/src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {HomeComponent } from './views/home/home.component';
-import {UsersComponent } from './views/users/users.component';
+import { HomeComponent } from './views/home/home.component';
+import { UsersComponent } from './views/users/users.component';
+import { AuthGuardService } from './_helpers/auth-guard.service';
 
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
 
 const routes: Routes = [
-	{ path: '', component: HomeComponent },
-	{ path: 'users', component: UsersComponent},
+	{ path: '', component: HomeComponent  /*, canActivate: [AuthGuardService]*/ },
+	{ path: 'users', component: UsersComponent /*, canActivate: [AuthGuardService] */},
 	
 	// login, register
 	{ path: 'account', loadChildren: accountModule },
