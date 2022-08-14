d0a77539a9da8ba9bc30afea93df854df136947f
mindscan-de <maxim.gansert@gmail.com>
2022-08-14 10:44:53 +0200

    add Home Component

--- Tryouts/FutureSQRTry0x03/src/app/app.module.ts
+++ Tryouts/FutureSQRTry0x03/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './views/login/login.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
