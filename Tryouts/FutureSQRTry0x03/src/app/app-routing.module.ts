2bf837890ad8168bde206e2222517c6af38342e4
mindscan-de <maxim.gansert@gmail.com>
2022-08-14 21:28:12 +0200

    start with account module having its own routing.

--- Tryouts/FutureSQRTry0x03/src/app/app-routing.module.ts
+++ Tryouts/FutureSQRTry0x03/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent } from './views/home/home.component';
 import {UsersComponent } from './views/users/users.component';
 
+
+const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
+
 const routes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'users', component: UsersComponent},
 	
 	// login, register
-	// { path: 'account'},
+	{ path: 'account', loadChildren: accountModule },
 	
 	// otherwise redirect to home
 	{ path: '**', redirectTo:''}
