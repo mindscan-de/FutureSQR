812e7d3f9cd7df05c108041c4c8f668191f12897
mindscan-de <maxim.gansert@gmail.com>
2022-08-15 21:41:05 +0200

    add comment for the account layout Component

--- Tryouts/FutureSQRTry0x03/src/app/account/account-routing.module.ts
+++ Tryouts/FutureSQRTry0x03/src/app/account/account-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from '../views/login/login.component';
 
 const routes: Routes = [
 	{
+		// we have a Account Layout Component, having it's  own router outlet.
 		path: '', component: AccountLayoutComponent,
 		children: [
 			{ path: 'login', component: LoginComponent }
