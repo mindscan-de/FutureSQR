fa9266bf7a7ca9d5a640458a487c727bc8a1facf
mindscan-de <maxim.gansert@gmail.com>
2022-08-15 21:57:56 +0200

    add support for formGroups in Form.

--- Tryouts/FutureSQRTry0x03/src/app/account/account.module.ts
+++ Tryouts/FutureSQRTry0x03/src/app/account/account.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AccountRoutingModule } from './account-routing.module';
 
@@ -10,6 +11,7 @@ import { AccountLayoutComponent } from './account-layout/account-layout.componen
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     AccountRoutingModule
   ],
   declarations: [
