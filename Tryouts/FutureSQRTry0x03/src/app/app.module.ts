1c4525e662b463e9a67abc08b8a3ff634d12da03
mindscan-de <maxim.gansert@gmail.com>
2022-08-16 22:12:44 +0200

    add missing Http client module.

--- Tryouts/FutureSQRTry0x03/src/app/app.module.ts
+++ Tryouts/FutureSQRTry0x03/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { UsersComponent } from './views/users/users.component';
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
+	HttpClientModule,
     NgbModule
   ],
   providers: [],
