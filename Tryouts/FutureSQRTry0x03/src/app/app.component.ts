e3fb4b33dbd8f89ae928440c467682a2fd240750
mindscan-de <maxim.gansert@gmail.com>
2022-08-19 21:30:25 +0200

    add login and logout stuff.

--- Tryouts/FutureSQRTry0x03/src/app/app.component.ts
+++ Tryouts/FutureSQRTry0x03/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+import { AccountService }  from './_services/account.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FutureSQRTry0x03';
+	title = 'FutureSQRTry0x03';
+	public user;
+	
+	constructor ( 
+		private accountService: AccountService
+	) {	
+	};
+
+
+	public onLogout(): void {
+		this.accountService.logout();
+	}
 }
