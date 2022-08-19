5c44333d280b5c21e5458c9eae7d2787c688bb30
mindscan-de <maxim.gansert@gmail.com>
2022-08-19 21:11:39 +0200

    make sure that the auth_guard can identify whether a user is logged on

--- Tryouts/FutureSQRTry0x03/src/app/_helpers/auth-guard.service.ts
+++ Tryouts/FutureSQRTry0x03/src/app/_helpers/auth-guard.service.ts
@@ -1,22 +1,28 @@
 import { Injectable } from '@angular/core';
 import {Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
 
+import { AccountService } from '../_services/account.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate{
 	
 	constructor(
-		private router: Router
+		private router: Router,
+		private accountService: AccountService
 	) { }
 	
 	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-		// TODO: check if we have a user
+		// so we are either logged in
+		const user = this.accountService.userValue;
 		
+		if( user ) {
+			return true;
+		}
 		
-		
-		// if we don't have a user, we want the user to navigate to the account page and perform a login
-		// using a return URL
+		// if we don't have a logged in user, we want the user to navigate to the 
+		// account page and perform a login using a return URL
 		
 		// redirect to the login page.
 		this.router.navigate(['/account/login'], { queryParams: { returnUrl : state.url}});
