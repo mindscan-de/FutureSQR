c2e69cc936933601246f4716d76c43a0277aa7b1
mindscan-de <maxim.gansert@gmail.com>
2022-09-21 21:56:11 +0200

    add simple user item into backend model.

---
+++ Tryouts/FutureSQRTry0x03/src/app/_models/backend-model-simple-user-item.spec.ts
@@ -0,0 +1,7 @@
+import { BackendModelSimpleUserItem } from './backend-model-simple-user-item';
+
+describe('BackendModelSimpleUserItem', () => {
+  it('should create an instance', () => {
+    expect(new BackendModelSimpleUserItem()).toBeTruthy();
+  });
+});
