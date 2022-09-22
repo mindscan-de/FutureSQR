9b4871b5bc70c25b8bdaa0935d444f7ebab14b40
mindscan-de <maxim.gansert@gmail.com>
2022-09-22 21:24:25 +0200

    start with basic "simpleUserItem" class, e.g. for userlist.

--- Tryouts/FutureSQRTry0x03/src/app/_models/backend-model-simple-user-item.ts
+++ Tryouts/FutureSQRTry0x03/src/app/_models/backend-model-simple-user-item.ts
@@ -1,2 +1,9 @@
 export class BackendModelSimpleUserItem {
+	
+	public uuid: string = "";
+	public loginname: string = "";
+	public displayname: string = "";
+	public avatarlocation: string = "";
+	public isbanned : boolean = false;
+	
 }
