f92ce3434190b5988a15085033b295fb9d1367f2
mindscan-de <maxim.gansert@gmail.com>
2022-09-24 22:19:41 +0200

    provide a user list by the backend.

--- FutureSQR-DevBackend/src/de/mindscan/futuresqr/users/users_database.py
+++ FutureSQR-DevBackend/src/de/mindscan/futuresqr/users/users_database.py
@@ -83,6 +83,10 @@ class UsersDatabase(object):
         
         return userRow
     
+    def selectAllUSers(self):
+        # TODO sort by some criteria
+        return self._userMap.values()
+    
     def banUser(self, logonname:str):
         for user in self._userMap:
             if user[USER_LOGON_NAME] == logonname:
