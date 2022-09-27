2a95eddaefcaf340259172a893d8fdf3390a6d88
mindscan-de <maxim.gansert@gmail.com>
2022-09-27 22:23:55 +0200

    move comment and empty callback to add user component.

--- Tryouts/FutureSQRTry0x03/src/app/views/users/users.component.ts
+++ Tryouts/FutureSQRTry0x03/src/app/views/users/users.component.ts
@@ -63,15 +63,4 @@ export class UsersComponent implements OnInit {
 		);
 	}
 	
-	onCreateUserClicked(): void {
-		// TODO: create a NBModal Dialog.
-		// the dialog should provide the invocation of the backend
-		// on success this modal dialog closes itself
-		// or the modal dialog closes itself on cancel
-		// in case of successful close, we want to trigger a reload of 
-		// the userlist or to patch the userlist and point to the entry (maybe we add user first to the list)
-		
-		// or instead of a modal dialog we can have a router endpoint
-		// which can be navigated, and a token may be obtained. / maybe better
-	}
 }
