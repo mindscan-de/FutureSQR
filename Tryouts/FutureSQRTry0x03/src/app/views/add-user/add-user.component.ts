d4807505bc7b7adbb6dd599d6c71397e905f428f
mindscan-de <maxim.gansert@gmail.com>
2022-09-28 22:15:47 +0200

    basic idea what to do - must be tested with a form....

--- Tryouts/FutureSQRTry0x03/src/app/views/add-user/add-user.component.ts
+++ Tryouts/FutureSQRTry0x03/src/app/views/add-user/add-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup,  Validators } from '@angular/forms';
+
+import { AccountService } from '../../_services/account.service';
 
 @Component({
   selector: 'app-add-user',
@@ -6,12 +9,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent implements OnInit {
+	
+	public addForm: FormGroup;
+	public submitted = false;
 
-  constructor() { }
+	constructor(
+		private formBuilder : FormBuilder,
+		private accountService: AccountService
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.addForm = this.formBuilder.group( {
+			username: ['', Validators.required],
+			// password field should depend on backed auth method and only then be set as required
+			// also add validator, that both passwords match each other
+			password: ['', Validators.required],
+			password2: ['', Validators.required],
+			
+			displayname: ['', Validators.required],
+			emailcontact: ['', [Validators.required, Validators.email]]
+			} )
+	}
 
+	// accessor for the form.
+	get f() { return this.addForm.controls; }
+	
+	onSubmit() : void {
+		this.submitted = true;
+		
+		if(this.addForm.invalid) {
+			this.submitted = false;
+			return;
+		}
+		
+		let that:AddUserComponent = this; 
+		
+		this.accountService.postAddUser(
+			this.f.username.value, 
+			this.f.displayname.value,
+			this.f.emailcontact.value,
+			this.f.password
+			).subscribe( {
+				next : (data) => {
+					// TODO navigate to userpage - not part of the backend
+					// to tell frontend which state the frontend should take
+					console.log("User created");
+					console.log(data);
+				},
+				error : error => { 
+					that.submitted = false;
+					}
+				}
+			)
+		
+	}
 
 /*	onCreateUserClicked(): void {
 		// TODO: create a NBModal Dialog.
