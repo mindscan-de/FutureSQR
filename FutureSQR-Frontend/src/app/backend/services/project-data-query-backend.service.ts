5700c0c1c027303c88573447fe5da40adcd7cbc3
mindscan-de <maxim.gansert@gmail.com>
2022-10-02 21:26:38 +0200

    add reminder to each of these functions, to rework their subscriptions

--- FutureSQR-Frontend/src/app/backend/services/project-data-query-backend.service.ts
+++ FutureSQR-Frontend/src/app/backend/services/project-data-query-backend.service.ts
@@ -18,7 +18,6 @@ import { BackendModelThreadsData } from '../model/backend-model-threads-data';
 
 /**
  * TODO: rework all subscription and post events to avoid any memory and/or resource leaks.
-
  */
 
 @Injectable({
@@ -35,16 +34,19 @@ export class ProjectDataQueryBackendService {
 
     constructor(private httpClient : HttpClient ) { }
 
+	// TODO: rework this subscription to once.
     getAllProjects () : Observable<BackendModelProjectItem[]> {
 	    return this.httpClient.get<BackendModelProjectItem[]>(ProjectDataQueryBackendService.URL_GET_ALL_PROJECTS, {});
     }
 
+	// TODO: rework this subscription to once.
 	// Actually the starred projects depend on the users choices
 	getMyStarredProjects() : Observable<BackendModelProjectItem[]> {
 		// TODO: provide USER-UUID / username
 		return this.httpClient.get<BackendModelProjectItem[]>(ProjectDataQueryBackendService.URL_GET_MY_STARRED_PROJECTS, {});
 	}
 	
+	// TODO: rework this subscription to once.
 	starProject(projectid:string) : Observable<any> {
 		var url =`/FutureSQR/rest/project/${projectid}/star`;
 		let formdata = new FormData();
@@ -55,6 +57,7 @@ export class ProjectDataQueryBackendService {
 		return this.httpClient.post<any>(url,formdata);
 	}
 	
+	// TODO: rework this subscription to once.
 	unstarProject(projectid:string) : Observable<any> {
 		var url =`/FutureSQR/rest/project/${projectid}/unstar`;
 		let formdata = new FormData();
@@ -65,6 +68,7 @@ export class ProjectDataQueryBackendService {
 		return this.httpClient.post<any>(url,formdata);
 	}
 	
+	// TODO: rework this subscription to once.
 	updateProjectCache(projectid:string) : Observable<any> {
 		var url =`/FutureSQR/rest/project/${projectid}/updatecache`;
 		let formdata = new FormData();
@@ -72,58 +76,67 @@ export class ProjectDataQueryBackendService {
 		return this.httpClient.post<any>(url,formdata);
 	}
 	
-	
+	// TODO: rework this subscription to once.
 	getRecentProjectCommits(projectid:string) : Observable<BackendModelProjectRecentCommits> {
 		var url = `/FutureSQR/rest/project/${projectid}/recentcommits`;
 		return this.httpClient.get<BackendModelProjectRecentCommits>(url, {});
 	}
 	
-	
+	// TODO: rework this subscription to once.
 	getRecentProjectCommutsSinceRevision(projectid: string, revisionid:string):Observable<BackendModelProjectRecentCommits> {
 		var url = `/FutureSQR/rest/project/${projectid}/recentcommitsfromrevid/${revisionid}`;
 		return this.httpClient.get<BackendModelProjectRecentCommits>(url, {});
 	} 	
 	
+	// TODO: rework this subscription to once.
 	getRecentProjectRevisionDiffFullChangeSet(projectid:string, revisionid:string): Observable<BackendModelSingleCommitFullChangeSet> {
 		var url = `/FutureSQR/rest/project/${projectid}/revisiondiff/${revisionid}`;
 		return this.httpClient.get<BackendModelSingleCommitFullChangeSet>(url, {});
 	}
 	
+	// TODO: rework this subscription to once.
 	getReviewRevisionDiffFullChangeSet(projectid:string, reviewid:string) : Observable<BackendModelSingleCommitFullChangeSet> {
 		var url = `/FutureSQR/rest/project/${projectid}/reviewdiff/${reviewid}`;
 		return this.httpClient.get<BackendModelSingleCommitFullChangeSet>(url, {});
 	}
 	
+	// TODO: rework this subscription to once.
 	getRecentProjectRevisionFilePathsData(projectid:string, revisionid:string): Observable<BackendModelSingleCommitFileActionsInfo> {
 		var url = `/FutureSQR/rest/project/${projectid}/revisionfilelist/${revisionid}`;
 		return this.httpClient.get<BackendModelSingleCommitFileActionsInfo>(url, {});
 	}
 	
+	// TODO: rework this subscription to once.
 	getRecentProjectRevisionInformation(projectid:string, revisionid:string): Observable<BackendModelProjectRecentCommitRevision> {
 		var url = `/FutureSQR/rest/project/${projectid}/revision/${revisionid}/information`;
 		return this.httpClient.get<BackendModelProjectRecentCommitRevision>(url, {});
 	}
 	
+	// TODO: rework this subscription to once.
 	getReviewFilePathsData(projectid:string, reviewid:string): Observable<BackendModelSingleCommitFileActionsInfo> {
 		var url = `/FutureSQR/rest/project/${projectid}/review/${reviewid}/filelist`;
 		return this.httpClient.get<BackendModelSingleCommitFileActionsInfo>(url, {});
 	}
 	
+	// TODO: rework this subscription to once.
 	getReviewData(projectid:string, reviewid:string): Observable<BackendModelReviewData> {
 		var url = `/FutureSQR/rest/project/${projectid}/review/${reviewid}/information`;
 		return this.httpClient.get<BackendModelReviewData>(url, {});
 	}
 	
+	// TODO: rework this subscription to once.
 	getThreadData(projectid:string, reviewid:string): Observable<BackendModelThreadsData> {
 		var url = `/FutureSQR/rest/project/${projectid}/review/${reviewid}/threads`;
 		return this.httpClient.get<BackendModelThreadsData>(url, {});
 	}
 	
+	// TODO: rework this subscription to once.
 	getReviewSimpleRevisionInformationList(projectid: string, reviewid: string): Observable <BackendModelProjectRecentCommitRevision[]> {
 		var url = `/FutureSQR/rest/project/${projectid}/review/${reviewid}/revisiondetails`
 		return this.httpClient.get<BackendModelProjectRecentCommitRevision[]>(url, {});
 	}
 	
+	// TODO: rework this subscription to once.
 	getRecentReviewsByProject(projectid:string): Observable<BackendModelProjectRecentReviews> {
 		var url = `/FutureSQR/rest/project/${projectid}/recentreviews`;
 		
@@ -131,12 +144,14 @@ export class ProjectDataQueryBackendService {
 		return this.httpClient.get<BackendModelProjectRecentReviews>(url, {});
 	}
 	
+	// TODO: rework this subscription to once.
 	getSimpleInformationByProject(projectid:string) : Observable<BackendModelProjectSimpleInformation> {
 		var url = `/FutureSQR/rest/project/${projectid}/information`;
 		
 		return this.httpClient.get<BackendModelProjectSimpleInformation>(url, {});
 	}
 	
+	// TODO: rework this subscription to once.
 	createNewReview(projectid:string, revisionid:string): Observable<BackendModelCreateReviewResult> {
 		var url = `/FutureSQR/rest/project/${projectid}/review/create`;
 		let formdata = new FormData();
@@ -146,7 +161,7 @@ export class ProjectDataQueryBackendService {
 		return this.httpClient.post<BackendModelCreateReviewResult>(url, formdata);
 	}
 	
-	
+	// TODO: rework this subscription to once.
 	closeReview(projectid:string, reviewid:string): Observable<any> {
 		var url =`/FutureSQR/rest/project/${projectid}/review/close`;
 		let formdata = new FormData();
@@ -156,6 +171,7 @@ export class ProjectDataQueryBackendService {
 		return this.httpClient.post<any>(url,formdata);
 	}
 	
+	// TODO: rework this subscription to once.
 	reopenReview(projectid:string, reviewid:string): Observable<any> {
 		var url =`/FutureSQR/rest/project/${projectid}/review/reopen`;
 		let formdata = new FormData();
@@ -165,6 +181,7 @@ export class ProjectDataQueryBackendService {
 		return this.httpClient.post<any>(url,formdata);
 	}
 	
+	// TODO: rework this subscription to once.
 	appendReviewWithRevision(projectid: string, reviewid:string, revisionid:string): Observable<any> {
 		var url = `/FutureSQR/rest/project/${projectid}/review/appendrevision`;
 		
@@ -175,6 +192,7 @@ export class ProjectDataQueryBackendService {
 		return this.httpClient.post<any>(url,formdata);
 	}
 	
+	// TODO: rework this subscription to once.
 	deleteReview(projectid:string, reviewid:string): Observable<any> {
 		var url =`/FutureSQR/rest/project/${projectid}/review/delete`;
 		let formdata = new FormData();
@@ -184,6 +202,7 @@ export class ProjectDataQueryBackendService {
 		return this.httpClient.post<any>(url,formdata);
 	}
 	
+	// TODO: rework this subscription to once.
 	addReviewer(projectid:string, reviewid:string, reviewerid:string): Observable<any> {
 		var url = `/FutureSQR/rest/project/${projectid}/review/addreviewer`;
 		
@@ -195,6 +214,7 @@ export class ProjectDataQueryBackendService {
 		return this.httpClient.post<any>(url,formdata);
 	}
 	
+	// TODO: rework this subscription to once.
 	approveReview(projectid: string, reviewid:string, reviewerid:string) : Observable<any> {
 		var url = `/FutureSQR/rest/project/${projectid}/review/approvereview`;
 		
@@ -206,7 +226,7 @@ export class ProjectDataQueryBackendService {
 		return this.httpClient.post<any>(url,formdata);
 	}
 
-
+	// TODO: rework this subscription to once.
 	concernReview(projectid: string, reviewid:string, reviewerid:string) : Observable<any> {
 		var url = `/FutureSQR/rest/project/${projectid}/review/concernreview`;
 		
@@ -218,6 +238,7 @@ export class ProjectDataQueryBackendService {
 		return this.httpClient.post<any>(url,formdata);
 	}
 	
+	// TODO: rework this subscription to once.
 	createThreadForReview(projectid: string, reviewid: string, authorid:string, message:string ) : Observable<any> {
 		var url = `/FutureSQR/rest/project/${projectid}/review/${reviewid}/createthread`;
 		
@@ -228,6 +249,7 @@ export class ProjectDataQueryBackendService {
 		return this.httpClient.post<any>(url,formdata);
 	}
 	
+	// TODO: rework this subscription to once.
 	replyThreadMessageForReview(projectid: string, reviewid: string, threadid:string, authorid:string, replytoid:string, message:string) : Observable<any> {
 		var url = `/FutureSQR/rest/project/${projectid}/review/${reviewid}/replythread`;
 		
@@ -240,6 +262,7 @@ export class ProjectDataQueryBackendService {
 		return this.httpClient.post<any>(url,formdata);
 	}
 	
+	// TODO: rework this subscription to once.
 	updateThreadMessageForReview(projectid: string, reviewid: string, threadid:string, authorid:string, messageid:string, newmessage_text:string) : Observable<any> {
 		var url = `/FutureSQR/rest/project/${projectid}/review/${reviewid}/editmessage`;
 		
