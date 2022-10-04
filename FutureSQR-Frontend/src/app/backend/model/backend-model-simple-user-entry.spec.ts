3bd3330f802d494e33ba6e3c0c88150a9112c8bf
mindscan-de <maxim.gansert@gmail.com>
2022-10-04 23:23:32 +0200

    add simple user entry type to backend model

---
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-simple-user-entry.spec.ts
@@ -0,0 +1,7 @@
+import { BackendModelSimpleUserEntry } from './backend-model-simple-user-entry';
+
+describe('BackendModelSimpleUserEntry', () => {
+  it('should create an instance', () => {
+    expect(new BackendModelSimpleUserEntry()).toBeTruthy();
+  });
+});
---
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-simple-user-entry.ts
@@ -0,0 +1,7 @@
+export class BackendModelSimpleUserEntry {
+	public uuid : string = "";
+	public displayname : string = "";
+	public avatarlocation : string = "";
+	public isbanned : boolean = false;
+	
+}
