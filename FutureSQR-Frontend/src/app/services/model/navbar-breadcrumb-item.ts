31c5bf992d3b0280c15fedea371617011638b43e
mindscan-de <maxim.gansert@gmail.com>
2022-10-08 21:40:10 +0200

    provide basic link info.

--- FutureSQR-Frontend/src/app/services/model/navbar-breadcrumb-item.ts
+++ FutureSQR-Frontend/src/app/services/model/navbar-breadcrumb-item.ts
@@ -1,2 +1,10 @@
 export class NavbarBreadcrumbItem {
+	public linkText:string = "linkText";
+	// can be string or something else, should actually be string array?
+	public routerLink:any[] = [];
+	
+	constructor(linkText, routerLink) {
+		this.linkText = linkText;
+		this.routerLink = routerLink;
+	}
 }
