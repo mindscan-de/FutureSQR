bf7ba0beac38dfe2979e26d06a87a3ecb5cb693d
mindscan-de <maxim.gansert@gmail.com>
2022-10-10 21:43:57 +0200

    add some reminders what todo in add participant selection dialog.

--- FutureSQR-Frontend/src/app/commonui/add-participant-to-review-selection-dialog/add-participant-to-review-selection-dialog.component.ts
+++ FutureSQR-Frontend/src/app/commonui/add-participant-to-review-selection-dialog/add-participant-to-review-selection-dialog.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+
+// Services
+import { UserDataQueryBackendService } from '../../backend/services/user-data-query-backend.service';
+
+
 @Component({
   selector: 'app-add-participant-to-review-selection-dialog',
   templateUrl: './add-participant-to-review-selection-dialog.component.html',
@@ -7,9 +14,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddParticipantToReviewSelectionDialogComponent implements OnInit {
 
-  constructor() { }
+	constructor(
+		private userDataQueryBackend: UserDataQueryBackendService,
+		public activeModal: NgbActiveModal
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		// TODO query the user data query Backend for a user list
+		// TODO query a suggestion shortlist for the review (based on file, and other metrics?)
+	}
+	
+	// add some on data provided information....
+	// for full userdata list (maybe with some filters or so...)
+	// for reviewer shortlist
+	
+	// Add some setters
+	
+	
 
 }
