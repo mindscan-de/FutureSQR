83a273beb0b3deac18190d16e24b358a095c167b
mindscan-de <maxim.gansert@gmail.com>
2022-10-21 20:56:39 +0200

    implement skeleton for a current ui user service.

--- FutureSQR-Frontend/src/app/services/current-user.service.ts
+++ FutureSQR-Frontend/src/app/services/current-user.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { CurrentUiUser } from './model/current-ui-user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrentUserService {
 
-  constructor() { }
+	private _currentUserValue:CurrentUiUser = new CurrentUiUser();
+	
+	private _currentUserSubject: BehaviorSubject<CurrentUiUser>;
+	private currentUserSubject: Observable<CurrentUiUser>;
+
+	constructor() {
+		this._currentUserSubject = new BehaviorSubject<CurrentUiUser>(this._currentUserValue);
+		this.currentUserSubject = this._currentUserSubject.asObservable();
+	}
+	
+	
+	setCurrentUser(currentUser):void {
+		// update the backing item
+		this._currentUserValue = currentUser;
+		
+		// update all subscribed current user Listeners
+		this._currentUserSubject.next(this._currentUserValue);
+	}
+	
+	
+	clearCurrentUser() : void {
+		this._currentUserValue = new CurrentUiUser();
+		this._currentUserSubject.next(this._currentUserValue);
+	}
+	
+	
+	asObservable(): Observable<CurrentUiUser> {
+		return this.currentUserSubject;
+	}
 }
