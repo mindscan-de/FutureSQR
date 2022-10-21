d0dce86d8db5826e61f8406ff4f6b176c8703c1e
mindscan-de <maxim.gansert@gmail.com>
2022-10-21 20:55:13 +0200

    implement a skeleton for a current ui user.

---
+++ FutureSQR-Frontend/src/app/services/model/current-ui-user.spec.ts
@@ -0,0 +1,7 @@
+import { CurrentUiUser } from './current-ui-user';
+
+describe('CurrentUiUser', () => {
+  it('should create an instance', () => {
+    expect(new CurrentUiUser()).toBeTruthy();
+  });
+});
