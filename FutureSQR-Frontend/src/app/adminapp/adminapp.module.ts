bdb222086354749e291e93d8ad08e70f2495178e
mindscan-de <maxim.gansert@gmail.com>
2022-10-23 16:22:42 +0200

    update navigation bar in admin view.

--- FutureSQR-Frontend/src/app/adminapp/adminapp.module.ts
+++ FutureSQR-Frontend/src/app/adminapp/adminapp.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AdminappRoutingModule } from './adminapp-routing.module';
 import { AdminappComponent } from './adminapp.component';
+import { ConfigureProjectComponent } from './views/configure-project/configure-project.component';
 
 
 @NgModule({
-  declarations: [AdminappComponent],
+  declarations: [AdminappComponent, ConfigureProjectComponent],
   imports: [
     CommonModule,
     AdminappRoutingModule
