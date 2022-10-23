bb2ad7868d08f3b484354897f6a6a7eb54ae8a82
mindscan-de <maxim.gansert@gmail.com>
2022-10-24 00:08:57 +0200

    integrate configure users component into admin app component

--- FutureSQR-Frontend/src/app/adminapp/views/configure-users/configure-users.component.ts
+++ FutureSQR-Frontend/src/app/adminapp/views/configure-users/configure-users.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+// Admin App Services
+import { AdminNavigationBarService }  from '../../services/admin-navigation-bar.service';
+import { AdminNavbarBreadcrumbItem } from '../../services/model/admin-navbar-breadcrumb-item';
+
+
 @Component({
   selector: 'app-configure-users',
   templateUrl: './configure-users.component.html',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigureUsersComponent implements OnInit {
 
-  constructor() { }
+	constructor(
+ 		private adminNavigationBarService : AdminNavigationBarService
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.updateNavigationBar();
+	}
+	
+	updateNavigationBar() : void {
+		let x:AdminNavbarBreadcrumbItem[] = []
+		
+		x.push(new AdminNavbarBreadcrumbItem('users', ['users'], true));
+		 
+		this.adminNavigationBarService.setAdminBreadCrumbNavigation(x);
+	}
 
 }
