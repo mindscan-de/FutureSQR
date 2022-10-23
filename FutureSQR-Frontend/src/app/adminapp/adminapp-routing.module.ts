5b5150dea873730d8dceb3724f72b7905f4b68c2
mindscan-de <maxim.gansert@gmail.com>
2022-10-24 00:13:51 +0200

    add routing for configure groups and configure users.

--- FutureSQR-Frontend/src/app/adminapp/adminapp-routing.module.ts
+++ FutureSQR-Frontend/src/app/adminapp/adminapp-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminappComponent } from './adminapp.component';
 import { ConfigureProjectComponent } from './views/configure-project/configure-project.component';
 import { ConfigureProjectsComponent } from './views/configure-projects/configure-projects.component';
+import { ConfigureUsersComponent } from './views/configure-users/configure-users.component';
+import { ConfigureGroupsComponent } from './views/configure-groups/configure-groups.component';
 
 const routes: Routes = [
 	// show the main admin page
@@ -14,8 +16,10 @@ const routes: Routes = [
 	{ path: 'projects' , component: ConfigureProjectsComponent },
 	
 	// users
+	{ path: 'users' , component: ConfigureUsersComponent },
 	
 	// groups
+	{ path: 'groups' , component: ConfigureGroupsComponent },
 	
 	// catch all other unknown URLs
 	{ path:'**',redirectTo:''}
