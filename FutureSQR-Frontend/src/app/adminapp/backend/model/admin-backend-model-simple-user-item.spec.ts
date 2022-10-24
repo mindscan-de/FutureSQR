cce3f777eb3a9d8522931e6f350bd3914b5bb5c2
mindscan-de <maxim.gansert@gmail.com>
2022-10-24 18:25:16 +0200

    add admin backend model for a simple user description.

---
+++ FutureSQR-Frontend/src/app/adminapp/backend/model/admin-backend-model-simple-user-item.spec.ts
@@ -0,0 +1,7 @@
+import { AdminBackendModelSimpleUserItem } from './admin-backend-model-simple-user-item';
+
+describe('AdminBackendModelSimpleUserItem', () => {
+  it('should create an instance', () => {
+    expect(new AdminBackendModelSimpleUserItem()).toBeTruthy();
+  });
+});
