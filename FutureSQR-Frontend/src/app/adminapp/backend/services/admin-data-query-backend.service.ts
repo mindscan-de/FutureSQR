25469e5597b5d69c6003c33b779ea15e6ab1e912
mindscan-de <maxim.gansert@gmail.com>
2022-10-24 18:31:45 +0200

    implement soome admin data query backend methods for tests.

--- FutureSQR-Frontend/src/app/adminapp/backend/services/admin-data-query-backend.service.ts
+++ FutureSQR-Frontend/src/app/adminapp/backend/services/admin-data-query-backend.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { map,first } from 'rxjs/operators';
+
+
+import { AdminBackendModelSimpleUserItem } from '../model/admin-backend-model-simple-user-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminDataQueryBackendService {
 
-  constructor() { }
+	constructor(
+		private httpClient: HttpClient
+	) { }
+	
+	
+	
+	public getAdminUserList(): Observable<AdminBackendModelSimpleUserItem[]> {
+		let restURL = '/FutureSQR/rest/user/adminuserlist';
+		
+		// TODO: actually this needs to be checked for rights, to get a complete userlist
+		return this.httpClient.get<AdminBackendModelSimpleUserItem[]>( restURL ).pipe( first() );
+	}
+	
+	public postBanUser(username:string): Observable<AdminBackendModelSimpleUserItem> {
+		let restURL = '/FutureSQR/rest/user/ban';
+		
+		let formdata = new FormData();
+		
+		formdata.append('username',username);
+		
+		// make sure we automatically unsubscribe / otherwise memory leak
+		return this.httpClient.post<AdminBackendModelSimpleUserItem>( restURL, formdata).pipe( first() );
+	}
+
+	public postUnbanUser(username:string): Observable<AdminBackendModelSimpleUserItem> {
+		let restURL = '/FutureSQR/rest/user/unban';
+		
+		let formdata = new FormData();
+		
+		formdata.append('username',username);
+		
+		// make sure we automatically unsubscribe / otherwise memory leak
+		return this.httpClient.post<AdminBackendModelSimpleUserItem>( restURL, formdata).pipe( first() );
+	}
+	
+	
 }
