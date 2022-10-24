c5f62583bdd7dafbc2f43768475289df2a76ee28
mindscan-de <maxim.gansert@gmail.com>
2022-10-24 18:30:52 +0200

    add class members to admin user model

--- FutureSQR-Frontend/src/app/adminapp/backend/model/admin-backend-model-simple-user-item.ts
+++ FutureSQR-Frontend/src/app/adminapp/backend/model/admin-backend-model-simple-user-item.ts
@@ -1,2 +1,9 @@
 export class AdminBackendModelSimpleUserItem {
+
+	public uuid: string = "";
+	public loginname: string = "";
+	public displayname: string = "";
+	public avatarlocation: string = "";
+	public isbanned : boolean = false;
+
 }
