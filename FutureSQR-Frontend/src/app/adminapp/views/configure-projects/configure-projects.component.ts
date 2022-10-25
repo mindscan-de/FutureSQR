4cb0f36ca790af0b34bec365843cd2627b43c93b
mindscan-de <maxim.gansert@gmail.com>
2022-10-25 23:10:20 +0200

    provide project base backend service for project data

--- FutureSQR-Frontend/src/app/adminapp/views/configure-projects/configure-projects.component.ts
+++ FutureSQR-Frontend/src/app/adminapp/views/configure-projects/configure-projects.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+// Services
+import { ProjectDataQueryBackendService } from '../../../backend/services/project-data-query-backend.service';
+
 // Admin App Services
 import { AdminNavigationBarService }  from '../../services/admin-navigation-bar.service';
 import { AdminNavbarBreadcrumbItem } from '../../services/model/admin-navbar-breadcrumb-item';
+
+// BackendModel - should be actually 
+import { BackendModelProjectItem } from '../../../backend/model/backend-model-project-item';
+
 
 @Component({
   selector: 'app-configure-projects',
@@ -10,14 +17,27 @@ import { AdminNavbarBreadcrumbItem } from '../../services/model/admin-navbar-bre
   styleUrls: ['./configure-projects.component.css']
 })
 export class ConfigureProjectsComponent implements OnInit {
+	
+	public uiModelAllProjects: BackendModelProjectItem[] = [];
 
 	constructor(
+		private projectDataQueryBackend : ProjectDataQueryBackendService,		
  		private adminNavigationBarService : AdminNavigationBarService
 	) { }
 
 	ngOnInit(): void {
 		this.updateNavigationBar();
+		
+		this.projectDataQueryBackend.getAllProjects().subscribe( 
+			data => this.onAllProjectsProvided(data),
+			error => console.log(error)
+		);
 	}
+	
+	onAllProjectsProvided( allProjects: BackendModelProjectItem[]) : void {
+		this.uiModelAllProjects = allProjects;
+	}
+	
 	
 	updateNavigationBar() : void {
 		let x:AdminNavbarBreadcrumbItem[] = []
