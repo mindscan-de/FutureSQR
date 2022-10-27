68d9a36fd0c04cbe31182d2731f2602d5a0c48a5
mindscan-de <maxim.gansert@gmail.com>
2022-10-27 22:23:14 +0200

    introduce authorization object - says what permissions current user has

---
+++ FutureSQR-Frontend/src/app/services/model/current-authorizations.spec.ts
@@ -0,0 +1,7 @@
+import { CurrentAuthorizations } from './current-authorizations';
+
+describe('CurrentAuthorizations', () => {
+  it('should create an instance', () => {
+    expect(new CurrentAuthorizations()).toBeTruthy();
+  });
+});
---
+++ FutureSQR-Frontend/src/app/services/model/current-authorizations.ts
@@ -0,0 +1,3 @@
+export class CurrentAuthorizations {
+	public isAdmin: boolean = false;
+}
