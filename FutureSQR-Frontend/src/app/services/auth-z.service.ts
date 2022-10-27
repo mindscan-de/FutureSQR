22358ab8025b35571f32a10dc457dec7578bb76a
mindscan-de <maxim.gansert@gmail.com>
2022-10-27 22:43:40 +0200

    add method For testing / to elevate to an admin user on demand.

--- FutureSQR-Frontend/src/app/services/auth-z.service.ts
+++ FutureSQR-Frontend/src/app/services/auth-z.service.ts
@@ -8,6 +8,9 @@ import { CurrentAuthorizations } from './model/current-authorizations';
 })
 export class AuthZService {
 	
+	// This service is only for components which need to be aware of changes of the user or his rights 
+	// (such as e.g. the top-navigation-bar)
+	
 	private _currentAuthorizationsValue:CurrentAuthorizations = new CurrentAuthorizations();
 	
 	private _currentAuthorizationsSubject: BehaviorSubject<CurrentAuthorizations>;
@@ -16,6 +19,13 @@ export class AuthZService {
 	constructor() { 
 		this._currentAuthorizationsSubject = new BehaviorSubject<CurrentAuthorizations>(this._currentAuthorizationsValue);
 		this.currentAuthorizationsSubject = this._currentAuthorizationsSubject.asObservable();
+	}
+	
+	setFakeAdmin():void {
+		let newAuthorizationValue:CurrentAuthorizations = new CurrentAuthorizations();
+		newAuthorizationValue.isAdmin = true;
+		this._currentAuthorizationsValue = newAuthorizationValue;
+		this._currentAuthorizationsSubject.next(this._currentAuthorizationsValue);
 	}
 	
 	asObservable(): Observable<CurrentAuthorizations> {
