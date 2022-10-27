8cbc6625ef6ddb215f0b2220dfc82b19639b8609
mindscan-de <maxim.gansert@gmail.com>
2022-10-27 22:25:01 +0200

    provide Service for authorization info provider.

--- FutureSQR-Frontend/src/app/services/auth-z.service.ts
+++ FutureSQR-Frontend/src/app/services/auth-z.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { CurrentAuthorizations } from './model/current-authorizations';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthZService {
+	
+	private _currentAuthorizationsValue:CurrentAuthorizations = new CurrentAuthorizations();
+	
+	private _currentAuthorizationsSubject: BehaviorSubject<CurrentAuthorizations>;
+	private currentAuthorizationsSubject: Observable<CurrentAuthorizations>;
 
-  constructor() { }
+	constructor() { 
+		this._currentAuthorizationsSubject = new BehaviorSubject<CurrentAuthorizations>(this._currentAuthorizationsValue);
+		this.currentAuthorizationsSubject = this._currentAuthorizationsSubject.asObservable();
+	}
+	
+	asObservable(): Observable<CurrentAuthorizations> {
+		return this.currentAuthorizationsSubject;
+	}
 }
