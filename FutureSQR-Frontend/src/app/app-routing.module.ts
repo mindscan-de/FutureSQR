b3ddf681f870190760457475d389ba609f848740
mindscan-de <maxim.gansert@gmail.com>
2022-10-28 23:36:44 +0200

    add router endpoint guard, such that each part requires a login first.

--- FutureSQR-Frontend/src/app/app-routing.module.ts
+++ FutureSQR-Frontend/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthNGuardService } from './authn/auth-n-guard.service';
+
 // Views
 import { MainPageComponent } from './views/main-page/main-page.component';
 import { AllProjectsPageComponent } from './views/all-projects-page/all-projects-page.component';
@@ -9,31 +11,33 @@ import { ProjectRecentReviewsPageComponent }  from './views/project-recent-revie
 import { SingleRevisionPageComponent } from './views/single-revision-page/single-revision-page.component';
 import { SingleReviewPageComponent } from './views/single-review-page/single-review-page.component';
 
+
 const routes: Routes = [
 	// show the main Page
-	{ path:'', pathMatch: 'full', component: MainPageComponent },
+	{ path:'', pathMatch: 'full', component: MainPageComponent, canActivate: [AuthNGuardService] },
 
 	// start the admin app.
-	{ path: 'admin', loadChildren: () => import('./adminapp/adminapp.module').then(m => m.AdminappModule) },
+	// WHERE TO AUTHN Guard?
+	{ path: 'admin', loadChildren: () => import('./adminapp/adminapp.module').then(m => m.AdminappModule), canActivate: [AuthNGuardService] },
 	
 	// show a list of all available projects for a logged on user
-	{ path:'allprojects', pathMatch: 'full', component: AllProjectsPageComponent },	
+	{ path:'allprojects', pathMatch: 'full', component: AllProjectsPageComponent, canActivate: [AuthNGuardService] },	
 
 	// show a list of all recent project reviews (open and maybe some recent closed)
-	{ path:':projectid/reviews', component:ProjectRecentReviewsPageComponent },
+	{ path:':projectid/reviews', component:ProjectRecentReviewsPageComponent, canActivate: [AuthNGuardService] },
 	
 	// show a review for a certain project
-	{ path:':projectid/review/:reviewid', component:SingleReviewPageComponent },
+	{ path:':projectid/review/:reviewid', component:SingleReviewPageComponent, canActivate: [AuthNGuardService] },
 	
 	// show a list of files which were changed for a single revision, and their diffs
 	// the action area should include some actions related to this particular revision
-	{ path:':projectid/revision/:revisionid', component: SingleRevisionPageComponent },
+	{ path:':projectid/revision/:revisionid', component: SingleRevisionPageComponent, canActivate: [AuthNGuardService] },
 	
 	// TODO: let the diffpage be about about the unified diff? e.g. download patch/diff file etc.
 	// :projectid/diff/:revisionid
 
 	// show the recent commits for a certain project
-	{ path:':projectid', component:ProjectRecentCommitsPageComponent },
+	{ path:':projectid', component:ProjectRecentCommitsPageComponent, canActivate: [AuthNGuardService] },
 	
 	// catch all other unknown URLs
 	{ path:'**',redirectTo:''}
