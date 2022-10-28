d231f4ec6c17e40fa7c2e81db4953fef06730df2
mindscan-de <maxim.gansert@gmail.com>
2022-10-28 23:23:33 +0200

    start with authn guard service to guard application urls

--- FutureSQR-Frontend/src/app/authn/auth-n-guard.service.ts
+++ FutureSQR-Frontend/src/app/authn/auth-n-guard.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthNGuardService {
+export class AuthNGuardService implements CanActivate {
 
-  constructor() { }
+	constructor(
+		private router: Router		
+	) { }
+	
+	canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot ) {
+		// We either return true, or false
+		// we return true, if the user is correctly authenticated
+		
+		// todo magic here....
+		
+		
+		// we return false, and route to the login page, when the user is not properly authenticated.
+		// this.router.navigate(['/','account','login'], { queryParams: {returnUrl : state.url} });
+		// return false;
+		
+		return true;
+	}
 }
