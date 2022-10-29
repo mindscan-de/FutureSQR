16d933a5af61bdf9e4e50ee1b8e722b3d69923b7
mindscan-de <maxim.gansert@gmail.com>
2022-10-29 20:20:04 +0200

    add some reminders, on what to do here:

--- FutureSQR-Frontend/src/app/authn/auth-n.service.ts
+++ FutureSQR-Frontend/src/app/authn/auth-n.service.ts
@@ -4,11 +4,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthNService {
-
+	
 	constructor() { }
 	
-	
+	// this will do a full login.
 	login( loginname: string, password: string, callbacks ):void {
-		
+		// TDOO: Use Backend to send password and username for authentication
+		// receive authorization and userdata
+		// deploy userdata
+		// deploy authorization data  
+		// 
 	}
+	
+	// TODO silent reauthentication e.g. on reload of the page, we need to retrieve the authn and authz data again
 }
