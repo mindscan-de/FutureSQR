135653b05d9f90fa3e88dcb6fd4a058172ef80f8
mindscan-de <maxim.gansert@gmail.com>
2022-10-29 20:24:55 +0200

    add some reminders on silent reauthentication

--- FutureSQR-Frontend/src/app/authn/auth-n.service.ts
+++ FutureSQR-Frontend/src/app/authn/auth-n.service.ts
@@ -13,8 +13,12 @@ export class AuthNService {
 		// receive authorization and userdata
 		// deploy userdata
 		// deploy authorization data  
-		// 
 	}
 	
-	// TODO silent reauthentication e.g. on reload of the page, we need to retrieve the authn and authz data again
+	// TODO silent reauthentication e.g. on reload of the page, we need to retrieve the authn and authz data again, 
+	// if that fails we have to inform someone, that the silent reauthentication did not worked, and the login page
+	// can be presented instead.
+	// on success we deploy userdata
+	// on success we deploy authoritzation data
+	// on success we inform authnguard via callback or so.
 }
