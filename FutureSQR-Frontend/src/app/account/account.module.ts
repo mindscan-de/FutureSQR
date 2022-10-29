3bb06c98b5719320b0c9f4b4276d8fb1a23b4a5e
mindscan-de <maxim.gansert@gmail.com>
2022-10-29 12:41:25 +0200

    add new module for account operations like login / register etc.

---
+++ FutureSQR-Frontend/src/app/account/account.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AccountRoutingModule } from './account-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AccountRoutingModule
+  ]
+})
+export class AccountModule { }
