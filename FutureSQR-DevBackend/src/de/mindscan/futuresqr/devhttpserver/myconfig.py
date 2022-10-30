9fd54ca68ec4ce1d4342282d28dd486e2d58c77e
mindscan-de <maxim.gansert@gmail.com>
2022-10-30 09:20:23 +0100

    add possibility to configure a system instance based on a profilename

---
+++ FutureSQR-DevBackend/src/de/mindscan/futuresqr/devhttpserver/myconfig.py
@@ -0,0 +1,40 @@
+'''
+Created on 30.10.2022
+
+MIT License
+
+Copyright (c) 2022 Maxim Gansert
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+@autor: Maxim Gansert
+'''
+
+# other confignames for development :
+# 'default'
+# 'mxm-dev'
+# 'rbt-dev-win'
+# 'rbt-dev-mac'
+# configure instances associated with the names:  
+# - default : systemInstance.7a15fa1f-7266-36b0-a0f1-22ab0f053829.json
+# - mxm-dev : systemInstance.b270794d-6c09-3f98-ad3a-0ad96dbcb528.json
+# - rbt-dev-win : systemInstance.5e3bb473-cf66-3a49-bf41-f75faea55a02.json
+# - rbt-dev-mac : systemInstance.1c964a67-f316-3b82-9dbc-d07fa315925c.json
+
+MY_CONFIGNAME = 'mxm-dev'
