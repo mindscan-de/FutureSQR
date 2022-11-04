bdaecec6174a0ab7a0be9ef07ef7e6e43354d47f
mindscan-de <maxim.gansert@gmail.com>
2022-11-04 19:21:22 +0100

    start with a authenticated sessions database.

---
+++ FutureSQR-DevBackend/src/de/mindscan/futuresqr/authnsession/session_database.py
@@ -0,0 +1,51 @@
+'''
+Created on 04.11.2022
+
+MIT License
+
+Copyright (c) 2022 Maxim Gansert
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+@autor: Maxim Gansert
+'''
+
+from de.mindscan.futuresqr.users.users_database import *  # @UnusedWildImport
+
+class SessionDatabase(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        self.__currentAuthenticatedUsers = {}
+        
+        
+    def userAuthenticationRegister(self, user):
+        self.__currentAuthenticatedUsers[user[USER_LOGON_NAME]] = user
+        
+    def userAuthenticationClear(self, user):
+        self.__currentAuthenticatedUsers.pop(user[USER_LOGON_NAME], None)
+
+    def isAuthenticationPresent(self, username):
+        return username in self.__currentAuthenticatedUsers
