e810c3f46a7e7ce8c8f62ac1e19732923d4a9bcd
mindscan-de <maxim.gansert@gmail.com>
2022-11-04 23:32:48 +0100

    add some reminders on the backend user data

--- FutureSQR-Frontend/src/app/authn/model/current-backend-user.ts
+++ FutureSQR-Frontend/src/app/authn/model/current-backend-user.ts
@@ -1,2 +1,8 @@
+// TODO align format, with user / user rights with all backends
+// python, springboot
+// this type contains the backend user info (login, uuid, displayname, etc. pp) 
+// and the current user permissions, such that the fontend can react to the
+// permissions in the backend (e.g. show admin links).
 export class CurrentBackendUser {
+	
 }
