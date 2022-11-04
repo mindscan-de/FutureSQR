d3689f17824cb4ba9b7449665af6bdf275d879fb
mindscan-de <maxim.gansert@gmail.com>
2022-11-04 23:29:35 +0100

    start with current (logged in) backend user type.

---
+++ FutureSQR-Frontend/src/app/authn/model/current-backend-user.spec.ts
@@ -0,0 +1,7 @@
+import { CurrentBackendUser } from './current-backend-user';
+
+describe('CurrentBackendUser', () => {
+  it('should create an instance', () => {
+    expect(new CurrentBackendUser()).toBeTruthy();
+  });
+});
