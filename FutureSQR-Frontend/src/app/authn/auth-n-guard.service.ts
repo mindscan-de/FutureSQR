7156fe62efdffde13719d8dc9eb8a285b6443197
mindscan-de <maxim.gansert@gmail.com>
2022-11-05 16:36:05 +0100

    first try with the reauthenication logic... doesn't work yet.

--- FutureSQR-Frontend/src/app/authn/auth-n-guard.service.ts
+++ FutureSQR-Frontend/src/app/authn/auth-n-guard.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AuthNService } from './auth-n.service';
+import { CurrentBackendUser } from './model/current-backend-user';
+import { CurrentBackendUserUtils } from './model/current-backend-user-utils';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthNGuardService implements CanActivate {
+	
+	private backendUserUtils = new CurrentBackendUserUtils();
 
 	constructor(
 		private authNService: AuthNService,
@@ -24,27 +28,47 @@ export class AuthNGuardService implements CanActivate {
 
 		// check first userlifecycle says that thelast known state was "logged in"
 		if(this.authNService.isUserLoggedIn()) {
-/*			
-
-			// if javascript lifecycle is new and authentication lifecycle is logged in user
-			// we want to reauthenticate - send the user to a waiting page (return false), and 
+			
+			// if javascript lifecycle is new  we want to reauthenticate - and maybe send the 
+			// user to a waiting page (return false), and 
 			// if user could be reauthenicated, and send him back to the url he actually wanted 
 			// to go, when the reauthenication was performed successfully. If the reauthenication 
-			// says no, we must send the user to the login page using this current state url.  
-			let foo = await this.authNService.reAuthenticateAsync();
+			// says no, we must send the user to the login page using this current state url.
+			let loginResult : boolean = false;
 			
-			// TODO check foo / if foo is not a user, then let the user to 
-			if (!foo) {
+			console.log("canactivate incoking reauthenticate async");
+			
+			// ------------------
+			// This is very likely not the right solution, because some of the details
+			// like distributing the backend user data is the job of authnservice.
+			// ------------------
+			let foo:Promise<CurrentBackendUser> = this.authNService.reAuthenticateAsync();
+			foo.then( user => {
+				console.log("this is the user data we got:");
+				console.log(user);
+				if(this.backendUserUtils.isValid(user)) {
+					console.log("we actually have a valid user.... for reauthenticate....");
+					loginResult = true;
+					// TODO: remember that the authNservice must distribute reauthenticated user.
+					// TODO:  
+				}
+				else
+				{
+					loginResult = false;
+				}
+			});
+			
+			// TODO: auf das ergebnis des Promise warten....
+			
+			if (!loginResult) {
 				// this is not a user any more from the servers pov, therefore
 				// this user must login
+				console.log("let's send them to login because reauthenticate failed.");
 				this.router.navigate(['/','account','login'], { queryParams: {returnUrl : state.url} });
-				return false;
 			}
-			else
-			{
-				// we now must setup such we don't make this expensive call...'
-			}
-*/		}
+			
+			return loginResult;
+		}
 		else
 		{
 			// user is not logged in, or is logged out
