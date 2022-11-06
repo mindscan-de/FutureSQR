e8297d937bc85fd37c04e60b20846c04daab1b8d
mindscan-de <maxim.gansert@gmail.com>
2022-11-06 08:55:32 +0100

    subscribe authz service to authn services live backend user data.

--- FutureSQR-Frontend/src/app/authz/auth-z.service.ts
+++ FutureSQR-Frontend/src/app/authz/auth-z.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { AuthNService } from '../authn/auth-n.service';
 
 import { CurrentAuthorizations } from './model/current-authorizations';
+import { CurrentBackendUser } from '../authn/model/current-backend-user';
 
 @Injectable({
   providedIn: 'root'
@@ -24,15 +25,27 @@ export class AuthZService {
 		this._currentAuthorizationsSubject = new BehaviorSubject<CurrentAuthorizations>(this._currentAuthorizationsValue);
 		this.currentAuthorizationsSubject = this._currentAuthorizationsSubject.asObservable();
 		
-		this.authNService.liveBackendUserData().subscribe(
-			updatedUserData => {
-				// TODO: transform data to UI Type format 
-				// m2m : CurrentBackendUser -> CurrentAuthorizations
-				// then send via next...
+		this.authNService.liveBackendUserData().subscribe({
+			next: data => {
+				this.setCurrentPrivileges(this.m2mUserTransform(data));
 			},
-		);
+		});
 	}
 	
+	m2mUserTransform(backendUser:CurrentBackendUser): CurrentAuthorizations {
+		let newPrivileges = new CurrentAuthorizations();
+		if(backendUser.capabilities != undefined) {
+			newPrivileges.isAdmin = backendUser.capabilities.includes('admin');
+		}
+		return newPrivileges;
+	}
+	
+	setCurrentPrivileges(privileges:CurrentAuthorizations ): void {
+		this._currentAuthorizationsValue = privileges;
+		this._currentAuthorizationsSubject.next(this._currentAuthorizationsValue);
+	}
+	
+	// TODO: deprecated
 	setFakeAdmin():void {
 		let newAuthorizationValue:CurrentAuthorizations = new CurrentAuthorizations();
 		newAuthorizationValue.isAdmin = true;
@@ -40,6 +53,7 @@ export class AuthZService {
 		this._currentAuthorizationsSubject.next(this._currentAuthorizationsValue);
 	}
 	
+	// TODO: deprecated
 	clearFakeAdmin():void {
 		let newAuthorizationValue:CurrentAuthorizations = new CurrentAuthorizations();
 		newAuthorizationValue.isAdmin = false;
@@ -47,6 +61,7 @@ export class AuthZService {
 		this._currentAuthorizationsSubject.next(this._currentAuthorizationsValue);
 	}
 	
+	// TODO rename this to liveUserAuthorizations
 	asObservable(): Observable<CurrentAuthorizations> {
 		return this.currentAuthorizationsSubject;
 	}
