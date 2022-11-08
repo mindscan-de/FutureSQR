659d2c9bc5a35af0f95828f82d649f6d84f5e4ee
Robert Breunung <rbreunung@gmail.com>
2022-11-09 00:11:57 +0100

    improve the auth guard a bit by returning the URL

--- FutureSQR-Frontend/src/app/authn/auth-n-guard.service.ts
+++ FutureSQR-Frontend/src/app/authn/auth-n-guard.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 
 import { AuthNService } from './auth-n.service';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AuthNGuardService implements CanActivate {
-	
+
 	constructor(
 		private authNService: AuthNService,
 		private router: Router
 	) { }
-	
+
 	canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot ) {
 		// if javascript lifecylce is authenticted and authentcation lifecycle is user logged in
 		if(this.authNService.isAuthenticatedInCurrentLifecycle()) {
@@ -22,17 +22,22 @@ export class AuthNGuardService implements CanActivate {
 		// check first userlifecycle says that thelast known state was "logged in"
 		if(!this.authNService.isUserLoggedIn()) {
 			// user is not logged in, or is logged out
-			this.router.navigate(['/','account','login'], { queryParams: {returnUrl : state.url} });
-			return false;
+			return this.getLoginUrl(state);
 		}
-		
+
 		// let silently reauthenticate
-		let isReauthenticatedPromise = this.authNService.reauthenticate({
-			failed: () => {
-					this.router.navigate(['/','account','login'], { queryParams: {returnUrl : state.url} });
-				}
-			});
-			
-		return isReauthenticatedPromise;
+		let isReauthenticatedPromise = this.authNService.reauthenticate();
+
+		return isReauthenticatedPromise.then(
+			isReauth => isReauth ? true : this.getLoginUrl(state),
+			reason => {
+				console.error(reason);
+				return this.getLoginUrl(state)
+			}
+		);
+	}
+
+	private getLoginUrl(state: RouterStateSnapshot): UrlTree {
+		return this.router.createUrlTree(['/', 'account', 'login'], { queryParams: { returnUrl: state.url } })
 	}
 }
--- FutureSQR-Frontend/src/app/authn/auth-n.service.ts
+++ FutureSQR-Frontend/src/app/authn/auth-n.service.ts
@@ -11,25 +11,25 @@ import { CurrentBackendUser } from './model/current-backend-user';
 
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AuthNService {
 	private static readonly LS_PREFIX = "futuresquare:ls:";
-	
+
 	// URL to visit to retrieve a preauth token, to be able to post to AUTHENTICATE / REAUTHENTICATE
 	private static readonly URL_PREAUTH_TOKEN          = "/FutureSQR/rest/user/preauthtoken";
-	
+
 	private static readonly URL_LOGIN_AUTHENTICATE    = "/FutureSQR/rest/user/authenticate";
 	private static readonly URL_LOGIN_REAUTHENTICATE  = "/FutureSQR/rest/user/reauthenticate";
 	private static readonly URL_LOGOUT                = "/FutureSQR/rest/user/logout";
-	
+
 	// LOCALSTORAGE KEY for the current user authentication lifecycle state
 	private static readonly LS_KEY_CURRENT_USER_AUTH_LIFECYCLE_STATE = AuthNService.LS_PREFIX + "currentUserAuthLifecycleState";
 	// TODO: LOCALSTORAGE KEY for the current user authentication lifecylce state
 	private static readonly LS_KEY_CURRENT_BACKEND_USER_VALUE = AuthNService.LS_PREFIX + "currentBackendUserValue";
 	// TODO: LOCALSTORARE KEY for previous user
 	private static readonly LS_KEY_LATEST_LOGGEDIN_USER_VALUE = AuthNService.LS_PREFIX + "previousLoggedInBackendUserValue";
-	
+
 	// Actually we have to deal with two different life cycles
 	// this one only lives as a variable in memory
 	// Javascript lifecycle (e.g. F5 (pagereload) or bookmarks)
@@ -37,26 +37,26 @@ export class AuthNService {
 	// Authentication lifecycle, which can span multiple javascript lifecycles
 	// this one lives in the localstorage
 	private __currentUserAuthLifecycleState: UserAuthLifecycleState;
-	
+
 	// We will keep a copy od the current Backend user in AuthNService.
 	private _currentBackendUserValue = new CurrentBackendUser();
 	private _currentBackendUserSubject: BehaviorSubject<CurrentBackendUser>;
 	private currentBackendUserSubject: Observable<CurrentBackendUser>;
-	
+
 	private backendUserUtils = new CurrentBackendUserUtils();
-	
+
 	constructor(
 		private httpClient : HttpClient		
-	) { 
+	) {
 		this._currentBackendUserSubject = new BehaviorSubject<any>(this._currentBackendUserValue);
 		this.currentBackendUserSubject = this._currentBackendUserSubject.asObservable();
-		
+
 		this.updateBrowserAuthLifecylceState( BrowserAuthLifecycleState.None);
 		this.updateUserAuthLifecycleState(
-			JSON.parse(localStorage.getItem(AuthNService.LS_KEY_CURRENT_USER_AUTH_LIFECYCLE_STATE)) 
+			JSON.parse(localStorage.getItem(AuthNService.LS_KEY_CURRENT_USER_AUTH_LIFECYCLE_STATE))
 			|| UserAuthLifecycleState.None);
 	}
-	
+
 	// this will do a full login using username and password.
 	login( loginname: string, password: string, callbacks ):void {
 		// TODO: maybe we need to aquire a pre-auth authentication crsf token to send our login
@@ -64,20 +64,20 @@ export class AuthNService {
 		// we only need to aquire this token, if we are in __currentBrowserAuthLifeCycleState None
 		// we have this pre-auth token, if we are at __currentBrowserAuthLifeCycleState PreAuthenticated
 
-		
+
 		// a login attempt will kill the previous lifecylce state
 		if(this.isUserLoggedIn()) {
 			this.updateUserAuthLifecycleState(UserAuthLifecycleState.None);
 		}
-		
+
 		let formData = new FormData();
-		
+
 		formData.set('loginname', loginname);
 		formData.set('password', password);
 		// TODO also present: some kind of pre auth crsf tokens?
-		
+
 		let that = this;
-		
+
 		this.httpClient.post<CurrentBackendUser>(AuthNService.URL_LOGIN_AUTHENTICATE, formData).pipe(first()).subscribe(
 			data => {
 				that.loginOnDataReceived(data);
@@ -100,85 +100,76 @@ export class AuthNService {
 						console.error(error);
 					}
 				}
-			}			
+			}
 		);
 	}
-	
+
 	loginOnDataReceived(newBackendUser:CurrentBackendUser):  void {
 		if(!this.backendUserUtils.isValid(newBackendUser)) {
 			this.updateUserAuthLifecycleState(UserAuthLifecycleState.None);
 			this.updateBrowserAuthLifecylceState(BrowserAuthLifecycleState.None);
-			
+
 			// update with empty backend user
 			this.updateCurrentBackendUser(new CurrentBackendUser());
-			
+
 			return;
 		}
-	
+
 		this.updateUserAuthLifecycleState(UserAuthLifecycleState.LoggedIn);
 		this.updateBrowserAuthLifecylceState(BrowserAuthLifecycleState.FullyAuthenticated);
-		
+
 		this.updateCurrentBackendUser(newBackendUser);
 		this.updateLatestLoggedInUser(newBackendUser);
 	}
-	
-	
+
+
 	loginOnError(): void {
 		// we received some error code from the server, so what to do in this case?
-		
+
 		// TODO: set currentUserAuthLifeCycleState to UserAuthLifecycleState.None
 	}
-	
-	reauthenticate(callbacks): Promise<boolean> {
+
+	reauthenticate(): Promise<boolean> {
 		let url = AuthNService.URL_LOGIN_REAUTHENTICATE
 
 		let formData = new FormData();
-		
+
 		formData.set("assumedusername", this.getLatestLoggedInUser());
-		
+
 		let reauthenticateResult = this.httpClient
-									.post<CurrentBackendUser>(url, formData)
-									.pipe(first())
+			.post<CurrentBackendUser>(url, formData)
+			.pipe(first())
 									.pipe(map( (newBackendUser:CurrentBackendUser) => {
-											let successResult: boolean = false;
-											 
+				let successResult: boolean = false;
+
 											if(this.backendUserUtils.isValid(newBackendUser)) {
 												this.updateBrowserAuthLifecylceState( BrowserAuthLifecycleState.ReAuthenticated );
 												this.updateUserAuthLifecycleState( UserAuthLifecycleState.LoggedIn );
-												this.updateCurrentBackendUser(newBackendUser);
-												
-												successResult = true;
-												
-												// TODO invoke success callback.
-											} 
-											else {
+					this.updateCurrentBackendUser(newBackendUser);
+
+					successResult = true;
+
+					// TODO invoke success callback.
+				}
+				else {
 												this.updateBrowserAuthLifecylceState( BrowserAuthLifecycleState.PreAuthenicated );
 												this.updateUserAuthLifecycleState( UserAuthLifecycleState.None );
-												this.updateCurrentBackendUser(newBackendUser);
-												
-												successResult = false;
-												
-												if(callbacks.failed != undefined) {
-													try {
-														callbacks.failed();
-													}
-													catch(error) {
-														console.error(error);
-													}
-												}
-											}
-											
-											return successResult;
-										}
-									)).toPromise();
+					this.updateCurrentBackendUser(newBackendUser);
+
+					successResult = false;
+				}
+
+				return successResult;
+			}
+			)).toPromise();
 		return reauthenticateResult;
 	}
-	
+
 	logout(callback):void {
 		if(this.isUserLoggedIn()) { 
 			let formData:FormData = new FormData();
 			formData.set( 'username', this._currentBackendUserValue.loginname); 
-			
+
 			this.httpClient.post<any>(AuthNService.URL_LOGOUT, formData).pipe(first()).subscribe(
 				data => {
 					if(callback.onlogout != undefined) {
@@ -193,14 +184,14 @@ export class AuthNService {
 				error => {}
 			);
 		}
-		
+
 		this.updateBrowserAuthLifecylceState( BrowserAuthLifecycleState.None );
 		this.updateUserAuthLifecycleState( UserAuthLifecycleState.LoggedOut );
 		this.updateCurrentBackendUser(new CurrentBackendUser());
 	}
 
-	
-	
+
+
 	isAuthenticatedInCurrentLifecycle() : boolean {
 		if(this.__currentUserAuthLifecycleState == UserAuthLifecycleState.LoggedIn) {
 			if(	(this.__currentBrowserAuthLifeCycleState === BrowserAuthLifecycleState.FullyAuthenticated) ||
@@ -211,36 +202,36 @@ export class AuthNService {
 
 		return false;
 	}
-	
-    isUserLoggedIn() {
-        return this.__currentUserAuthLifecycleState == UserAuthLifecycleState.LoggedIn;
-    }
-	
-	
+
+	isUserLoggedIn() {
+		return this.__currentUserAuthLifecycleState == UserAuthLifecycleState.LoggedIn;
+	}
+
+
 	liveBackendUserData():Observable<CurrentBackendUser> {
 		return this.currentBackendUserSubject;
 	}
-	
-	
+
+
 	private updateUserAuthLifecycleState(newState: UserAuthLifecycleState): void {
 		this.__currentUserAuthLifecycleState = newState;
-		
+
 		localStorage.setItem(AuthNService.LS_KEY_CURRENT_USER_AUTH_LIFECYCLE_STATE, JSON.stringify(this.__currentUserAuthLifecycleState))
 	}
-	
+
 	private updateBrowserAuthLifecylceState(newState: BrowserAuthLifecycleState) : void {
 		this.__currentBrowserAuthLifeCycleState = newState;
 	}
-	
+
 	private updateCurrentBackendUser(newUser: CurrentBackendUser): void  {
 		this._currentBackendUserValue = newUser;
 		this._currentBackendUserSubject.next(this._currentBackendUserValue);
 	}
-	
+
 	private updateLatestLoggedInUser(newUser: CurrentBackendUser): void {
 		localStorage.setItem(AuthNService.LS_KEY_LATEST_LOGGEDIN_USER_VALUE, JSON.stringify(newUser.loginname));
 	}
-	
+
 	getLatestLoggedInUser(): string {
 		return JSON.parse(localStorage.getItem(AuthNService.LS_KEY_LATEST_LOGGEDIN_USER_VALUE))	|| "";
 	}
