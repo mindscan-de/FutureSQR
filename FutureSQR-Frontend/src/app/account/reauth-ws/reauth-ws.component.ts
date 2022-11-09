fb9e42f2315e9efc0d924a62ac2a438b0b6ea74e
Robert Breunung <rbreunung@gmail.com>
2022-11-09 23:15:17 +0100

    WS implementation
    - including some entertaining debugging information to read the token

--- FutureSQR-Frontend/src/app/account/reauth-ws/reauth-ws.component.ts
+++ FutureSQR-Frontend/src/app/account/reauth-ws/reauth-ws.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpXsrfTokenExtractor } from '@angular/common/http';
+import { first } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-reauth-ws',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReauthWsComponent implements OnInit {
 
-  constructor() { }
+  private returnUrl: string
 
-  ngOnInit(): void {
+  token: string | null = null
+  tokenDefinition: CsrfToken | null = null
+
+  constructor(extraktor: HttpXsrfTokenExtractor, private http: HttpClient, private router: Router, route: ActivatedRoute) {
+    this.returnUrl = route.snapshot.queryParams['returnUrl'] || '/';
+
+    this.token = extraktor.getToken()
   }
 
+  ngOnInit(): void {
+    this.http.get<CsrfToken>("/FutureSQR/rest/login/csrf").pipe(first()).subscribe({
+      next: n => { this.tokenDefinition = n },
+      // timeout is only for debugging purpose and may be removed later
+      complete: () => { setTimeout(() => this.router.navigate(['/', 'account', 'login'], { queryParams: { returnUrl: this.returnUrl } }), 3000) }
+    })
+  }
+}
+
+interface CsrfToken {
+  headerName: String
+  parameterName: String
+  token: String
 }
