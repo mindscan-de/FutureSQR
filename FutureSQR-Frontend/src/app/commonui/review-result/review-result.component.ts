1c09619fb485f1e5144a21051dc69e5bcd467ed0
mindscan-de <maxim.gansert@gmail.com>
2022-11-12 10:37:51 +0100

    show displayname and avatar in review result.

--- FutureSQR-Frontend/src/app/commonui/review-result/review-result.component.ts
+++ FutureSQR-Frontend/src/app/commonui/review-result/review-result.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 
+
+import { UserLookupService }  from '../../uiservices/user-lookup.service';
+import { UiUser } from '../../uiservices/model/ui-user';
+
 import { BackendModelReviewResult } from '../../backend/model/backend-model-review-result';
+
 
 @Component({
   selector: 'app-review-result',
@@ -9,11 +14,24 @@ import { BackendModelReviewResult } from '../../backend/model/backend-model-revi
 })
 export class ReviewResultComponent implements OnInit {
 
+	public userInfo: UiUser;
 	@Input() reviewState:BackendModelReviewResult = new BackendModelReviewResult();
 
-	constructor() { }
+	constructor(
+		private userLookupService : UserLookupService
+	) { 
+		this.userInfo = this.userLookupService.unknown();
+	}
 
 	ngOnInit(): void {
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		if(changes.reviewState != undefined) {
+			let reviewResult:BackendModelReviewResult = changes.reviewState.currentValue;
+			
+			this.userInfo = this.userLookupService.lookup(reviewResult.reviewer_id);
+		}
 	}
 
 }
