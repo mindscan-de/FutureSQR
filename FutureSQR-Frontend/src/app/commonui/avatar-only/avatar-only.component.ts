d48127e1c13f7e4ce32a7eeddc828aef655476d4
mindscan-de <maxim.gansert@gmail.com>
2022-11-13 15:01:41 +0100

    implement avatar only component - to be used in recent commits.

--- FutureSQR-Frontend/src/app/commonui/avatar-only/avatar-only.component.ts
+++ FutureSQR-Frontend/src/app/commonui/avatar-only/avatar-only.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+
+import { UserLookupService }  from '../../uiservices/user-lookup.service';
+
 
 @Component({
   selector: 'app-avatar-only',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./avatar-only.component.css']
 })
 export class AvatarOnlyComponent implements OnInit {
+	
+	public avatarLocation: string = "";
+	public avatarDisplayName: string = "";
+	
+	@Input() url: string = "";
+	@Input() uuid: string = "";
+	@Input() size: number = 28;
 
-  constructor() { }
+	constructor(
+		private userLookupService : UserLookupService
+	) { 
+		this.avatarLocation = this.userLookupService.unknown().avatarLocation;
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		console.log("ngOnchanges");
+		console.log(changes);
+		if(changes.uuid != undefined) {
+			
+			let uuid:string = changes.uuid.currentValue;
+			let userInfo = this.userLookupService.lookup(uuid);
+			
+			this.avatarLocation = userInfo.avatarLocation;
+			this.avatarDisplayName = userInfo.displayName;
+		}
+		else if (changes.url != undefined) {
+			
+			this.avatarLocation= changes.url.currentValue;
+		}
+	}
 
 }
