6440ce74f8858e2e0746b16217b5cd4fe4e3f5d2
mindscan-de <maxim.gansert@gmail.com>
2022-11-14 18:17:46 +0100

    implement simple admin avatar solution - maybe needs better solution

--- FutureSQR-Frontend/src/app/adminapp/commonui/adminapp-avatar-only/adminapp-avatar-only.component.ts
+++ FutureSQR-Frontend/src/app/adminapp/commonui/adminapp-avatar-only/adminapp-avatar-only.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+
+import { UserLookupService } from '../../../uiservices/user-lookup.service';
+
 
 @Component({
   selector: 'app-adminapp-avatar-only',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adminapp-avatar-only.component.css']
 })
 export class AdminappAvatarOnlyComponent implements OnInit {
+	
+	public avatarLocation: string = "";
+	public avatarDisplayName: string = "";
 
-  constructor() { }
+	@Input() uuid: string = "";
+	@Input() size: number = 28; 
 
-  ngOnInit(): void {
-  }
+	constructor(
+		private userLookupService : UserLookupService,	
+	) { 
+		this.avatarLocation = this.userLookupService.unknown().avatarLocation;		
+	}
+
+	ngOnInit(): void {
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		console.log("ngOnchanges");
+		console.log(changes);
+		if(changes.uuid != undefined) {
+			
+			let uuid:string = changes.uuid.currentValue;
+			let userInfo = this.userLookupService.lookup(uuid);
+			
+			this.avatarLocation = userInfo.avatarLocation;
+			this.avatarDisplayName = userInfo.displayName;
+		}
+		
+	}
 
 }
