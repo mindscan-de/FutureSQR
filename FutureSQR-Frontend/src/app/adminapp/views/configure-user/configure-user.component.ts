1d0f8d691f95572909f0398dada46ce1571a4585
Robert Breunung <rbreunung@gmail.com>
2022-11-16 08:21:43 +0100

    white space and cleanup

--- FutureSQR-Frontend/src/app/adminapp/views/configure-user/configure-user.component.ts
+++ FutureSQR-Frontend/src/app/adminapp/views/configure-user/configure-user.component.ts
@@ -4,25 +4,24 @@ import { AdminBackendModelSimpleUserItem } from '../../backend/model/admin-backe
 import { AdminDataQueryBackendService } from '../../backend/services/admin-data-query-backend.service';
 
 // Admin App Services
-import { AdminNavigationBarService }  from '../../services/admin-navigation-bar.service';
+import { AdminNavigationBarService } from '../../services/admin-navigation-bar.service';
 import { AdminNavbarBreadcrumbItem } from '../../services/model/admin-navbar-breadcrumb-item';
 
 
 @Component({
-  selector: 'app-configure-user',
-  templateUrl: './configure-user.component.html',
-  styleUrls: ['./configure-user.component.css']
+	selector: 'app-configure-user',
+	templateUrl: './configure-user.component.html',
+	styleUrls: ['./configure-user.component.css']
 })
 export class ConfigureUserComponent implements OnInit {
-	
+
 	public activeUserID: string = '';
 	public activeUser?: AdminBackendModelSimpleUserItem;
 
 	constructor(
 		private adminNavigationBarService : AdminNavigationBarService,
 		private adminDataQueryBackend : AdminDataQueryBackendService,
-		private route: ActivatedRoute, 
-		private router: Router
+		private route : ActivatedRoute,
 	) { }
 
 	ngOnInit(): void {
@@ -31,30 +30,30 @@ export class ConfigureUserComponent implements OnInit {
 		this.updateNavigationBar(this.activeUserID);
 
 		this.adminDataQueryBackend.getAdminUserList().subscribe(
-			data => {this.onAccountListProvided(data)},
-			error => {}
-		);		
+			data => { this.onAccountListProvided(data) },
+			error => { }
+		);
 	}
 
-	private onAccountListProvided( userlist: AdminBackendModelSimpleUserItem[]): void {
-		let uiModelSimpleUserlist = this.m2mTransform(userlist);
-		this.activeUser = uiModelSimpleUserlist.find(user => user.uuid === this.activeUserID)
-
-		if (this.activeUser) {
+	private onAccountListProvided(userlist: AdminBackendModelSimpleUserItem[]): void {
+		let user = userlist.find(user => user.uuid === this.activeUserID)
+		
+		if (user) {
+			this.activeUser = this.m2mTransform(user);
 			this.updateNavigationBar(this.activeUser.displayname)
 		}
 	}
-	
-	private m2mTransform(input: AdminBackendModelSimpleUserItem[]): AdminBackendModelSimpleUserItem[] {
+
+	private m2mTransform(input: AdminBackendModelSimpleUserItem): AdminBackendModelSimpleUserItem {
 		return input;
 	}
 
-	updateNavigationBar(username : string) : void {
-		let x:AdminNavbarBreadcrumbItem[] = []
-		
+	private updateNavigationBar(username: string): void {
+		let x: AdminNavbarBreadcrumbItem[] = []
+
 		x.push(new AdminNavbarBreadcrumbItem('users', ['users'], false));
-		x.push(new AdminNavbarBreadcrumbItem(username, ['user',this.activeUserID], true));
-		 
+		x.push(new AdminNavbarBreadcrumbItem(username, ['user', this.activeUserID], true));
+
 		this.adminNavigationBarService.setAdminBreadCrumbNavigation(x);
 	}
 
