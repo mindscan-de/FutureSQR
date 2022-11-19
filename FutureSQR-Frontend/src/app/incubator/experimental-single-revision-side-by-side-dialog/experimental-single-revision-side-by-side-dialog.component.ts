119c160dc4dda203f7f83a9efd260a5606da3100
mindscan-de <maxim.gansert@gmail.com>
2022-11-19 18:35:02 +0100

    duplicate SingleRevisionSideBySideDialog for experimental diff.

--- FutureSQR-Frontend/src/app/incubator/experimental-single-revision-side-by-side-dialog/experimental-single-revision-side-by-side-dialog.component.ts
+++ FutureSQR-Frontend/src/app/incubator/experimental-single-revision-side-by-side-dialog/experimental-single-revision-side-by-side-dialog.component.ts
@@ -11,6 +11,19 @@ import { BackendModelSingleCommitFileChangeSet } from '../../backend/model/backe
   styleUrls: ['./experimental-single-revision-side-by-side-dialog.component.css']
 })
 export class ExperimentalSingleRevisionSideBySideDialogComponent implements OnInit {
+	
+	private allUiFileChangeSets: BackendModelSingleCommitFileChangeSet[] = [];
+	private allUiFileChangeSetsProvided: boolean = false;
+	
+	public  currentUiFileChangeSet: BackendModelSingleCommitFileChangeSet = new BackendModelSingleCommitFileChangeSet();
+	private currentUiFileChangeSetProvided: boolean = false;
+	
+	public numberOfFiles: number = 0;
+	private currentShownFileIndex = 0;
+	public currentShownFileUiIndex = 0;
+	public nextEnabled = false;
+	public prevEnabled = false;
+	
 
 	constructor(
 		public activeModal: NgbActiveModal
@@ -23,4 +36,59 @@ export class ExperimentalSingleRevisionSideBySideDialogComponent implements OnIn
 		this.activeModal.close(null)
 	}
 
+	setAllChangeSets(allFileChangeSets:BackendModelSingleCommitFileChangeSet[]): void {
+		this.numberOfFiles = allFileChangeSets.length;
+		this.allUiFileChangeSets = allFileChangeSets;
+		this.allUiFileChangeSetsProvided = true;
+		this.updateFileNavigationValues();
+	}
+	
+	setSelectedFileChangeSet(newFileChangeSet:BackendModelSingleCommitFileChangeSet) : void {
+		this.currentUiFileChangeSet = newFileChangeSet;
+		this.currentUiFileChangeSetProvided = true;
+		this.updateFileNavigationValues();
+	}
+	
+	onNavLeftClicked() : void {
+		if( this.currentShownFileIndex <= 0) {
+			return;
+		}
+		
+		this.currentUiFileChangeSet = this.allUiFileChangeSets[this.currentShownFileIndex-1];
+		
+		this.updateFileNavigationValues();
+	}
+	
+	onNavRightClicked() : void {
+		if( this.currentShownFileIndex >= this.numberOfFiles -1) {
+			return;
+		}
+		
+		this.currentUiFileChangeSet = this.allUiFileChangeSets[this.currentShownFileIndex+1];
+		
+		this.updateFileNavigationValues();		
+	}
+	
+	private updateFileNavigationValues() : void {
+		if(!this.allUiFileChangeSetsProvided) {
+			return;
+		}
+		if(!this.currentUiFileChangeSetProvided) {
+			return;
+		}
+		
+		this.currentShownFileIndex = 0;
+		this.currentShownFileUiIndex = 0;
+		for(let i:number=0;i<this.allUiFileChangeSets.length;i++) {
+			if(this.allUiFileChangeSets[i]==this.currentUiFileChangeSet) {
+				this.currentShownFileIndex = i;
+				this.currentShownFileUiIndex = i+1;
+				
+				this.nextEnabled = this.currentShownFileIndex < this.numberOfFiles-1;
+				this.prevEnabled = this.currentShownFileIndex > 0;
+				 
+				return;
+			}
+		}
+	}
 }
