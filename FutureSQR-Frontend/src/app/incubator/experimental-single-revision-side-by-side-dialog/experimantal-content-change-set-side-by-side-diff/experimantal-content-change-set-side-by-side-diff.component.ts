f3f1df90796c6332d21d81f183c04e4ecd23d03a
mindscan-de <maxim.gansert@gmail.com>
2022-11-19 19:22:51 +0100

    implement+integrate experimental content change set side by side diff

--- FutureSQR-Frontend/src/app/incubator/experimental-single-revision-side-by-side-dialog/experimantal-content-change-set-side-by-side-diff/experimantal-content-change-set-side-by-side-diff.component.ts
+++ FutureSQR-Frontend/src/app/incubator/experimental-single-revision-side-by-side-dialog/experimantal-content-change-set-side-by-side-diff/experimantal-content-change-set-side-by-side-diff.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+
+// ui model
+import { UiDiffContentModel } from '../../../commonui/content-change-set-side-by-side-diff/uimodel/ui-diff-content-model';
+
 
 @Component({
   selector: 'app-experimantal-content-change-set-side-by-side-diff',
@@ -6,10 +10,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experimantal-content-change-set-side-by-side-diff.component.css']
 })
 export class ExperimantalContentChangeSetSideBySideDiffComponent implements OnInit {
+	
+	public leftContent : UiDiffContentModel = new UiDiffContentModel("",1); 
+	public rightContent : UiDiffContentModel = new UiDiffContentModel("",1);
+	
+	// make the editor readonly
+	public readOnly:boolean = true;
+	public viewPortMargin:number = 1;
+	
+	// TODO: create a ui model from it
+	// actually this will an intermediate external model
+	@Input() contentChangeSet:string[] =[];
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+	
+ 	ngOnChanges(changes: SimpleChanges): void {
+	
+		if(changes.contentChangeSet != undefined) {
+			let contentChangeSetCurrent:string[] = changes.contentChangeSet.currentValue;
+			// This needs to be reworked such that the line numbers are correctly transferred.
+			this.leftContent = this.filterLeftDiff(contentChangeSetCurrent, 12);
+			this.rightContent = this.filterRightDiff(contentChangeSetCurrent, 15)
+		}
+	}
+	
+	private filterLeftDiff(linediff: string[], left_line_count_start: number) : UiDiffContentModel {
+		let leftdiff = linediff.filter(line => !line.startsWith("+")).join("\n");
+		
+		let result:UiDiffContentModel = new UiDiffContentModel(leftdiff, left_line_count_start);
+		
+		return result; 
+	}
+	
+	private filterRightDiff(linediff: string[], right_line_count_start: number) : UiDiffContentModel {
+		let rightdiff = linediff.filter(line => !line.startsWith("-")).join("\n");
+		
+		let result:UiDiffContentModel = new UiDiffContentModel(rightdiff,right_line_count_start);
+		
+		return result; 
+	}
+	
+	
 
 }
