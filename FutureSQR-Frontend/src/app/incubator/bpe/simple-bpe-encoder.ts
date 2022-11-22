c97b4d2c3c8b6160f64463a07dc28443bc63622a
mindscan-de <maxim.gansert@gmail.com>
2022-11-22 19:12:46 +0100

    add some more information about where i already solved this.

--- FutureSQR-Frontend/src/app/incubator/bpe/simple-bpe-encoder.ts
+++ FutureSQR-Frontend/src/app/incubator/bpe/simple-bpe-encoder.ts
@@ -6,6 +6,11 @@
  * i think i will use them for testing - since i wrote that in python, i
  * should go for a jupyter notebook first and then and only then for a
  * javascript implementation, which is also temporary?
+ * you can find the 10k token BPE dataset in the FluentGenesis-Classifier
+ * project and also an encoder for this. ( which is actually quite slow... )
+ * but i would say, it is god enough right now.
+ * 
+ * time for some experiments offline.... 
  */ 
 export class SimpleBPEEncoder {
 }
