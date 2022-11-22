9d9eba2532e6ebf8cbb85c8f800175d9ee8553e9
mindscan-de <maxim.gansert@gmail.com>
2022-11-22 18:57:56 +0100

    start with a BPE idea, to calculate diffs.

---
+++ FutureSQR-Frontend/src/app/incubator/bpe/simple-bpe-encoder.spec.ts
@@ -0,0 +1,7 @@
+import { SimpleBpeEncoder } from './simple-bpe-encoder';
+
+describe('SimpleBpeEncoder', () => {
+  it('should create an instance', () => {
+    expect(new SimpleBpeEncoder()).toBeTruthy();
+  });
+});
---
+++ FutureSQR-Frontend/src/app/incubator/bpe/simple-bpe-encoder.ts
@@ -0,0 +1,11 @@
+/*
+ * For the diff I would like to test, whether building the diff via 
+ * BytePairEncoding is able to provide some readability.  
+ *
+ * I calculated some BPE tables for a general java corpus, some time back.
+ * i think i will use them for testing - since i wrote that in python, i
+ * should go for a jupyter notebook first and then and only then for a
+ * javascript implementation, which is also temporary?
+ */ 
+export class SimpleBPEEncoder {
+}
