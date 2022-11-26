3d14eefa5820d5f8b4a2dd52b7cf0a587f559ec7
mindscan-de <maxim.gansert@gmail.com>
2022-11-26 18:39:11 +0100

    add some transition destails, and outlook how this datatype is extended

--- FutureSQR-Frontend/src/app/backend/model/backend-model-project-simple-information.ts
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-project-simple-information.ts
@@ -1,7 +1,16 @@
 export class BackendModelProjectSimpleInformation {
+	// a project should have a UUID - e.g for administration purposes and internal DB purposes.
+	public projectUUID: string ="";
+	
 	public reviewPrefix: string = "";
+	// basically this is the projectShortName of the project, it should be fixed once it is set
+	// becasue references can exists for 10 or more years.
 	public projectID: string = "";
+	// prepare transition to project shortname.
+	public projectShortName: string = "";
+	
 	public projectDisplayName: string = "";
 	public projectDescription: string = "";
+	// Obviously this is a property calculated from current user.
 	public projectIsStarred: boolean = false;
 }
