1ac71fd1c69bad0b61b2a27849377d2bbf3e568b
mindscan-de <maxim.gansert@gmail.com>
2022-11-26 11:54:39 +0100

    start with typescript BPE encoder implementation

--- FutureSQR-Frontend/src/app/incubator/bpe/simple-bpe-encoder.ts
+++ FutureSQR-Frontend/src/app/incubator/bpe/simple-bpe-encoder.ts
@@ -10,7 +10,44 @@
  * project and also an encoder for this. ( which is actually quite slow... )
  * but i would say, it is god enough right now.
  * 
- * time for some experiments offline.... 
- */ 
+ * time for some experiments offline....
+ *
+ * -----
+ * The more i test this idea ob using byte pair encoding for the diff analysis and diff presentation, the more i
+ * become convinced this is the right way to do it.
+ * -----
+ * 
+ */
+
 export class SimpleBPEEncoder {
+
+	private __bpeEncoderTable:Map<string,number> = new Map<string,number>();
+	
+	constructor() {
+		
+	}
+
+	public encode( tokens : string[] ):number[] {
+		let encoded_tokens:number[] = [];
+		
+		for( let token of tokens ) {
+			let more_tokens: number[] = this.__buildBpeTokens(token)
+			encoded_tokens.push(...more_tokens);
+		}
+		
+		return encoded_tokens;
+	}
+	
+	
+	private __buildBpeTokens( textToken:string ): number[] {
+		// we don't' need to transform a token, when it already has an unique encoding in the bpeEncoderTable
+		// then looking up this token is absolutely sufficient
+		if( this.__bpeEncoderTable.has(textToken) ) {
+			return [ this.__bpeEncoderTable.get(textToken) ];
+		}
+		
+		// TODO do the real fun part here.
+		
+		return []
+	}
 }
