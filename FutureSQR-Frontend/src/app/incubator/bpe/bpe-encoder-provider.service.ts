9c71f87b45f1590b6084ef202995b5c2c2f16280
mindscan-de <maxim.gansert@gmail.com>
2022-11-27 20:56:46 +0100

    initialize a bpe encoder with the retrieved tokenmap.

--- FutureSQR-Frontend/src/app/incubator/bpe/bpe-encoder-provider.service.ts
+++ FutureSQR-Frontend/src/app/incubator/bpe/bpe-encoder-provider.service.ts
@@ -15,7 +15,9 @@ export class BpeEncoderProviderService {
 
 	constructor(
 		private httpClient : HttpClient	
-	) { }
+	) { 
+		this.initialize();
+	}
 	
 	public hasBPEEncoder():boolean {
 		if(this.bpeEncoder == undefined || this.bpeEncoder == null) {
@@ -32,9 +34,20 @@ export class BpeEncoderProviderService {
 	
 	public initialize() : void {
 		this.loadTokenMap().subscribe( 
-			data => {},
+			data => {
+				this.onBPETokenMapProvided(data);
+			},
 			error => {}
 		)
+	}
+	
+	private onBPETokenMapProvided(tokenMap: BPETokenMap) : void {
+		console.log(tokenMap.tokens);
+		console.log(typeof(tokenMap.tokens));
+		
+		let test = new Map<string,number>(Object.entries(tokenMap.tokens));
+		console.log(test);
+		this.bpeEncoder = new SimpleBPEEncoder(test);
 	}
 	
 	private loadTokenMap() : Observable<BPETokenMap> {
