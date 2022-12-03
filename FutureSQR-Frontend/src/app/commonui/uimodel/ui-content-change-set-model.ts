be1290e01cfca2a869db187d9b496eb8a8173110
mindscan-de <maxim.gansert@gmail.com>
2022-12-04 00:36:43 +0100

    begin ui content change set model.

--- FutureSQR-Frontend/src/app/commonui/uimodel/ui-content-change-set-model.ts
+++ FutureSQR-Frontend/src/app/commonui/uimodel/ui-content-change-set-model.ts
@@ -1,2 +1,27 @@
 export class UiContentChangeSetModel {
+	
+	// each line starts with a " ", "-" or "+" according to GIT based diffs.
+	// 
+	public diffContent: string[] = [];
+	public diffLeftLineCountStart:number = 1;
+	public diffRightLineCountStart:number = 1;
+	
+	constructor(content:string[], startLeft:number, startRight:number) {
+		this.diffContent = content;
+		this.diffLeftLineCountStart = startLeft;
+		this.diffRightLineCountStart = startRight;
+	}
+	
+	getLineCountStartLeft() : number {
+		return this.diffLeftLineCountStart;
+	}
+	
+	getLineCountStartRight() : number {
+		return this.diffRightLineCountStart;
+	}
+	
+	getDiffContent() : string[] {
+		return this.diffContent;
+	}
+	
 }
