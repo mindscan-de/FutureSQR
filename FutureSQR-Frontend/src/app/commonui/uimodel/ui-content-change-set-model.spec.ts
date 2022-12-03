52781e1ecff590807aa4d9421e9b8525302bc2f7
mindscan-de <maxim.gansert@gmail.com>
2022-12-03 23:25:40 +0100

    start with empty uiContentChangeSetModel.

---
+++ FutureSQR-Frontend/src/app/commonui/uimodel/ui-content-change-set-model.spec.ts
@@ -0,0 +1,7 @@
+import { UiContentChangeSetModel } from './ui-content-change-set-model';
+
+describe('UiContentChangeSetModel', () => {
+  it('should create an instance', () => {
+    expect(new UiContentChangeSetModel()).toBeTruthy();
+  });
+});
