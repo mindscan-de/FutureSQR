491ebafb61c8430497994e2c63d87c8dc2e344f0
mindscan-de <maxim.gansert@gmail.com>
2022-12-04 21:00:33 +0100

    update model to contain from/to filenames.

--- FutureSQR-Frontend/src/app/commonui/uimodel/ui-file-change-set-model.ts
+++ FutureSQR-Frontend/src/app/commonui/uimodel/ui-file-change-set-model.ts
@@ -4,8 +4,15 @@ export class UiFileChangeSetModel {
 	
 	public uiContentChangeSet: UiContentChangeSetModel[] = [];
 	
+	public scmFromFilePath: string = "";
+	public scmFromFileName: string = "";
+	
+	public scmToFilePath: string = "";
+	public scmToFileName: string = "";
+	
 	// ATTN: This will be replaced by fromFilename, toFileName, InfoWhether we have moves etc.
 	public tempLazyDiffLineInfo: string = "";
+	
 	// ATTN: This will be replaced and converted into a file mode, if needed.
 	public tempLayzIndexLineInfo: string = "";
 	
@@ -20,4 +27,27 @@ export class UiFileChangeSetModel {
 	public setIndexLine(indexLineInfo:string): void {
 		this.tempLayzIndexLineInfo = indexLineInfo;
 	}
+	
+	public setScmFromPath(scmPath:string): void {
+		this.scmFromFilePath = scmPath;
+		let lastindex:number = scmPath.lastIndexOf("/");
+		if(lastindex==-1) {
+			this.scmFromFileName = scmPath;
+		} 
+		else {
+			this.scmFromFileName = scmPath.substring(lastindex);
+		}
+	}
+	
+	public setScmToPath(scmPath:string): void {
+		this.scmToFilePath = scmPath;
+		let lastindex:number = scmPath.lastIndexOf("/");
+		if(lastindex==-1) {
+			this.scmToFileName = scmPath;
+		} 
+		else {
+			this.scmToFileName = scmPath.substring(lastindex);
+		}
+	}
+	
 }
