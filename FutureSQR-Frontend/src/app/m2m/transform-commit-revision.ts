f44c432cd126dfaccd502a420750ab69f0f81d47
mindscan-de <maxim.gansert@gmail.com>
2022-12-10 13:43:56 +0100

    make the magic constants more readable.

--- FutureSQR-Frontend/src/app/m2m/transform-commit-revision.ts
+++ FutureSQR-Frontend/src/app/m2m/transform-commit-revision.ts
@@ -9,12 +9,19 @@ import { UiReviewFileInformation } from '../commonui/uimodel/ui-review-file-info
 
 export class TransformCommitRevision {
 	
+	public static readonly FILE_INFO_FILEPATH : number  = 1;
+	public static readonly FILE_INFO_FILEACTION: number = 0; 
+	
 	public static convertToUiReviewFileinformationArray(fileChanges: BackendModelSingleCommitFileActionsInfo):  UiReviewFileInformation[] {
-		let map = fileChanges.fileActionMap;
+		let map:string[][] = fileChanges.fileActionMap;
 		
 		let fileInformations : UiReviewFileInformation[] = [];
 		for(let i: number = 0;i<map.length;i++) {
-			let fileInfo: UiReviewFileInformation = new UiReviewFileInformation( map[i][1], map[i][0], true );
+			let fileInfo: UiReviewFileInformation = new UiReviewFileInformation( 
+															map[i][TransformCommitRevision.FILE_INFO_FILEPATH], 
+															map[i][TransformCommitRevision.FILE_INFO_FILEACTION], 
+															true );
+						
 			fileInformations.push(fileInfo);
 		}
 
