4a4e6ae15c709aa80d4e94f9279d8afed9c58def
mindscan-de <maxim.gansert@gmail.com>
2022-12-17 14:50:38 +0100

    provide more supported highlightmodes.

--- FutureSQR-Frontend/src/main.ts
+++ FutureSQR-Frontend/src/main.ts
@@ -8,6 +8,9 @@ import { environment } from './environments/environment';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/markdown/markdown';
 import 'codemirror/mode/diff/diff';
+import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/python/python';
+import 'codemirror/mode/clike/clike';
 
 
 if (environment.production) {
