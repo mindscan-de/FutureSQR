83efabd64d22cf982e014a22386514a27c081200
mindscan-de <maxim.gansert@gmail.com>
2022-12-20 12:42:51 +0100

    add interface to retrieve information for an SCM.

---
+++ FutureSQR-Backend/FutureSQR-ScmAccess/src/de/mindscan/futuresqr/scmaccess/ScmAccess.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2022 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.futuresqr.scmaccess;
+
+import de.mindscan.futuresqr.scmaccess.types.ScmFileContent;
+import de.mindscan.futuresqr.scmaccess.types.ScmFileHistory;
+import de.mindscan.futuresqr.scmaccess.types.ScmPath;
+import de.mindscan.futuresqr.scmaccess.types.ScmRepository;
+
+/**
+ * 
+ */
+public interface ScmAccess {
+
+    // see getParticularFileHistory for context of this method.
+    // some modifications should be prossible: full history, limit (number of revisions in History, cut off date), until last copy / rename operation,   
+    ScmFileHistory getFilePathHistory( ScmRepository repository, ScmPath filePath );
+
+    // see getParticularRevisionContentForFile for context of this method
+    ScmFileContent getFileContentForRevision( ScmRepository repository, String revisionId, ScmPath filePath );
+}
