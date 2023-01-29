3fe68b0cde549f3d02934a869bc3639b94450474
mindscan-de <maxim.gansert@gmail.com>
2023-01-29 20:05:32 +0100

    align with python code in git_output_parser#parse_log_full_changeset

--- FutureSQR-Backend/FutureSQR-ScmAccess/src/de/mindscan/futuresqr/scmaccess/git/processor/impl/ScmFullChangeSetOutputProcessor.java
+++ FutureSQR-Backend/FutureSQR-ScmAccess/src/de/mindscan/futuresqr/scmaccess/git/processor/impl/ScmFullChangeSetOutputProcessor.java
@@ -47,6 +47,16 @@ public class ScmFullChangeSetOutputProcessor implements GitCLICommandOutputProce
     private static final String GIT_DIFF_RENAME_FROM = "rename from";
     private static final String GIT_DIFF_RENAME_TO = "rename to";
 
+    private static final String GIT_DIFF_INDEX_IDENTIFIER = "index";
+    private static final String GIT_DIFF_BINARY_FILES_IDENTIFIER = "Binary files";
+
+    private static final String GIT_DIFF_LEFT_FILEPATH_IDENTIFIER = "---";
+    private static final String GIT_DIFF_RIGHT_FILEPATH_IDENTIFIER = "+++";
+
+    private static final String GIT_DIFF_CONTENT_CHANGESET_IDENTIFIER = "@@ ";
+
+    private static final String GIT_DIFF_NO_NEWLINE_AT_END_OF_FILE_INDICATOR = "\\ No newline at end of file";
+
     /**
      * 
      */
@@ -132,31 +142,94 @@ public class ScmFullChangeSetOutputProcessor implements GitCLICommandOutputProce
 
         // parse from name / from directory
         if (lineLexer.peekCurrentLine().startsWith( GIT_DIFF_RENAME_FROM )) {
+            // TODO: parse and consume this info and add info to current file change set.
             currentFileChangeSet.renamed_from = lineLexer.consumeCurrentLine();
         }
 
         // parse to name / to directory
         if (lineLexer.peekCurrentLine().startsWith( GIT_DIFF_RENAME_TO )) {
+            // TODO: parse and consume this info and add info to current file change set.
             currentFileChangeSet.renamed_to = lineLexer.consumeCurrentLine();
         }
 
         // ----------------------------
         // index line... / binary files
-        // ----------------------------        
+        // ----------------------------
 
-        // while a first menge of a content changeset is found, we will trigger parseContentChangeSetEntry
-        parseContentChangeSetEntry( currentFileChangeSet.fileContentChangeSet::add );
+        if (lineLexer.peekCurrentLine().startsWith( GIT_DIFF_INDEX_IDENTIFIER )) {
+            // TODO: parse and consume this info and add info to current file change set.            
+            currentFileChangeSet.lazy_index_line = lineLexer.consumeCurrentLine();
+        }
+        else {
+            currentFileChangeSet.lazy_index_line = "(empty)";
+        }
 
-        // end of stream or
-        // first menge of a file changeset will finish scanning this file entry 
+        if (lineLexer.peekCurrentLine().startsWith( GIT_DIFF_BINARY_FILES_IDENTIFIER )) {
+            // TODO: parse and consume this info and add info to current file change set.            
+            currentFileChangeSet.isBinaryFile = true;
+            currentFileChangeSet.binary_file_info_line = lineLexer.consumeCurrentLine();
+        }
+
+        // ----------------------------
+        // left and right file path
+        // ----------------------------
+        if (lineLexer.peekCurrentLine().startsWith( GIT_DIFF_LEFT_FILEPATH_IDENTIFIER )) {
+            // TODO: parse and consume this info and add info to current file change set.
+            lineLexer.consumeCurrentLine();
+        }
+
+        if (lineLexer.peekCurrentLine().startsWith( GIT_DIFF_RIGHT_FILEPATH_IDENTIFIER )) {
+            // TODO: parse and consume this info and add info to current file change set.
+            lineLexer.consumeCurrentLine();
+        }
+
+        if (!lineLexer.hasNextLine()) {
+            fileChangeSetConsumer.accept( currentFileChangeSet );
+            return;
+        }
+
+        if (currentFileChangeSet.isBinaryFile) {
+            fileChangeSetConsumer.accept( currentFileChangeSet );
+            return;
+        }
+
+        // content changeset identifier is @@ at start
+        if (lineLexer.peekCurrentLine().startsWith( GIT_DIFF_CONTENT_CHANGESET_IDENTIFIER )) {
+
+            // while a first menge of a content changeset is found, we will trigger parseContentChangeSetEntry
+            while (lineLexer.hasNextLine() && lineLexer.peekCurrentLine().startsWith( GIT_DIFF_CONTENT_CHANGESET_IDENTIFIER )) {
+                parseContentChangeSetEntry( lineLexer, currentFileChangeSet.fileContentChangeSet::add );
+            }
+        }
 
         // result is a file entry in the ScmFullChangeSet
         fileChangeSetConsumer.accept( currentFileChangeSet );
     }
 
-    private void parseContentChangeSetEntry( Consumer<ScmFileContentChangeSet> contentChangeSetConsumer ) {
+    private void parseContentChangeSetEntry( GitScmLineBasedLexer lineLexer, Consumer<ScmFileContentChangeSet> contentChangeSetConsumer ) {
         // this is the parsing of a single
         ScmFileContentChangeSet contentChangeset = new ScmFileContentChangeSet();
+
+        // double check in case we change the outer parser.
+        if (lineLexer.peekCurrentLine().startsWith( GIT_DIFF_CONTENT_CHANGESET_IDENTIFIER )) {
+            // TODO: parse and consume this info and add info to current file change set.            
+            contentChangeset.line_info = lineLexer.consumeCurrentLine();
+        }
+
+        // we must stop at end of input, start at new file info 
+        while (lineLexer.hasNextLine() && !lineLexer.peekCurrentLine().startsWith( GIT_DIFF_FILENAMEINFO_IDENTIFIER )
+                        && !lineLexer.peekCurrentLine().startsWith( GIT_DIFF_CONTENT_CHANGESET_IDENTIFIER )) {
+
+            String currentLine = lineLexer.consumeCurrentLine();
+            if (currentLine.startsWith( GIT_DIFF_NO_NEWLINE_AT_END_OF_FILE_INDICATOR )) {
+                //intentionally left blank;
+            }
+            else {
+                // TODO: it either starts with a '+', '-', ' ' this should be handled and split into a different Array, 
+                // where add, remove and unchanged info is stored for each index. 
+                contentChangeset.line_diff_data.add( currentLine );
+            }
+        }
 
         contentChangeSetConsumer.accept( contentChangeset );
     }
