590fa918657701975a287b748b153cd804f1dd99
mindscan-de <maxim.gansert@gmail.com>
2023-01-29 18:14:13 +0100

    add some file mode parsing.

--- FutureSQR-Backend/FutureSQR-ScmAccess/src/de/mindscan/futuresqr/scmaccess/git/processor/impl/ScmFullChangeSetOutputProcessor.java
+++ FutureSQR-Backend/FutureSQR-ScmAccess/src/de/mindscan/futuresqr/scmaccess/git/processor/impl/ScmFullChangeSetOutputProcessor.java
@@ -40,7 +40,12 @@ import de.mindscan.futuresqr.scmaccess.types.ScmFullChangeSet;
 public class ScmFullChangeSetOutputProcessor implements GitCLICommandOutputProcessor<ScmFullChangeSet> {
 
     private static final String GIT_DIFF_FILENAMEINFO_IDENTIFIER = "diff --git ";
-    private static final String GIT_DIFF_FILENAMEINFO_IDENTIFIER_TRIMMED = GIT_DIFF_FILENAMEINFO_IDENTIFIER.trim();
+    private static final String GIT_DIFF_NEW_FILE_MODE = "new file mode ";
+    private static final String GIT_DIFF_DELETED_FILE_MODE = "deleted file mode";
+
+    private static final String GIT_DIFF_RENAME_SIMILARITY_INDEX = "similarity index";
+    private static final String GIT_DIFF_RENAME_FROM = "rename from";
+    private static final String GIT_DIFF_RENAME_TO = "rename to";
 
     /**
      * 
@@ -75,9 +80,12 @@ public class ScmFullChangeSetOutputProcessor implements GitCLICommandOutputProce
             // peek at current line
             if (currentLine.startsWith( GIT_DIFF_FILENAMEINFO_IDENTIFIER )) {
                 // for each file file entry
+                System.out.println( "[parseFullChangeSet][p]: '" + currentLine + "'" );
                 parseFileChangeSetEntry( lineLexer, fileChangeSetConsumer );
             }
             else {
+                // This will either skip some header or unparsed stuff from the inner parsers.
+                System.out.println( "[parseFullChangeSet][u]: '" + currentLine + "'" );
                 lineLexer.advanceToNextLine();
             }
         }
@@ -92,8 +100,49 @@ public class ScmFullChangeSetOutputProcessor implements GitCLICommandOutputProce
         ScmFileChangeSet currentFileChangeSet = new ScmFileChangeSet();
 
         // Parse the file info
-        // TODO: parse this file name info identifier, such that this info is in the currentFileChangeSet
-        currentFileChangeSet.lazy_diff_line = lineLexer.consumeCurrentLine();
+        if (lineLexer.peekCurrentLine().startsWith( GIT_DIFF_FILENAMEINFO_IDENTIFIER )) {
+            // TODO: parse and consume this file name info identifier, such that this info is in the currentFileChangeSet
+            currentFileChangeSet.lazy_diff_line = lineLexer.consumeCurrentLine();
+        }
+
+        // ---------------------
+        // parse file mode
+        // ---------------------
+
+        // Parse new file mode
+        if (lineLexer.peekCurrentLine().startsWith( GIT_DIFF_NEW_FILE_MODE )) {
+            // TODO: parse and consume this info and add info to current file change set.
+            lineLexer.consumeCurrentLine();
+        }
+        // Parse deleted file mode
+        if (lineLexer.peekCurrentLine().startsWith( GIT_DIFF_DELETED_FILE_MODE )) {
+            // TODO: parse and consume this info and add info to current file change set.
+            lineLexer.consumeCurrentLine();
+        }
+
+        // ---------------------
+        // parse renames
+        // ---------------------
+
+        // parse similarity of rename / move operation 
+        if (lineLexer.peekCurrentLine().startsWith( GIT_DIFF_RENAME_SIMILARITY_INDEX )) {
+            // TODO: parse and consume this info and add info to current file change set.
+            currentFileChangeSet.similarity_info_line = lineLexer.consumeCurrentLine();
+        }
+
+        // parse from name / from directory
+        if (lineLexer.peekCurrentLine().startsWith( GIT_DIFF_RENAME_FROM )) {
+            currentFileChangeSet.renamed_from = lineLexer.consumeCurrentLine();
+        }
+
+        // parse to name / to directory
+        if (lineLexer.peekCurrentLine().startsWith( GIT_DIFF_RENAME_TO )) {
+            currentFileChangeSet.renamed_to = lineLexer.consumeCurrentLine();
+        }
+
+        // ----------------------------
+        // index line... / binary files
+        // ----------------------------        
 
         // while a first menge of a content changeset is found, we will trigger parseContentChangeSetEntry
         parseContentChangeSetEntry( currentFileChangeSet.fileContentChangeSet::add );
