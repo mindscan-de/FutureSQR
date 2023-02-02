79f10beb348a14c1674c434864d733c5f85ed1a3
mindscan-de <maxim.gansert@gmail.com>
2023-02-02 20:21:54 +0100

    start with a simple skeleton for the MultiPartFormdataParser

---
+++ FutureSQR-Backend/FutureSQR-TomcatDevBackend/src/main/java/de/mindscan/futuresqr/devbackend/legacy/MultiPartFormdataParser.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2023 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.futuresqr.devbackend.legacy;
+
+import java.io.InputStream;
+
+/**
+ * Because we use Tomcat 7 right now, we don't have access to Servlet version 3 and 
+ * multipart formdata is not supported by default before version 3. We use Tomcat 7 
+ * because it is simple to use with maven for rapid development. I don't want to 
+ * change the developer backend right now. I will live with this deficit for some 
+ * time until the backend is good enough to invest time to fix the http-rest-backend
+ * to a more recent version.
+ * 
+ * I currently don't want to spend time for ramp up, to a different framework.
+ */
+public class MultiPartFormdataParser {
+
+    /**
+     * 
+     */
+    public MultiPartFormdataParser( InputStream is ) {
+
+    }
+
+    public MultiPartFormdataParameters parse() {
+        MultiPartFormdataParameters parameters = new MultiPartFormdataParameters();
+
+        // TODO: parse header and isolate boundary
+
+        // advance boundary and then parse name and then parse value
+
+        // 
+
+        return parameters;
+    }
+}
