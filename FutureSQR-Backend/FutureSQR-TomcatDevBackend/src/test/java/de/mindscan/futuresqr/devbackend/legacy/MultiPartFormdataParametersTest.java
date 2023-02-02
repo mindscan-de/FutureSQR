d4eb970bbba5d3a263c613997e1ee23615d4f203
mindscan-de <maxim.gansert@gmail.com>
2023-02-02 20:14:01 +0100

    add unit test for remaining methods.

--- FutureSQR-Backend/FutureSQR-TomcatDevBackend/src/test/java/de/mindscan/futuresqr/devbackend/legacy/MultiPartFormdataParametersTest.java
+++ FutureSQR-Backend/FutureSQR-TomcatDevBackend/src/test/java/de/mindscan/futuresqr/devbackend/legacy/MultiPartFormdataParametersTest.java
@@ -3,6 +3,7 @@ package de.mindscan.futuresqr.devbackend.legacy;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -48,6 +49,70 @@ public class MultiPartFormdataParametersTest {
 
         // assert
         assertThat( result, equalTo( expectedValueX ) );
+    }
+
+    @Test
+    public void testGetStringOrThrow_CtorUnknownParameter_throwsException() throws Exception {
+        // arrange
+        MultiPartFormdataParameters parameters = new MultiPartFormdataParameters();
+
+        // act
+        // assert
+        assertThrows( RuntimeException.class, () -> {
+            parameters.getStringOrThrow( UNKNOWN_KEY );
+        } );
+    }
+
+    @Test
+    public void testGetStringOrThrow_CtorUnknownParameterButSetKnownParameter_throwsException() throws Exception {
+        // arrange
+        MultiPartFormdataParameters parameters = new MultiPartFormdataParameters();
+        parameters.addParameter( KNOWN_KEY, "ValueX" );
+
+        // act
+        // assert
+        assertThrows( RuntimeException.class, () -> {
+            parameters.getStringOrThrow( UNKNOWN_KEY );
+        } );
+    }
+
+    @Test
+    public void testGetStringOrThrow_CtorKnownParameter_retursExpectedValueX() throws Exception {
+        // arrange
+        MultiPartFormdataParameters parameters = new MultiPartFormdataParameters();
+        String expectedValueX = "ValueX";
+        parameters.addParameter( KNOWN_KEY, expectedValueX );
+
+        // act
+        String result = parameters.getStringOrThrow( KNOWN_KEY );
+
+        // assert
+        assertThat( result, equalTo( expectedValueX ) );
+    }
+
+    @Test
+    public void testGetStringOrDefault_CtorUnknownParameter_returnsDefaultValue() throws Exception {
+        // arrange
+        MultiPartFormdataParameters parameters = new MultiPartFormdataParameters();
+
+        // act
+        String result = parameters.getStringOrDefault( UNKNOWN_KEY, "defaultValue" );
+
+        // assert
+        assertThat( result, equalTo( "defaultValue" ) );
+    }
+
+    @Test
+    public void testGetStringOrDefault_CtorKnownKeyKnownValue_returnsKnownKnownValue() throws Exception {
+        // arrange
+        MultiPartFormdataParameters parameters = new MultiPartFormdataParameters();
+        parameters.addParameter( KNOWN_KEY, "knownValue" );
+
+        // act
+        String result = parameters.getStringOrDefault( KNOWN_KEY, "defaultValue" );
+
+        // assert
+        assertThat( result, equalTo( "knownValue" ) );
     }
 
 }
