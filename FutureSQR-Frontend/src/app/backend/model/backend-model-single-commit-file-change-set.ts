44e90b54f877db4c3284be3d7c5c4f3b715bb024
mindscan-de <maxim.gansert@gmail.com>
2023-03-01 21:23:29 +0100

    remove tempLazyLineDiffLineInfo from type script code. remove diff --git

--- FutureSQR-Frontend/src/app/backend/model/backend-model-single-commit-file-change-set.ts
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-single-commit-file-change-set.ts
@@ -1,10 +1,6 @@
 import { BackendModelSingleCommitFileContentChangeSet } from './backend-model-single-commit-file-content-change-set';
 
 export class BackendModelSingleCommitFileChangeSet {
-	// TODO: contains information about a particular file
-	//       and what happened to this file:
-	// modified, added, renamed, etc., which hash it has. and so on....
-	public lazy_diff_line:string = "";
 	public fromPath:string = "";
 	public toPath:string = "";
 	public lazy_index_line:string = "";
--- FutureSQR-Frontend/src/app/commonui/uimodel/ui-file-change-set-model.ts
+++ FutureSQR-Frontend/src/app/commonui/uimodel/ui-file-change-set-model.ts
@@ -13,18 +13,11 @@ export class UiFileChangeSetModel {
 	public scmToFileName: string = "";
 	
 	
-	// ATTN: This will be replaced by fromFilename, toFileName, InfoWhether we have moves etc.
-	public tempLazyDiffLineInfo: string = "";
-	
 	// ATTN: This will be replaced and converted into a file mode, if needed.
 	public tempLayzIndexLineInfo: string = "";
 	
 	public setUiContentChangeSet( contentChangeSet: UiContentChangeSetModel[]) : void {
 		this.uiContentChangeSet = contentChangeSet;
-	}
-	
-	public setDiffLine(diffLineInfo:string): void {
-		this.tempLazyDiffLineInfo = diffLineInfo;
 	}
 	
 	public setIndexLine(indexLineInfo:string): void {
