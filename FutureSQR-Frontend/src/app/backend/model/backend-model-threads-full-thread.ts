da2860e86ed53e16c2d2f64a1c71935fc114803d
mindscan-de <maxim.gansert@gmail.com>
2023-03-04 17:48:04 +0100

    adapt backend code to retrieve timestamps from backend.

--- FutureSQR-Frontend/src/app/backend/model/backend-model-threads-full-thread.ts
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-threads-full-thread.ts
@@ -3,6 +3,7 @@ import { BackendModelThreadsMessage } from './backend-model-threads-message';
 export class BackendModelThreadsFullThread {
 	public threadId: string = "";
 	public authorId: string = "";
+	public threadCreatedTS: number = 0;
 	public messagesId: string[] = [];
 	public messages: Map<string,BackendModelThreadsMessage> = new Map<string,BackendModelThreadsMessage>();
 }
--- FutureSQR-Frontend/src/app/backend/model/backend-model-threads-message.ts
+++ FutureSQR-Frontend/src/app/backend/model/backend-model-threads-message.ts
@@ -1,6 +1,7 @@
 export class BackendModelThreadsMessage {
 	public messageId: string = "";
 	public threadId: string = "";
+	public messageCreatedTS: number = 0;
 	public message: string = "";
 	public authorId: string = "";
 	public replyToMsgId: string = "";
