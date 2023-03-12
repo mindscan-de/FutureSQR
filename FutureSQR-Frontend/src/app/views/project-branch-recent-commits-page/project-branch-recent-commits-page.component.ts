37e2875539a3a02a2f3a948b6b3e7b6ebcaca8e5
mindscan-de <maxim.gansert@gmail.com>
2023-03-12 16:43:08 +0100

    start implementation of recent commits page for a defined branch.

--- FutureSQR-Frontend/src/app/views/project-branch-recent-commits-page/project-branch-recent-commits-page.component.ts
+++ FutureSQR-Frontend/src/app/views/project-branch-recent-commits-page/project-branch-recent-commits-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+// Services
+import { NavigationBarService } from '../../uiservices/navigation-bar.service';
+
 
 @Component({
   selector: 'app-project-branch-recent-commits-page',
@@ -6,10 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-branch-recent-commits-page.component.css']
 })
 export class ProjectBranchRecentCommitsPageComponent implements OnInit {
+	
+	public activeProjectID: string = '';
+    public activeBranchName: string;
 
-  constructor() { }
+	constructor(
+		private navigationBarService : NavigationBarService,
+		private route: ActivatedRoute
+	) { }
 
-  ngOnInit(): void {
-  }
 
+	ngOnInit(): void {
+		this.activeProjectID = this.route.snapshot.paramMap.get('projectid');
+		this.activeBranchName = this.route.snapshot.paramMap.get('branchname');
+		
+		this.setTopNavigation();
+	}
+	
+	setTopNavigation() : void {
+		let x = []
+		
+		x.push(this.navigationBarService.createItem( this.activeProjectID, ['/',this.activeProjectID], false ));
+		x.push(this.navigationBarService.createItem( 'Branch', ['/', this.activeProjectID, 'branches'], false));
+		x.push(this.navigationBarService.createItem( this.activeBranchName,['/', this.activeProjectID, 'branches'], true ));
+		
+		this.navigationBarService.setBreadcrumbNavigation(x);		
+	}
+	
+
+
+	refreshCommitHistory(event:any) : void {
+/*		this.projectDataQueryBackend.getRecentProjectCommits(this.activeProjectID).subscribe( 
+			data => this.onRecentProjectCommitsProvided(data),
+			error => console.log(error)
+		);
+*/	}
+	
 }
