df24465ca7d81013da2caa098e066421cdb68bfa
mindscan-de <maxim.gansert@gmail.com>
2023-03-23 20:02:40 +0100

    start with unit tests (integration tests) for squashing diffs.

--- FutureSQR-Backend/FutureSQR-ReviewSystem-Core/test/de/mindscan/futuresqr/domain/incubator/UnifiedDiffCalculationV1Test.java
+++ FutureSQR-Backend/FutureSQR-ReviewSystem-Core/test/de/mindscan/futuresqr/domain/incubator/UnifiedDiffCalculationV1Test.java
@@ -1,6 +1,7 @@
 package de.mindscan.futuresqr.domain.incubator;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
 
 import java.util.ArrayList;
@@ -10,6 +11,10 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import de.mindscan.futuresqr.domain.model.changeset.FSqrRevisionFullChangeSet;
+import de.mindscan.futuresqr.scmaccess.git.command.GitCommands;
+import de.mindscan.futuresqr.scmaccess.git.processor.GitOutputProcessors;
+import de.mindscan.futuresqr.scmaccess.types.ScmFullChangeSet;
+import de.mindscan.futuresqr.scmaccess.types.ScmRepository;
 
 public class UnifiedDiffCalculationV1Test {
 
@@ -26,6 +31,39 @@ public class UnifiedDiffCalculationV1Test {
 
         // assert
         assertThat( squashedDiffs.getFileChangeSet(), empty() );
+    }
+
+    @Test
+    public void testSquashDiffs_threeRevisionsInRowSelectFirst_returnsFullChangeSetWithFirstRevisionId() throws Exception {
+        // arrange
+        UnifiedDiffCalculationV1 diffCalculation = new UnifiedDiffCalculationV1();
+        List<String> selectedRevisions = new ArrayList<>();
+        // first revision.
+        selectedRevisions.add( "1a306de710d0366ab68f3dc77b538600e1891488" );
+        Collection<String> filterRevisions = null;
+        List<FSqrRevisionFullChangeSet> intermediateRevisions = retrieveRevisions( "1a306de710d0366ab68f3dc77b538600e1891488",
+                        "1921fa018c148056407eeed200a229349daadb9f" );
+
+        // act
+        FSqrRevisionFullChangeSet squashedDiffs = diffCalculation.squashDiffs( intermediateRevisions, filterRevisions, selectedRevisions );
+
+        // assert
+        assertThat( squashedDiffs.getRevisionId(), equalTo( "1a306de710d0366ab68f3dc77b538600e1891488" ) );
+    }
+
+    private List<FSqrRevisionFullChangeSet> retrieveRevisions( String firstRevisionId, String lastRevisionId ) {
+        ScmRepository repository = new ScmRepository( "D:\\Temp\\future-square-cache\\FutureSQR" );
+
+        FakeGitCLICommandExecutor gitContentProvider = new FakeGitCLICommandExecutor( false );
+
+        List<ScmFullChangeSet> revisionList = gitContentProvider//
+                        .execute( repository, GitCommands.createGetDiffBetweenRevisionsCommand( firstRevisionId, lastRevisionId ) ) //
+                        .transform( GitOutputProcessors.toScmFullChangeSetList() );
+
+        List<FSqrRevisionFullChangeSet> result = new ArrayList<>();
+        revisionList.stream().forEach( changeSet -> result.add( new FSqrRevisionFullChangeSet( changeSet ) ) );
+
+        return result;
     }
 
 }
