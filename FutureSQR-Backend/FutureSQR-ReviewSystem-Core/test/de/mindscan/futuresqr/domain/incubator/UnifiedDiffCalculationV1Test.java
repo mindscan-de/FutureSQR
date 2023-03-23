55be61a150b5a5c633810d5503e10c050921e122
mindscan-de <maxim.gansert@gmail.com>
2023-03-23 20:15:01 +0100

    add tests for squashdiffs (select the correct revision)

--- FutureSQR-Backend/FutureSQR-ReviewSystem-Core/test/de/mindscan/futuresqr/domain/incubator/UnifiedDiffCalculationV1Test.java
+++ FutureSQR-Backend/FutureSQR-ReviewSystem-Core/test/de/mindscan/futuresqr/domain/incubator/UnifiedDiffCalculationV1Test.java
@@ -18,6 +18,18 @@ import de.mindscan.futuresqr.scmaccess.types.ScmRepository;
 
 public class UnifiedDiffCalculationV1Test {
 
+    // UnifiedDiffCalculationV1.java
+    private static final String FIRST_OF_THREE_1A306DE7 = "1a306de710d0366ab68f3dc77b538600e1891488";
+    private static final String SECOND_OF_THREE_DFB2A463 = "dfb2a46357b1f5cd02407a8ee9dc88ecb24eace8";
+    private static final String LAST_OF_THREE_1921FA01 = "1921fa018c148056407eeed200a229349daadb9f";
+
+    // UnifiedDiffCalculationV1.java + FSqrRevisionFullChangeSet.java
+    private static final String FIRST_OF_FIVE_1A306DE7 = "1a306de710d0366ab68f3dc77b538600e1891488";
+    private static final String SECOND_OF_FIVE_DFB2A463 = "dfb2a46357b1f5cd02407a8ee9dc88ecb24eace8";
+    private static final String THIRD_OF_FIVE_1921FA01 = "1921fa018c148056407eeed200a229349daadb9f";
+    private static final String FOURTH_OF_FIVE_30370E41 = "30370e41bf66e94f5fb5c1f3b6787f11cdf8b606";
+    private static final String LAST_OF_FIVE_EF0B753D = "ef0b753dba814ab41dd11a760c41aaa19d250389";
+
     @Test
     public void testSquashDiffs_emptySelections_returnsEmptyFileChangeSetInFullChangeSet() throws Exception {
         // arrange
@@ -39,16 +51,32 @@ public class UnifiedDiffCalculationV1Test {
         UnifiedDiffCalculationV1 diffCalculation = new UnifiedDiffCalculationV1();
         List<String> selectedRevisions = new ArrayList<>();
         // first revision.
-        selectedRevisions.add( "1a306de710d0366ab68f3dc77b538600e1891488" );
+        selectedRevisions.add( FIRST_OF_THREE_1A306DE7 );
         Collection<String> filterRevisions = null;
-        List<FSqrRevisionFullChangeSet> intermediateRevisions = retrieveRevisions( "1a306de710d0366ab68f3dc77b538600e1891488",
-                        "1921fa018c148056407eeed200a229349daadb9f" );
+        List<FSqrRevisionFullChangeSet> intermediateRevisions = retrieveRevisions( FIRST_OF_THREE_1A306DE7, LAST_OF_THREE_1921FA01 );
 
         // act
         FSqrRevisionFullChangeSet squashedDiffs = diffCalculation.squashDiffs( intermediateRevisions, filterRevisions, selectedRevisions );
 
         // assert
-        assertThat( squashedDiffs.getRevisionId(), equalTo( "1a306de710d0366ab68f3dc77b538600e1891488" ) );
+        assertThat( squashedDiffs.getRevisionId(), equalTo( FIRST_OF_THREE_1A306DE7 ) );
+    }
+
+    @Test
+    public void testSquashDiffs_threeRevisionsInRowSelectLast_returnsFullChangeSetWithLastRevisionId() throws Exception {
+        // arrange
+        UnifiedDiffCalculationV1 diffCalculation = new UnifiedDiffCalculationV1();
+        List<String> selectedRevisions = new ArrayList<>();
+        // first revision.
+        selectedRevisions.add( LAST_OF_THREE_1921FA01 );
+        Collection<String> filterRevisions = null;
+        List<FSqrRevisionFullChangeSet> intermediateRevisions = retrieveRevisions( FIRST_OF_THREE_1A306DE7, LAST_OF_THREE_1921FA01 );
+
+        // act
+        FSqrRevisionFullChangeSet squashedDiffs = diffCalculation.squashDiffs( intermediateRevisions, filterRevisions, selectedRevisions );
+
+        // assert
+        assertThat( squashedDiffs.getRevisionId(), equalTo( LAST_OF_THREE_1921FA01 ) );
     }
 
     private List<FSqrRevisionFullChangeSet> retrieveRevisions( String firstRevisionId, String lastRevisionId ) {
