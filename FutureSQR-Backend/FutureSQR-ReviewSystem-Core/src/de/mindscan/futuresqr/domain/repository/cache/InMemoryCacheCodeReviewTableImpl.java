71f78df10a872ba2835815a1ea2d308c5d6263f6
mindscan-de <maxim.gansert@gmail.com>
2023-03-28 14:07:39 +0200

    refactor: cleanup in memory cache for code reviews.

--- FutureSQR-Backend/FutureSQR-ReviewSystem-Core/src/de/mindscan/futuresqr/domain/repository/cache/InMemoryCacheCodeReviewTableImpl.java
+++ FutureSQR-Backend/FutureSQR-ReviewSystem-Core/src/de/mindscan/futuresqr/domain/repository/cache/InMemoryCacheCodeReviewTableImpl.java
@@ -36,11 +36,9 @@ import de.mindscan.futuresqr.domain.model.FSqrCodeReview;
  */
 public class InMemoryCacheCodeReviewTableImpl {
 
-    // also LRU cache implementation, such that the number of reviews in memory
+    // TODO: also LRU cache implementation, such that the number of reviews in memory
     // are limited, and those which aren't used are rotated out from cache.
 
-    // TODO: replace this map in map / multimap (map of maps) with single map and a simple key calculation.
-    // TODO: project id and review id should be UUIDs?
     private Map<String, Map<String, FSqrCodeReview>> projectIdReviewIdToCodeReviewCache;
 
     /**
@@ -64,25 +62,21 @@ public class InMemoryCacheCodeReviewTableImpl {
         return null;
     }
 
-    // TODO: getOrCompute which will either get or execute the expression and then put the code review into the table.
-    public FSqrCodeReview getCodeReviewOrComputeIfAbsent( String projectId, String reviewId, BiFunction<String, String, FSqrCodeReview> computeFunction ) {
+    public FSqrCodeReview getCodeReviewOrComputeIfAbsent( String projectId, String reviewId, BiFunction<String, String, FSqrCodeReview> computeCodeReview ) {
         if (isCached( projectId, reviewId )) {
             return getCodeReview( projectId, reviewId );
         }
 
-        FSqrCodeReview codeReview = computeFunction.apply( projectId, reviewId );
+        FSqrCodeReview codeReview = computeCodeReview.apply( projectId, reviewId );
         putCodeReview( projectId, reviewId, codeReview );
         return codeReview;
     }
 
-    // TODO: putCodeReview - use the multi map initialization / key function.
     public void putCodeReview( String projectId, String reviewId, FSqrCodeReview codeReview ) {
         getOrCreateProjectToCodeReviewMap( projectId ).put( reviewId, codeReview );
     }
 
-    // ATTENTION: Actually only call on insert.
     private Map<String, FSqrCodeReview> getOrCreateProjectToCodeReviewMap( String projectId ) {
-        // ATTENTION this allows for a DOS attack because it forces the repository map to grow.
         return projectIdReviewIdToCodeReviewCache.computeIfAbsent( projectId, pk -> new HashMap<String, FSqrCodeReview>() );
     }
 
