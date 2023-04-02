76a40625dc6e126a5adc0d7816d9872638686730
mindscan-de <maxim.gansert@gmail.com>
2023-04-02 20:01:41 +0200

    add "add project configuration" to routing and such.

--- FutureSQR-Frontend/src/app/adminapp/adminapp-routing.module.ts
+++ FutureSQR-Frontend/src/app/adminapp/adminapp-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminappComponent } from './adminapp.component';
 import { ConfigureProjectComponent } from './views/configure-project/configure-project.component';
 import { ConfigureProjectsComponent } from './views/configure-projects/configure-projects.component';
+import { ConfigureAddProjectComponent } from './views/configure-add-project/configure-add-project.component';
 import { ConfigureUserComponent } from './views/configure-user/configure-user.component';
 import { ConfigureAddUserComponent } from './views/configure-add-user/configure-add-user.component';
 import { ConfigureUsersComponent } from './views/configure-users/configure-users.component';
 import { ConfigureGroupComponent } from './views/configure-group/configure-group.component';
 import { ConfigureGroupsComponent } from './views/configure-groups/configure-groups.component';
+
 
 const routes: Routes = [
 	// show the main admin page
@@ -16,6 +18,7 @@ const routes: Routes = [
 	
 	// projects
 	{ path: 'project/:projectid', component: ConfigureProjectComponent },
+	{ path: 'projects/add', component: ConfigureAddProjectComponent },
 	{ path: 'projects' , component: ConfigureProjectsComponent },
 	
 	// users
@@ -26,6 +29,8 @@ const routes: Routes = [
 	// groups
 	{ path: 'group/:groupuuid' , component: ConfigureGroupComponent },
 	{ path: 'groups' , component: ConfigureGroupsComponent },
+	
+	// TODO: servers - this is when we start communicating with servers directly, instead of local checked out repositories
 	
 	// catch all other unknown URLs
 	{ path:'**',redirectTo:''}
