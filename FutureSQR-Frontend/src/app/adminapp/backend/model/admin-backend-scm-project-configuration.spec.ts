6527baa18d26d47cfd5fe39633a96f507b5a83e8
mindscan-de <maxim.gansert@gmail.com>
2023-04-17 21:54:19 +0200

    add backend model for the scm configuration

---
+++ FutureSQR-Frontend/src/app/adminapp/backend/model/admin-backend-scm-project-configuration.spec.ts
@@ -0,0 +1,7 @@
+import { AdminBackendScmProjectConfiguration } from './admin-backend-scm-project-configuration';
+
+describe('AdminBackendScmProjectConfiguration', () => {
+  it('should create an instance', () => {
+    expect(new AdminBackendScmProjectConfiguration()).toBeTruthy();
+  });
+});
---
+++ FutureSQR-Frontend/src/app/adminapp/backend/model/admin-backend-scm-project-configuration.ts
@@ -0,0 +1,10 @@
+export class AdminBackendScmProjectConfiguration {
+
+	public projectDisplayName: string = "";
+	public projectDescription: string = "";
+	public projectId: string = "";
+	public projectReviewPrefix: string = "";
+	public projectUuid: string = "";
+	public scmProjectType: string = "";
+	
+}
