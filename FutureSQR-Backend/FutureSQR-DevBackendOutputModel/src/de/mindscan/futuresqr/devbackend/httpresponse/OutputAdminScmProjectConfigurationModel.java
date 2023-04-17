628054cc8523906dc325234822661bbbdbc6b2d9
mindscan-de <maxim.gansert@gmail.com>
2023-04-17 21:45:50 +0200

    add admin output model for an scm project configuration.

---
+++ FutureSQR-Backend/FutureSQR-DevBackendOutputModel/src/de/mindscan/futuresqr/devbackend/httpresponse/OutputAdminScmProjectConfigurationModel.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2023 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.futuresqr.devbackend.httpresponse;
+
+import de.mindscan.futuresqr.domain.model.FSqrScmProjectConfiguration;
+
+/**
+ * 
+ */
+public class OutputAdminScmProjectConfigurationModel {
+
+    public String projectDisplayName = "";
+    public String projectDescription = "";
+    public String projectId = "";
+    public String projectReviewPrefix = "";
+    public String projectUuid = "";
+    public String scmProjectType = "";
+
+    public OutputAdminScmProjectConfigurationModel( FSqrScmProjectConfiguration projectConfiguration ) {
+        this.projectDescription = projectConfiguration.getProjectDescription();
+        this.projectDisplayName = projectConfiguration.getProjectDisplayName();
+        this.projectId = projectConfiguration.getProjectId();
+        this.projectReviewPrefix = projectConfiguration.getProjectReviewPrefix();
+        this.projectUuid = projectConfiguration.getProjectUuid();
+        this.scmProjectType = projectConfiguration.getScmProjectType().toString();
+    }
+
+}
