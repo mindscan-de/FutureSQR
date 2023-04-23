8d420307306267530011f3f639faf278bb5fb42f
mindscan-de <maxim.gansert@gmail.com>
2023-04-23 13:55:41 +0200

    start package for the scm crawler - will be separated later.

---
+++ FutureSQR-Backend/FutureSQR-ReviewSystem-Core/src/de/mindscan/futuresqr/crawlers/impl/FutureSquareScmCrawler.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2023 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.futuresqr.crawlers.impl;
+
+/**
+ * 
+ */
+public class FutureSquareScmCrawler {
+
+    // TODO: we need the system configuration
+    // TODO: we need the scm project configuration / e.g. refresh intervall,
+    // 
+    // for git and svn we must have different strategies, 
+    //   like, for svn we need a server configuration, such that multiple projects can be observed at once, in cases
+    //   where branches and projects are basically in the same "branches" folder, and checking for changes 
+    // 
+}
