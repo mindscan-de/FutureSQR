af838b2195b6eb41ba70d3a1fdc3c704c47fbf49
mindscan-de <maxim.gansert@gmail.com>
2023-05-19 14:16:27 +0200

    add AlternateScmAliasesDatabseTable implementation with hardcoded data.

---
+++ FutureSQR-Backend/FutureSQR-ReviewSystem-Core/src/de/mindscan/futuresqr/domain/databases/impl/FSqrAlternateScmAliasesDatabaseTableImpl.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2023 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.futuresqr.domain.databases.impl;
+
+import de.mindscan.futuresqr.domain.databases.FSqrAlternateScmAliasesDatabaseTable;
+
+/**
+ * 
+ */
+public class FSqrAlternateScmAliasesDatabaseTableImpl implements FSqrAlternateScmAliasesDatabaseTable {
+
+    /**
+     * 
+     */
+    public FSqrAlternateScmAliasesDatabaseTableImpl() {
+        // TODO: remove me, when we have a database and a database session object.
+        initHardcodedData();
+    }
+
+    /**
+     * 
+     */
+    protected void initHardcodedData() {
+        insertUserAlias( "mindscan-de", "8ce74ee9-48ff-3dde-b678-58a632887e31" );
+        insertUserAlias( "Maxim Gansert", "8ce74ee9-48ff-3dde-b678-58a632887e31" );
+        insertUserAlias( "someoneelsa", "f5fc8449-3049-3498-9f6b-ce828515bba2" );
+        insertUserAlias( "mindscan-banned", "6822a80d-1854-304c-a26d-81acd2c008f3" );
+        insertUserAlias( "rbreunung", "35c94b55-559f-30e4-a2f4-ee16d31fc276" );
+        insertUserAlias( "Robert Breunung", "35c94b55-559f-30e4-a2f4-ee16d31fc276" );
+    }
+
+    @Override
+    public void insertUserAlias( String aliasName, String userUuid ) {
+        // TODO implement me.
+    }
+
+}
