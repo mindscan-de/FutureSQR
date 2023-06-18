861b2e970694c4281ca415bd798dd6ff43e30c9a
mindscan-de <maxim.gansert@gmail.com>
2023-06-18 14:52:47 +0200

    notice about the backup and restore process and its particular use.

--- FutureSQR-Backend/FutureSQR-ReviewSystem-Core/src/de/mindscan/futuresqr/domain/repository/impl/FSqrBackupRestoreInstallSystemServicesImpl.java
+++ FutureSQR-Backend/FutureSQR-ReviewSystem-Core/src/de/mindscan/futuresqr/domain/repository/impl/FSqrBackupRestoreInstallSystemServicesImpl.java
@@ -30,6 +30,17 @@ import de.mindscan.futuresqr.domain.repository.FSqrBackupRestoreInstallSystemSer
 
 /**
  * 
+ * For now speaking i currently have no idea, how i'm going to implement this. The code 
+ * will speak to me, when it is time...
+ * 
+ * Backup Restore is not only important for Backup and Restore a working instance but also
+ * for development. E.g. keeping my own reviews/and review data (dogfooding), but also to
+ * provide a mechanism to migrate to new databases and to migrate to new perstence layouts.
+ * 
+ * Also this feature is important to import 3rd party databases / backups to migrate them 
+ * to this review tool. Therefore we have multiple reasons to actually have this feature
+ * being present very "early" in the development process. (I mean before other features). 
+ *  
  */
 public class FSqrBackupRestoreInstallSystemServicesImpl implements FSqrBackupRestoreInstallSystemServices {
 
