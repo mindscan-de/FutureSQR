ac856a7c3c76b83ea4cc1070e9f375989ca1bd88
mindscan-de <maxim.gansert@gmail.com>
2023-07-01 00:27:41 +0200

    add m2m package to convert from Fsqr to scm access types.

---
+++ FutureSQR-Backend/FutureSQR-ReviewSystem-Core/src/de/mindscan/futuresqr/domain/model/m2m/ScmRepositoryFactory.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2023 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.futuresqr.domain.model.m2m;
+
+import de.mindscan.futuresqr.domain.configuration.FSqrSystemInstanceConfiguration;
+import de.mindscan.futuresqr.domain.model.FSqrScmProjectConfiguration;
+import de.mindscan.futuresqr.domain.model.FSqrScmProjectType;
+import de.mindscan.futuresqr.scmaccess.types.ScmRepository;
+
+/**
+ * 
+ */
+public class ScmRepositoryFactory {
+
+    public static ScmRepository toScmRepository( FSqrSystemInstanceConfiguration systemConfiguration, FSqrScmProjectConfiguration scmConfiguration ) {
+        // TDOO: 
+        // - we need A factory, which can translate some FSqrScmProjectConfiguration to a useful ScmRepository Object
+        // - we should not do this here....
+        // depending on the type, we might want to distinguish between different needs according to the repository type.
+    
+        String repoCachePath = systemConfiguration.getSystemRepoCachePath();
+    
+        if (scmConfiguration.isScmProjectType( FSqrScmProjectType.git )) {
+            if (scmConfiguration.hasLocalRepoPath()) {
+                // not yet nice but better than before.
+                return new ScmRepository( repoCachePath + scmConfiguration.getScmGitAdminConfiguration().localPath );
+            }
+        }
+    
+        // TODO: this has to be fixed soon...
+        ScmRepository result = new ScmRepository( repoCachePath + "FutureSQR" );
+        return result;
+    }
+
+}
