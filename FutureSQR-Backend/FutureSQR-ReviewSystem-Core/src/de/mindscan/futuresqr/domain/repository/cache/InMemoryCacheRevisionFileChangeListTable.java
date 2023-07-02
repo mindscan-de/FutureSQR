6f2174e6ad835adf78dda9a17b89da1655d6001d
mindscan-de <maxim.gansert@gmail.com>
2023-07-02 22:15:02 +0200

    continue in memory cache implementation for FSqrRevisionFileChangeList.

--- FutureSQR-Backend/FutureSQR-ReviewSystem-Core/src/de/mindscan/futuresqr/domain/repository/cache/InMemoryCacheRevisionFileChangeListTable.java
+++ FutureSQR-Backend/FutureSQR-ReviewSystem-Core/src/de/mindscan/futuresqr/domain/repository/cache/InMemoryCacheRevisionFileChangeListTable.java
@@ -25,9 +25,76 @@
  */
 package de.mindscan.futuresqr.domain.repository.cache;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+import de.mindscan.futuresqr.domain.model.FSqrRevisionFileChangeList;
+
 /**
  * 
  */
 public class InMemoryCacheRevisionFileChangeListTable {
 
+    private Map<String, Map<String, FSqrRevisionFileChangeList>> projectIdRevisionIdToFileChangeListCache;
+
+    /**
+     * 
+     */
+    public InMemoryCacheRevisionFileChangeListTable() {
+        this.projectIdRevisionIdToFileChangeListCache = new HashMap<>();
+    }
+
+    public boolean isCached( String projectId, String revisionId ) {
+        if (ifKnownProjectId( projectId )) {
+            return projectIdRevisionIdToFileChangeListCache.get( projectId ).containsKey( revisionId );
+        }
+
+        return false;
+    }
+
+    private boolean ifKnownProjectId( String projectId ) {
+        return projectIdRevisionIdToFileChangeListCache.containsKey( projectId );
+    }
+
+    public FSqrRevisionFileChangeList getFSqrRevisionFileChangeListOrComputIfAbsent( String projectId, String revisionId,
+                    BiFunction<String, String, FSqrRevisionFileChangeList> fileListLoader ) {
+        if (isCached( projectId, revisionId )) {
+            return projectIdRevisionIdToFileChangeListCache.get( projectId ).get( revisionId );
+        }
+
+        if (fileListLoader == null) {
+            return null;
+        }
+
+        FSqrRevisionFileChangeList fileChangelist = fileListLoader.apply( projectId, revisionId );
+
+        if (fileChangelist != null) {
+            putFileChangeList( projectId, revisionId, fileChangelist );
+        }
+        else {
+            // TODO this (projectId x revisionId) doesn't seem to exist, we may buffer that negative answer for a while. 
+        }
+
+        return fileChangelist;
+    }
+
+    public void putFileChangeList( String projectId, String revisionId, FSqrRevisionFileChangeList fileChangeList ) {
+        if (fileChangeList == null) {
+            return;
+        }
+
+        // TODO: do we need some more checks?
+
+        // insert the file changelist to cache
+        getOrCreateProjectToRevisionIdMap( projectId ).put( revisionId, fileChangeList );
+    }
+
+    /**
+     * @param projectId
+     * @return
+     */
+    private Map<String, FSqrRevisionFileChangeList> getOrCreateProjectToRevisionIdMap( String projectId ) {
+        return projectIdRevisionIdToFileChangeListCache.computeIfAbsent( projectId, pk -> new HashMap<String, FSqrRevisionFileChangeList>() );
+    }
 }
