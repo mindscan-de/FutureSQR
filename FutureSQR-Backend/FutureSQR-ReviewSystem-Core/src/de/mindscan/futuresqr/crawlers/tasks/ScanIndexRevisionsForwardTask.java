a75905a2c3357502b91a102762be155e713888fc
mindscan-de <maxim.gansert@gmail.com>
2023-07-22 23:45:24 +0200

    add two new tasks to index revisions.

---
+++ FutureSQR-Backend/FutureSQR-ReviewSystem-Core/src/de/mindscan/futuresqr/crawlers/tasks/ScanIndexRevisionsForwardTask.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2023 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.futuresqr.crawlers.tasks;
+
+import de.mindscan.futuresqr.tasks.FSqrBackgroundTask;
+
+/**
+ * We actually have two scan and index strategies for scm revisions. 
+ * 
+ * When we commit and update the revisions we start at the oldest known 
+ * revision in the database for a certain project with a certain revision.  
+ */
+public class ScanIndexRevisionsForwardTask implements FSqrBackgroundTask {
+
+    private String projectIdentifier;
+    private String projectBranch;
+    private String startRevision;
+
+    /**
+     * 
+     */
+    public ScanIndexRevisionsForwardTask( String projectIdentifier, String projectBranch, String startRevision ) {
+        this.projectIdentifier = projectIdentifier;
+        this.projectBranch = projectBranch;
+        this.startRevision = startRevision;
+    }
+
+}
