d8b878c7f9a075ce8c22da71895e0d5baf8a8310
mindscan-de <maxim.gansert@gmail.com>
2023-08-03 21:05:11 +0200

    add unit tests for the borrow mechanism.

--- FutureSQR-Backend/FutureSQR-Core/test/de/mindscan/futuresqr/core/thread/FSqrWorkerThreadPoolTest.java
+++ FutureSQR-Backend/FutureSQR-Core/test/de/mindscan/futuresqr/core/thread/FSqrWorkerThreadPoolTest.java
@@ -2,7 +2,10 @@ package de.mindscan.futuresqr.core.thread;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FSqrWorkerThreadPoolTest {
@@ -63,6 +66,88 @@ public class FSqrWorkerThreadPoolTest {
         // assert
         int result = threadPool.getNumberOfPooledThreads();
         assertThat( result, equalTo( 1 ) );
+    }
+
+    @Test
+    public void testBorrowThread_Uninitialized_throwsIllegalStateException() throws Exception {
+        // arrange
+        FSqrWorkerThreadPool threadPool = new FSqrWorkerThreadPool( 1, "Test" );
+
+        // act
+        // assert
+        Assertions.assertThrows( IllegalStateException.class, () -> threadPool.borrowThread() );
+    }
+
+    @Test
+    public void testBorrowThread_Initialized_threadIsNotNull() throws Exception {
+        // arrange
+        FSqrWorkerThreadPool threadPool = new FSqrWorkerThreadPool( 1, "Test" );
+        threadPool.initializeThreadPool();
+
+        // act
+        FSqrWorkerThread result = threadPool.borrowThread();
+
+        // assert
+        assertThat( result, not( nullValue() ) );
+    }
+
+    @Test
+    public void testBorrowThread_initialized_threadIsInBorrowedCondition() throws Exception {
+        // arrange
+        FSqrWorkerThreadPool threadPool = new FSqrWorkerThreadPool( 1, "Test" );
+        threadPool.initializeThreadPool();
+
+        // act
+        FSqrWorkerThread borrowedThread = threadPool.borrowThread();
+
+        // assert
+        FSqrWorkerThreadLifecylce result = borrowedThread.getWorkerThreadState();
+        assertThat( result, equalTo( FSqrWorkerThreadLifecylce.BORROWED ) );
+    }
+
+    @Test
+    public void testGetNumberOfBorrowedThreads_borrowOneThread_returnOne() throws Exception {
+        // arrange
+        FSqrWorkerThreadPool threadPool = new FSqrWorkerThreadPool( 1, "Test" );
+        threadPool.initializeThreadPool();
+        FSqrWorkerThread borrowedThread = threadPool.borrowThread();
+
+        // act
+        int result = threadPool.getNumberOfBorrowedThreads();
+
+        // assert
+        assertThat( result, equalTo( 1 ) );
+    }
+
+    @Test
+    public void testGetNumberOfPooledThreads_borrowOneThread_returnsZeroPooledThreads() throws Exception {
+        // arrange
+        FSqrWorkerThreadPool threadPool = new FSqrWorkerThreadPool( 1, "Test" );
+        threadPool.initializeThreadPool();
+        FSqrWorkerThread borrowedThread = threadPool.borrowThread();
+
+        // act
+        int result = threadPool.getNumberOfPooledThreads();
+
+        // assert
+        assertThat( result, equalTo( 0 ) );
+
+    }
+
+    @Test
+    public void testGetNumberOfBorrowedThreads_borrowTwoThread_returnOne() throws Exception {
+        // arrange
+        FSqrWorkerThreadPool threadPool = new FSqrWorkerThreadPool( 3, "Test" );
+        threadPool.initializeThreadPool();
+
+        FSqrWorkerThread borrowedThread1 = threadPool.borrowThread();
+        FSqrWorkerThread borrowedThread2 = threadPool.borrowThread();
+
+        // act
+        int result = threadPool.getNumberOfBorrowedThreads();
+
+        // assert
+        assertThat( result, equalTo( 2 ) );
     }
 
 }
