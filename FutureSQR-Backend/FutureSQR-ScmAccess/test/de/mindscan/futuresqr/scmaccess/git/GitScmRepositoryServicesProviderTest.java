870dc50573411224c5cdcc3638d86383afd32be2
mindscan-de <maxim.gansert@gmail.com>
2023-09-03 20:29:00 +0200

    add two unit tests for the UpdatedFrom and UpdatedTo revision.

--- FutureSQR-Backend/FutureSQR-ScmAccess/test/de/mindscan/futuresqr/scmaccess/git/GitScmRepositoryServicesProviderTest.java
+++ FutureSQR-Backend/FutureSQR-ScmAccess/test/de/mindscan/futuresqr/scmaccess/git/GitScmRepositoryServicesProviderTest.java
@@ -1,15 +1,19 @@
 package de.mindscan.futuresqr.scmaccess.git;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 import org.junit.jupiter.api.Test;
 
 import de.mindscan.futuresqr.scmaccess.configuration.HardcodedScmConfigurationProviderImpl;
 import de.mindscan.futuresqr.scmaccess.types.ScmRepository;
+import de.mindscan.futuresqr.scmaccess.types.ScmResultType;
 import de.mindscan.futuresqr.scmaccess.types.ScmUpdateResult;
 
 public class GitScmRepositoryServicesProviderTest {
 
     @Test
-    public void testUpdateProjectCache() throws Exception {
+    public void testUpdateProjectCache_isSuccessResult() throws Exception {
         // arrange
         GitScmRepositoryServicesProvider provider = new GitScmRepositoryServicesProvider( new HardcodedScmConfigurationProviderImpl() );
         provider.setGitCLICommandExecutor( getFakeGitCommandExecutor() );
@@ -18,7 +22,33 @@ public class GitScmRepositoryServicesProviderTest {
         ScmUpdateResult result = provider.updateProjectCache( new ScmRepository( "D:\\Temp\\future-square-cache\\FutureSQR" ), "main" );
 
         // assert
+        assertThat( result.getResult(), equalTo( ScmResultType.SUCCESS_RESULT ) );
+    }
 
+    @Test
+    public void testUpdateProjectCache_getFromRevision_resultIsAC96C349() throws Exception {
+        // arrange
+        GitScmRepositoryServicesProvider provider = new GitScmRepositoryServicesProvider( new HardcodedScmConfigurationProviderImpl() );
+        provider.setGitCLICommandExecutor( getFakeGitCommandExecutor() );
+
+        // act
+        ScmUpdateResult result = provider.updateProjectCache( new ScmRepository( "D:\\Temp\\future-square-cache\\FutureSQR" ), "main" );
+
+        // assert
+        assertThat( result.getUpdatedFromRevision(), equalTo( "ac96c349" ) );
+    }
+
+    @Test
+    public void testUpdateProjectCache_getFromRevision_resultIsB2092B33() throws Exception {
+        // arrange
+        GitScmRepositoryServicesProvider provider = new GitScmRepositoryServicesProvider( new HardcodedScmConfigurationProviderImpl() );
+        provider.setGitCLICommandExecutor( getFakeGitCommandExecutor() );
+
+        // act
+        ScmUpdateResult result = provider.updateProjectCache( new ScmRepository( "D:\\Temp\\future-square-cache\\FutureSQR" ), "main" );
+
+        // assert
+        assertThat( result.getUpdatedToRevision(), equalTo( "b2092b33" ) );
     }
 
     private FakeGitCLICommandExecutor getFakeGitCommandExecutor() {
